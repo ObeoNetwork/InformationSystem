c0655ae09ae32e660094c0bfa558cbc318ba7028
Stephane Thibaudeau <stephane.thibaudeau@obeo.fr>
2012-07-23 15:52:43 +0200

    Add edition table to create requirements and the corresponding properties views

---
+++ designs/requirement/plugins/org.obeonetwork.dsl.requirement.design/src/org/obeonetwork/dsl/requirement/parts/forms/CustomRequirementPropertiesEditionPartForm.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.obeonetwork.dsl.requirement.parts.forms;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.eef.runtime.api.component.IPropertiesEditionComponent;
+
+import fr.obeo.dsl.viewpoint.DSemanticDecorator;
+
+/**
+ * Specific class to override resolveSemanticElement() so it works with tables
+ * 
+ */
+public class CustomRequirementPropertiesEditionPartForm extends RequirementPropertiesEditionPartForm {
+	
+	public CustomRequirementPropertiesEditionPartForm() {
+		super();
+	}
+
+	public CustomRequirementPropertiesEditionPartForm(IPropertiesEditionComponent editionComponent) {
+		super(editionComponent);
+	}
+
+	@Override
+	protected EObject resolveSemanticObject(Object object) {
+		if (object instanceof DSemanticDecorator) {
+			return((DSemanticDecorator) object).getTarget();
+		}
+		return super.resolveSemanticObject(object);
+	}
+}
