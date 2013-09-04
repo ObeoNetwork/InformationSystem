216ca5cf8dd1ecef1a2b0e6c73df861a6f51a3d4
Stephane Thibaudeau <stephane.thibaudeau@obeo.fr>
2013-09-04 10:56:52 +0200

    Fix bug with oppositeOf and controlled resources

--- models/environment/plugins/org.obeonetwork.dsl.environment/src/org/obeonetwork/dsl/environment/spec/ReferenceSpec.java
+++ models/environment/plugins/org.obeonetwork.dsl.environment/src/org/obeonetwork/dsl/environment/spec/ReferenceSpec.java
@@ -1,28 +1,20 @@
 package org.obeonetwork.dsl.environment.spec;
 
-import org.eclipse.emf.common.notify.Notification;
-import org.eclipse.emf.ecore.impl.ENotificationImpl;
 import org.obeonetwork.dsl.environment.DTO;
-import org.obeonetwork.dsl.environment.EnvironmentPackage;
 import org.obeonetwork.dsl.environment.Reference;
 import org.obeonetwork.dsl.environment.impl.ReferenceImpl;
 
 public class ReferenceSpec extends ReferenceImpl {
 	@Override
 	public void setType(DTO newType) {
-		DTO oldType = getType();
 		super.setType(newType);
 
 		// We check if the oppositeOf is still consistent
-		if (getOppositeOf() != null
+		if (getOppositeOf() != null && !newType.eIsProxy()
 				&& (newType == null || getOppositeOf().getDto() == null || !getOppositeOf()
 						.getDto().isSubtypeOf(newType))) {
 			setOppositeOf(null);
 		}
-
-		if (eNotificationRequired())
-			eNotify(new ENotificationImpl(this, Notification.SET,
-					EnvironmentPackage.REFERENCE__TYPE, oldType, getType()));
 	}
 
 	
@@ -37,16 +29,10 @@ public class ReferenceSpec extends ReferenceImpl {
 			}
 
 			// If a new opposite is specified, we have to ensure consistency
-			if (newOppositeOf != null
+			if (newOppositeOf != null && !newOppositeOf.eIsProxy()
 					&& (newOppositeOf.getOppositeOf() == null || !newOppositeOf
 							.getOppositeOf().equals(this))) {
 				newOppositeOf.setOppositeOf(this);
 			}
-
-			if (eNotificationRequired())
-				eNotify(new ENotificationImpl(this, Notification.SET,
-						EnvironmentPackage.REFERENCE__OPPOSITE_OF, oldOppositeOf,
-						getOppositeOf()));
-		
 	}
 }
