fa3a55a8e4e12d123e5adb147b285815f664f15a
Stephane Thibaudeau <stephane.thibaudeau@obeo.fr>
2014-02-10 17:38:54 +0100

    Fix bug with combined fragments and participants ordering

--- models/interaction/plugins/org.obeonetwork.dsl.interaction/src/org/obeonetwork/dsl/interaction/impl/OperandImpl.java
+++ models/interaction/plugins/org.obeonetwork.dsl.interaction/src/org/obeonetwork/dsl/interaction/impl/OperandImpl.java
@@ -11,6 +11,9 @@
 package org.obeonetwork.dsl.interaction.impl;
 
 import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.obeonetwork.dsl.interaction.CombinedFragment;
+import org.obeonetwork.dsl.interaction.End;
 import org.obeonetwork.dsl.interaction.InteractionPackage;
 import org.obeonetwork.dsl.interaction.Operand;
 
@@ -41,6 +44,32 @@ public class OperandImpl extends InteractionFragmentImpl implements Operand {
 	@Override
 	protected EClass eStaticClass() {
 		return InteractionPackage.Literals.OPERAND;
+	}
+	
+	/**
+	 * @generated NOT
+	 */
+	@Override
+	public End getFinishingEnd() {
+        End result = null;
+        EObject eContainer = eContainer();
+
+        if (eContainer instanceof CombinedFragment) {
+            CombinedFragment cf = (CombinedFragment) eContainer;
+            result = cf.getFinishingEnd();
+
+            Operand prev = null;
+            for (Operand op : cf.getOwnedOperands()) {
+                if (this.equals(prev)) {
+                    result = op.getStartingEnd();
+                    break;
+                } else {
+                    prev = op;
+                }
+            }
+        }
+
+        return result;
 	}
 
 } //OperandImpl
