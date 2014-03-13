94bc651fe73d2cedfb61d9de0b72a54488b7e499
Stéphane Thibaudeau <stephane.thibaudeau@obeo.fr>
2014-03-13 14:28:57 +0100

    Graal designer : small fix to avoid potential NPE

--- designs/graal/plugins/org.obeonetwork.graal.design/src/org/obeonetwork/graal/design/ui/decoration/AbstractUserStoryDecorator.java
+++ designs/graal/plugins/org.obeonetwork.graal.design/src/org/obeonetwork/graal/design/ui/decoration/AbstractUserStoryDecorator.java
@@ -17,6 +17,7 @@ import java.util.Map;
 
 import org.eclipse.emf.common.notify.Adapter;
 import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.common.notify.Notifier;
 import org.eclipse.emf.common.notify.impl.AdapterImpl;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.gmf.runtime.diagram.ui.services.decorator.AbstractDecorator;
@@ -194,7 +195,10 @@ public abstract class AbstractUserStoryDecorator extends AbstractDecorator {
 	 */
 	private void detachUserStoriesAdapters() {
 		for (Adapter adapter : activeUserStoriesAdapters) {
-			adapter.getTarget().eAdapters().remove(adapter);
+			Notifier target = adapter.getTarget();
+			if (target != null) {
+				target.eAdapters().remove(adapter);
+			}
 		}
 		activeUserStoriesAdapters.clear();
 	}
