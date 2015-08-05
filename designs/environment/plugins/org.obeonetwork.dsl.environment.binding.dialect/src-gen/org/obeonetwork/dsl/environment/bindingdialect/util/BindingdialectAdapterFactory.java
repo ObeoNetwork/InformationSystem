81b3bbf6203f16c2e6f3aebece0846d4cfc821f2
Julien Dupont <julien.dupont@obeo.fr>
2015-08-05 14:24:34 +0200

    [OD4IS] Migration from OD7 to OD8

--- designs/environment/plugins/org.obeonetwork.dsl.environment.binding.dialect/src-gen/org/obeonetwork/dsl/environment/bindingdialect/util/BindingdialectAdapterFactory.java
+++ designs/environment/plugins/org.obeonetwork.dsl.environment.binding.dialect/src-gen/org/obeonetwork/dsl/environment/bindingdialect/util/BindingdialectAdapterFactory.java
@@ -19,7 +19,6 @@ import org.obeonetwork.dsl.environment.bindingdialect.DBindingEdge;
 import org.obeonetwork.dsl.environment.bindingdialect.DBindingEditor;
 import org.obeonetwork.dsl.environment.bindingdialect.DBoundElement;
 
-import org.eclipse.sirius.viewpoint.DLabelled;
 import org.eclipse.sirius.viewpoint.DMappingBased;
 import org.eclipse.sirius.viewpoint.DRefreshable;
 import org.eclipse.sirius.viewpoint.DRepresentation;
@@ -123,11 +122,7 @@ public class BindingdialectAdapterFactory extends AdapterFactoryImpl {
 			@Override
 			public Adapter caseDSemanticDecorator(DSemanticDecorator object) {
 				return createDSemanticDecoratorAdapter();
-			}
-			@Override
-			public Adapter caseDLabelled(DLabelled object) {
-				return createDLabelledAdapter();
-			}
+			}			
 			@Override
 			public Adapter caseDMappingBased(DMappingBased object) {
 				return createDMappingBasedAdapter();
