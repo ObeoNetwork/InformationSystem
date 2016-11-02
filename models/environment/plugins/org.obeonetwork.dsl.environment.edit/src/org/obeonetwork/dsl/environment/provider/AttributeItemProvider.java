73e4e8e5aee3714007a3aac93e6184a1d68782b7
Stéphane Thibaudeau <stephane.thibaudeau@obeo.fr>
2016-11-02 14:43:58 +0100

    SAFRAN-422: Restrict enumerations suggested for an attribute
    Suggested enumerations are filtered to keep only enumerations defined
    within the same context as the attribute. For example an attribut on a
    Graal Domain class can only use an enumeration defined in a graal model.
    
    Bug: SAFRAN-422
    Change-Id: Ib412a590d98250ad2918e4b5f7d9db3b8ac78277
    Signed-off-by: Stéphane Thibaudeau <stephane.thibaudeau@obeo.fr>

--- models/environment/plugins/org.obeonetwork.dsl.environment.edit/src/org/obeonetwork/dsl/environment/provider/AttributeItemProvider.java
+++ models/environment/plugins/org.obeonetwork.dsl.environment.edit/src/org/obeonetwork/dsl/environment/provider/AttributeItemProvider.java
@@ -12,15 +12,21 @@
  */
 package org.obeonetwork.dsl.environment.provider;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
 import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
+import org.eclipse.emf.edit.provider.ItemPropertyDescriptor;
 import org.obeonetwork.dsl.environment.Attribute;
+import org.obeonetwork.dsl.environment.Enumeration;
 import org.obeonetwork.dsl.environment.EnvironmentPackage;
+import org.obeonetwork.dsl.environment.NamespacesContainer;
+import org.obeonetwork.dsl.environment.StructuredType;
 
 /**
  * This is the item provider adapter for a {@link org.obeonetwork.dsl.environment.Attribute} object.
@@ -66,15 +72,84 @@ public class AttributeItemProvider extends PropertyItemProvider {
 	 * This adds a property descriptor for the Type feature.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
+	 * @generated NOT
 	 */
 	protected void addTypePropertyDescriptor(Object object) {
-		itemPropertyDescriptors
-				.add(createItemPropertyDescriptor(((ComposeableAdapterFactory) adapterFactory).getRootAdapterFactory(),
-						getResourceLocator(), getString("_UI_Attribute_type_feature"),
-						getString("_UI_PropertyDescriptor_description", "_UI_Attribute_type_feature",
-								"_UI_Attribute_type"),
-						EnvironmentPackage.Literals.ATTRIBUTE__TYPE, true, false, true, null, null, null));
+		// Filter enumerations to keep only those defined in same kind of resource
+		itemPropertyDescriptors.add(
+			new ItemPropertyDescriptor(
+					((ComposeableAdapterFactory) adapterFactory).getRootAdapterFactory(),
+					getResourceLocator(),
+					getString("_UI_Attribute_type_feature"),
+					getString("_UI_PropertyDescriptor_description", "_UI_Attribute_type_feature", "_UI_Attribute_type"),
+					EnvironmentPackage.Literals.ATTRIBUTE__TYPE,
+					true, false, true, null, null, null) {
+				public Collection<?> getChoiceOfValues(Object object) {
+					Collection<?> choiceOfValues = super.getChoiceOfValues(object);
+					
+					Collection<Object> result = new ArrayList<Object>();
+					if (!choiceOfValues.isEmpty()) {
+						NamespacesContainer rootContainer = null;
+						if (object instanceof Attribute) {
+							rootContainer = getRootNamespacesContainer((Attribute) object);
+						}
+						
+						// Loop on all choices
+						for (Object choice : choiceOfValues) {
+							if (choice instanceof Enumeration) {
+								// We have to ensure objects are in the same kind of resource
+								if (rootContainer != null) {
+									NamespacesContainer choiceRootContainer = getRootNamespacesContainer((Enumeration)choice);
+									if (choiceRootContainer != null && rootContainer.eClass().equals(choiceRootContainer.eClass())) {
+									result.add(choice);
+									}
+								}
+								
+							} else {
+								result.add(choice);
+							}
+						}
+					}
+					return result;
+				};
+			}
+		);
+	}
+	
+	private NamespacesContainer getRootNamespacesContainer(Enumeration enumeration) {
+		EObject eContainer = enumeration.eContainer();
+		if (eContainer instanceof NamespacesContainer) {
+			return getRootNamespacesContainer((NamespacesContainer)eContainer);
+		} else {
+			return null;
+		}
+	}
+	
+	private NamespacesContainer getRootNamespacesContainer(Attribute attribute) {
+		EObject eContainer = attribute.eContainer();
+		if (eContainer instanceof StructuredType) {
+			return getRootNamespacesContainer((StructuredType)eContainer);
+		} else {
+			return null;
+		}
+	}
+	
+	private NamespacesContainer getRootNamespacesContainer(StructuredType structuredType) {
+		EObject eContainer = structuredType.eContainer();
+		if (eContainer instanceof NamespacesContainer) {
+			return getRootNamespacesContainer((NamespacesContainer)eContainer);
+		} else {
+			return null;
+		}
+	}
+	
+	private NamespacesContainer getRootNamespacesContainer(NamespacesContainer container) {
+		EObject eContainer = container.eContainer();
+		if (eContainer instanceof NamespacesContainer) {
+			return getRootNamespacesContainer((NamespacesContainer)eContainer);
+		} else {
+			return container;
+		}
 	}
 
 	/**
