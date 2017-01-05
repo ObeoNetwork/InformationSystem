6781cf7d34a93564911b7f29a59cec95ba3e5953
Nathalie Lepine <nathalie.lepine@obeo.fr>
2017-01-05 14:20:08 +0100

    fix copyrights for entity, entityrelation and database
    Change-Id: I23b42de9480d72563de1cc0ed4c4a27114f61228

--- models/ecorebinding/plugins/org.obeonetwork.dsl.ecorebinding.editor/src/org/obeonetwork/dsl/ecorebinding/presentation/EcoreSelectionWizardPage.java
+++ models/ecorebinding/plugins/org.obeonetwork.dsl.ecorebinding.editor/src/org/obeonetwork/dsl/ecorebinding/presentation/EcoreSelectionWizardPage.java
@@ -1,24 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2012 Obeo. All Rights Reserved.
+ * Copyright (c) 2006, 2017 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- * This file is part of Obeo Ecore Binding.
- *
- * This software and the attached documentation are the exclusive ownership
- * of its authors and was conceded to the profit of Obeo SARL.
- * This software and the attached documentation are protected under the rights
- * of intellectual ownership, including the section "Titre II Droits des auteurs (Articles L121-1 L123-12)"
- * By installing this software, you acknowledge being aware of these rights and
- * accept them, and as a consequence you must:
- * - be in possession of a valid license of use conceded by Obeo only.
- * - agree that you have read, understood, and will comply with the license terms and conditions.
- * - agree not to do anything that could conflict with intellectual ownership owned by Obeo or its beneficiaries
- * or the authors of this software
- *
- * Should you not agree with these terms, you must stop using this software and give it back to its legitimate owner.
- *
- * 
  * Contributors:
- *     Hugo Marchadour (Obeo) - initial API and implementation
+ *     Obeo - initial API and implementation
  *******************************************************************************/
 package org.obeonetwork.dsl.ecorebinding.presentation;
 
