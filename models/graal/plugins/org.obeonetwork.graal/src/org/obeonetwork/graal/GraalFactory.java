f79f51ca319eb31223025539fa4f7ae4d2409a41
Nathalie Lepine <nathalie.lepine@obeo.fr>
2017-01-05 16:05:28 +0100

    fix isdesigner and graal copyrights
    Change-Id: I7be2e9c3d2e1e8b84b7e1a88065739c8e132834b

--- models/graal/plugins/org.obeonetwork.graal/src/org/obeonetwork/graal/GraalFactory.java
+++ models/graal/plugins/org.obeonetwork.graal/src/org/obeonetwork/graal/GraalFactory.java
@@ -1,8 +1,13 @@
-/**
- * </copyright>
+/*******************************************************************************
+ * Copyright (c) 2010, 2017 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- * $Id$
- */
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
 package org.obeonetwork.graal;
 
 import org.eclipse.emf.ecore.EFactory;
