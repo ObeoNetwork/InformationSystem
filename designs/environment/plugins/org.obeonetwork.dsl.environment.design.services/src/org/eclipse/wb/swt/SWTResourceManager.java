8e2c357fefb5280b51d4f56c182c9b34c87c4ee4
Nathalie Lepine <nathalie.lepine@obeo.fr>
2017-01-05 14:41:07 +0100

    fix environment copyrights
    Change-Id: I0a53848b341ae391edfe8a45d94ce6f9f3f68921

--- designs/environment/plugins/org.obeonetwork.dsl.environment.design.services/src/org/eclipse/wb/swt/SWTResourceManager.java
+++ designs/environment/plugins/org.obeonetwork.dsl.environment.design.services/src/org/eclipse/wb/swt/SWTResourceManager.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2011 Google, Inc.
+ * Copyright (c) 2015, 2017 Obeo.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    Google, Inc. - initial API and implementation
+ *     Obeo - initial API and implementation
  *******************************************************************************/
 package org.eclipse.wb.swt;
 
