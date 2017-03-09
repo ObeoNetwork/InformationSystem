166e8a3c28dd466b0460327ae52a6c877d3e468d
Florent Latombe <florent.latombe@obeo.fr>
2017-03-09 14:10:05 +0100

    Add wizard entry to import sample E-Book Store project.
    Change-Id: Ief3737b3d81f61aa1616c9def126c679ba0d446d
    Signed-off-by: Florent Latombe <florent.latombe@obeo.fr>

---
+++ samples/plugins/org.obeonetwork.is.samples/src/org/obeonetwork/is/samples/InformationSystemExampleWizard.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+
+package org.obeonetwork.is.samples;
+
+import org.eclipse.emf.common.ui.wizard.ExampleInstallerWizard;
+
+/**
+ * Wizard used to import the example projects.
+ * 
+ * @author Florent Latombe
+ *         <a href= "mailto:florent.latombe@obeo.fr">florent.latombe@obeo.fr</a>
+ *
+ */
+public class InformationSystemExampleWizard extends ExampleInstallerWizard {
+
+}
