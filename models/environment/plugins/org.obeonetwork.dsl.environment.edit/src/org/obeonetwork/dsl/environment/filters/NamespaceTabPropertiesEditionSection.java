9b0b5b9c036ebc09e47d09eb75a3542abb692ec5
Stéphane Thibaudeau <stephane.thibaudeau@obeo.fr>
2017-07-11 17:41:10 +0200

    Hide namespace properties tab for soa System

--- models/environment/plugins/org.obeonetwork.dsl.environment.edit/src/org/obeonetwork/dsl/environment/filters/NamespaceTabPropertiesEditionSection.java
+++ models/environment/plugins/org.obeonetwork.dsl.environment.edit/src/org/obeonetwork/dsl/environment/filters/NamespaceTabPropertiesEditionSection.java
@@ -13,13 +13,15 @@ package org.obeonetwork.dsl.environment.filters;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.eef.runtime.impl.utils.EEFUtils;
 import org.eclipse.jface.viewers.IFilter;
-import org.obeonetwork.dsl.environment.Namespace;
+import org.obeonetwork.dsl.environment.EnvironmentPackage;
 
 public class NamespaceTabPropertiesEditionSection implements IFilter {
 
 	
 	public boolean select(Object toTest) {
 		EObject eObj = EEFUtils.resolveSemanticObject(toTest);
-		return (eObj != null && eObj instanceof Namespace);
+		// we want to display the tab only on pure Namespace instances
+		// otherwise we have a problem with the SOA System instances
+		return (eObj != null && eObj.eClass().equals(EnvironmentPackage.Literals.NAMESPACE));
 	}
 }
