19e7200a96551ba5521c3f99f86a7bef75e446c0
Stéphane Thibaudeau <stephane.thibaudeau@obeo.fr>
2017-12-13 15:11:41 +0100

    [SAFRAN-548] Add dependencies links between namespaces
    Change-Id: I0e2707efefebb3df52a20bfa73a0957ebb83bf0e
    Signed-off-by: Stéphane Thibaudeau <stephane.thibaudeau@obeo.fr>

--- designs/environment/plugins/org.obeonetwork.dsl.environment.design.services/src/org/obeonetwork/dsl/environment/design/services/DesignServices.java
+++ designs/environment/plugins/org.obeonetwork.dsl.environment.design.services/src/org/obeonetwork/dsl/environment/design/services/DesignServices.java
@@ -10,27 +10,217 @@
  *******************************************************************************/
 package org.obeonetwork.dsl.environment.design.services;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.Set;
 
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.sirius.diagram.DEdge;
 import org.eclipse.sirius.diagram.DSemanticDiagram;
+import org.eclipse.sirius.diagram.EdgeTarget;
 import org.eclipse.sirius.viewpoint.DSemanticDecorator;
+import org.obeonetwork.dsl.environment.Attribute;
+import org.obeonetwork.dsl.environment.DataType;
+import org.obeonetwork.dsl.environment.Enumeration;
+import org.obeonetwork.dsl.environment.Namespace;
+import org.obeonetwork.dsl.environment.Reference;
 import org.obeonetwork.dsl.environment.StructuredType;
+import org.obeonetwork.dsl.environment.Type;
 
 import com.google.common.collect.Iterators;
 import com.google.common.collect.Sets;
 
 public class DesignServices {
 
+	/**
+	 * Returns all structured types displayed on a diagram
+	 * @param diagram
+	 * @return
+	 */
 	public static Set<StructuredType> getDisplayedStructuredTypes(DSemanticDiagram diagram) {
-		Set<StructuredType> result = Sets.newLinkedHashSet();
+		return getDisplayedElementsByType(diagram, StructuredType.class);
+	}
+	
+	/**
+	 * Returns all namespaces displayed on a diagram
+	 * @param diagram
+	 * @return
+	 */
+	public static Set<Namespace> getDisplayedNamespaces(DSemanticDiagram diagram) {
+		return getDisplayedElementsByType(diagram, Namespace.class);
+	}
+	
+	/**
+	 * Returns all elements of the specified class displayed on a diagram
+	 * @param diagram
+	 * @param clazz
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	private static <T extends EObject> Set<T> getDisplayedElementsByType(DSemanticDiagram diagram, Class<T> clazz) {
+		Set<T> result = Sets.newLinkedHashSet();
 		Iterator<DSemanticDecorator> it = Iterators.filter(diagram.eAllContents(), DSemanticDecorator.class);
 		while (it.hasNext()) {
 			DSemanticDecorator dec = it.next();
-			if (dec.getTarget() instanceof StructuredType) {
-				result.add((StructuredType) dec.getTarget());
+			EObject target = dec.getTarget();
+			if (target != null && clazz.isAssignableFrom(target.getClass())) {
+				result.add((T) target);
 			}
 		}
 		return result;
+	}
+	
+	/**
+	 * Returns all namespaces for which a dependency should be displayed starting from source
+	 * @param source
+	 * @param diagram
+	 * @return
+	 */
+	public static Collection<Namespace> getDependenciesToNamespaces(Namespace source, DSemanticDiagram diagram) {
+		Collection<Namespace> targetNamespaces = new ArrayList<Namespace>();
+		
+		Set<Namespace> displayedNamespaces = getDisplayedNamespaces(diagram);
+		// Check all namespaces displayed on the diagram	
+		for (Namespace displayedNamespace : displayedNamespaces) {
+			// Check if the potential target is not :
+			// - the same namespace than the source one
+			// - already taken into account by a call of this same method on this potential namespace (to prevent having 2 edges between 2 namespaces)
+			// - an ancestor or a descendant of the source namespace
+			if (displayedNamespace != source
+					&& displayedNamespace.getTechnicalid().compareTo(source.getTechnicalid()) > 0
+					&& !EcoreUtil.isAncestor(displayedNamespace, source)
+					&& !EcoreUtil.isAncestor(source, displayedNamespace)) {
+				// Now we can check if dependencies exist between the 2 namespaces
+				if (isExistingDependency(source, displayedNamespace)
+					|| isExistingDependency(displayedNamespace, source)) {
+					targetNamespaces.add(displayedNamespace);
+				}
+			}
+		}
+		
+		return targetNamespaces;
+	}
+	
+	/**
+	 * Checks whether a dependency should be displayed between 2 namespaces
+	 * @param source
+	 * @param target
+	 * @return
+	 */
+	public static boolean isExistingDependency(Namespace source, Namespace target) {
+		for (Type type : source.getTypes()) {
+			if (type instanceof StructuredType) {
+				StructuredType structuredType = (StructuredType)type;
+				
+				// Check inheritance
+				StructuredType supertype = structuredType.getSupertype();
+				if (supertype != null && target.equals(supertype.eContainer())) {
+					return true;
+				}
+				
+				// Check references
+				for (Reference reference : structuredType.getOwnedReferences()) {
+					StructuredType referencedType = reference.getReferencedType();
+					if (referencedType != null && target.equals(referencedType.eContainer())) {
+						return true;
+					}
+				}
+				
+				// Check attributes
+				for (Attribute attribute : structuredType.getOwnedAttributes()) {
+					DataType datatype = attribute.getType();
+					if (datatype instanceof Enumeration && target.equals(datatype.eContainer())) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * Returns the number of dependencies between 2 namespaces
+	 * @param source
+	 * @param target
+	 * @return
+	 */
+	public static int getNbDependencies(Namespace source, Namespace target) {
+		int count = 0;
+		for (Type type : source.getTypes()) {
+			if (type instanceof StructuredType) {
+				StructuredType structuredType = (StructuredType)type;
+				
+				// Check inheritance
+				StructuredType supertype = structuredType.getSupertype();
+				if (supertype != null && target.equals(supertype.eContainer())) {
+					count += 1;
+				}
+				
+				// Check references
+				for (Reference reference : structuredType.getOwnedReferences()) {
+					StructuredType referencedType = reference.getReferencedType();
+					if (referencedType != null && target.equals(referencedType.eContainer())) {
+						count += 1;
+					}
+				}
+				
+				// Check attributes
+				for (Attribute attribute : structuredType.getOwnedAttributes()) {
+					DataType datatype = attribute.getType();
+					if (datatype instanceof Enumeration && target.equals(datatype.eContainer())) {
+						count += 1;
+					}
+				}
+			}
+		}
+		return count;
+	}
+	
+	public static boolean isDependencyBeginDecorated(DEdge edge) {
+		Namespace source = getNamespaceFromEdgeTarget(edge.getSourceNode());
+		Namespace target = getNamespaceFromEdgeTarget(edge.getTargetNode());
+		
+		return source != null
+				&& target != null
+				&& isExistingDependency(target, source);
+	}
+	
+	public static String getDependencyBeginLabel(DEdge edge) {
+		Namespace source = getNamespaceFromEdgeTarget(edge.getSourceNode());
+		Namespace target = getNamespaceFromEdgeTarget(edge.getTargetNode());
+		
+		return "" + getNbDependencies(target, source);
+	}
+	
+	public static boolean isDependencyEndDecorated(DEdge edge) {
+		Namespace source = getNamespaceFromEdgeTarget(edge.getSourceNode());
+		Namespace target = getNamespaceFromEdgeTarget(edge.getTargetNode());
+		
+		return source != null
+				&& target != null
+				&& isExistingDependency(source, target);
+	}
+	
+	public static String getDependencyEndLabel(DEdge edge) {
+		Namespace source = getNamespaceFromEdgeTarget(edge.getSourceNode());
+		Namespace target = getNamespaceFromEdgeTarget(edge.getTargetNode());
+		
+		return "" + getNbDependencies(source, target);
+	}
+	
+	private static Namespace getNamespaceFromEdgeTarget(EdgeTarget edgeTarget) {
+		if (edgeTarget instanceof DSemanticDecorator) {
+			EObject target = ((DSemanticDecorator) edgeTarget).getTarget();
+			if (target instanceof Namespace) {
+				return (Namespace)target;
+			}
+		}
+		return null;
+	}
+
+	public static int getNbDependencies(DEdge edge) {
+		return 2;
 	}
 }
