58813f7dfddc1ce2dc40e57ad6c33e3a73b72e9d
Guillaume Coutable <guillaume.coutable@obeo.fr>
2019-06-07 15:09:34 +0200

    SAFRAN-617: Handle wizard to create new TypesLibrary and toolkit model
    - Allow to create a new TypesLibrary or a new Toolkit model
    - Removes old wizards

---
+++ designs/cinematic/plugins/org.obeonetwork.dsl.cinematic.design/src/org/obeonetwork/dsl/cinematic/design/wizards/NewToolkitsModelWizard.java
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.obeonetwork.dsl.cinematic.design.wizards;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.eclipse.emf.ecore.EObject;
+import org.obeonetwork.dsl.cinematic.design.Activator;
+import org.obeonetwork.dsl.cinematic.toolkits.Toolkit;
+import org.obeonetwork.dsl.cinematic.toolkits.ToolkitsFactory;
+import org.obeonetwork.dsl.is.ui.wizards.AbstractISNewModelWizard;
+import org.obeonetwork.dsl.is.ui.wizards.NewModelCreationPage;
+
+public class NewToolkitsModelWizard extends AbstractISNewModelWizard {
+	
+	private static final String TOOLKITS_RESOURCE_FILE_EXTENSION = ".cinematic_toolkits";
+
+	public NewToolkitsModelWizard() {
+		super("New Toolkits Model", Activator.imageDescriptorFromPlugin(Activator.PLUGIN_ID, "icons/full/wizban/NewToolkits.gif"));
+	}
+	
+	@Override
+	protected Collection<EObject> createInitialObjects() {
+		Toolkit toolkit = ToolkitsFactory.eINSTANCE.createToolkit();
+		toolkit.setName(extractNameFromTargetResourceName(TOOLKITS_RESOURCE_FILE_EXTENSION));
+		return Arrays.asList(toolkit);
+	}
+	
+	@Override
+	public void addPages() {
+		modelCreationPage = new NewModelCreationPage("ModelCreationPage", selection, TOOLKITS_RESOURCE_FILE_EXTENSION);
+		addPage(modelCreationPage);
+	}
+
+	@Override
+	protected String getWizardTitle() {
+		return "New Toolkits model";
+	}
+}
---
+++ designs/database/plugins/org.obeonetwork.dsl.database.design/src/org/obeonetwork/dsl/database/design/wizards/NewTypesLibraryModelWizard.java
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.obeonetwork.dsl.database.design.wizards;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.eclipse.emf.ecore.EObject;
+import org.obeonetwork.dsl.database.design.Activator;
+import org.obeonetwork.dsl.is.ui.wizards.AbstractISNewModelWizard;
+import org.obeonetwork.dsl.is.ui.wizards.NewModelCreationPage;
+import org.obeonetwork.dsl.typeslibrary.NativeTypesLibrary;
+import org.obeonetwork.dsl.typeslibrary.TypesLibraryFactory;
+
+public class NewTypesLibraryModelWizard extends AbstractISNewModelWizard {
+	
+	private static final String TYPESLIBRARY_RESOURCE_FILE_EXTENSION = ".typeslibrary";
+
+	public NewTypesLibraryModelWizard() {
+		super("New Types library Model", Activator.imageDescriptorFromPlugin(Activator.PLUGIN_ID, "icons/full/wizban/NewTypesLibrary.gif"));
+	}
+
+	@Override
+	protected Collection<EObject> createInitialObjects() {
+		NativeTypesLibrary nativeTypesLibrary = TypesLibraryFactory.eINSTANCE.createNativeTypesLibrary();
+		return Arrays.asList(nativeTypesLibrary);
+	}
+	
+	@Override
+	public void addPages() {
+		modelCreationPage = new NewModelCreationPage("ModelCreationPage", selection, TYPESLIBRARY_RESOURCE_FILE_EXTENSION);
+		addPage(modelCreationPage);
+	}
+
+	@Override
+	protected String getWizardTitle() {
+		return "New Types Library model";
+	}
+
+}
