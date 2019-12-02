bfba2a45679f64507220bde8875c95e87770f14b
Yvan Lussaud <yvan.lussaud@obeo.fr>
2019-12-02 10:08:20 +0100

    [SAFRAN-622] Added M2Doc services for Entity / DTO / Domain Classes.

---
+++ m2doc/entity/plugins/org.obeonetwork.entity.m2doc.services.test/src/org/obeonetwork/entity/m2doc/services/test/EntityServiceTest.java
@@ -0,0 +1,114 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.obeonetwork.entity.m2doc.services.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.obeonetwork.dsl.entity.Entity;
+import org.obeonetwork.dsl.entity.EntityPackage;
+import org.obeonetwork.dsl.environment.DTO;
+import org.obeonetwork.dsl.environment.EnvironmentPackage;
+import org.obeonetwork.dsl.environment.Namespace;
+import org.obeonetwork.entity.m2doc.services.EntityServices;
+
+public class EntityServiceTest {
+
+	EntityServices es = new EntityServices();
+
+	@Test(expected = NullPointerException.class)
+	public void getOwnedEntitiesNull() {
+		es.getOwnedEntities(null);
+	}
+
+	@Test
+	public void getOwnedEntitiesEmptyNamespace() {
+		final Namespace namespace = EnvironmentPackage.eINSTANCE.getEnvironmentFactory().createNamespace();
+
+		final List<Entity> entities = es.getOwnedEntities(namespace);
+
+		assertTrue(entities.isEmpty());
+	}
+
+	@Test
+	public void getOwnedEntitiesNoEntitiesNamespace() {
+		final Namespace namespace = EnvironmentPackage.eINSTANCE.getEnvironmentFactory().createNamespace();
+		final DTO dto = EnvironmentPackage.eINSTANCE.getEnvironmentFactory().createDTO();
+		namespace.getTypes().add(dto);
+
+		final List<Entity> entities = es.getOwnedEntities(namespace);
+
+		assertTrue(entities.isEmpty());
+	}
+
+	@Test
+	public void getOwnedEntitiesNamespace() {
+		final Namespace namespace = EnvironmentPackage.eINSTANCE.getEnvironmentFactory().createNamespace();
+		final Entity entity1 = EntityPackage.eINSTANCE.getEntityFactory().createEntity();
+		entity1.setName("entity1");
+		final Entity entity2 = EntityPackage.eINSTANCE.getEntityFactory().createEntity();
+		entity2.setName("entity2");
+		final Entity entity3 = EntityPackage.eINSTANCE.getEntityFactory().createEntity();
+		entity3.setName("entity3");
+		namespace.getTypes().add(entity2);
+		namespace.getTypes().add(entity3);
+		namespace.getTypes().add(entity1);
+
+		final List<Entity> entities = es.getOwnedEntities(namespace);
+
+		assertEquals(3, entities.size());
+		assertEquals(entity1, entities.get(0));
+		assertEquals(entity2, entities.get(1));
+		assertEquals(entity3, entities.get(2));
+	}
+
+	@Test(expected = NullPointerException.class)
+	public void hasOwnedEntitiesNull() {
+		es.hasOwnedEntities(null);
+	}
+
+	@Test
+	public void hasOwnedEntitiesEmptyNamespace() {
+		final Namespace namespace = EnvironmentPackage.eINSTANCE.getEnvironmentFactory().createNamespace();
+
+		assertFalse(es.hasOwnedEntities(namespace));
+	}
+
+	@Test
+	public void hasOwnedEntitiesNoEntitiesNamespace() {
+		final Namespace namespace = EnvironmentPackage.eINSTANCE.getEnvironmentFactory().createNamespace();
+		final DTO dto = EnvironmentPackage.eINSTANCE.getEnvironmentFactory().createDTO();
+		namespace.getTypes().add(dto);
+
+		assertFalse(es.hasOwnedEntities(namespace));
+	}
+
+	@Test
+	public void hasOwnedEntitiesNamespace() {
+		final Namespace namespace = EnvironmentPackage.eINSTANCE.getEnvironmentFactory().createNamespace();
+		final Entity entity1 = EntityPackage.eINSTANCE.getEntityFactory().createEntity();
+		entity1.setName("entity1");
+		final Entity entity2 = EntityPackage.eINSTANCE.getEntityFactory().createEntity();
+		entity2.setName("entity2");
+		final Entity entity3 = EntityPackage.eINSTANCE.getEntityFactory().createEntity();
+		entity3.setName("entity3");
+		namespace.getTypes().add(entity2);
+		namespace.getTypes().add(entity3);
+		namespace.getTypes().add(entity1);
+
+		assertTrue(es.hasOwnedEntities(namespace));
+	}
+
+}
---
+++ m2doc/entity/plugins/org.obeonetwork.entity.m2doc.services/src/org/obeonetwork/entity/m2doc/services/EntityServices.java
@@ -0,0 +1,78 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.obeonetwork.entity.m2doc.services;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import org.eclipse.acceleo.annotations.api.documentation.Documentation;
+import org.eclipse.acceleo.annotations.api.documentation.Example;
+import org.obeonetwork.dsl.entity.Entity;
+import org.obeonetwork.dsl.environment.Namespace;
+import org.obeonetwork.dsl.environment.Type;
+
+/**
+ * AQL Services for Entity's concepts.
+ *
+ * @author <a href="mailto:yvan.lussaud@obeo.fr">Yvan Lussaud</a>
+ *
+ */
+public class EntityServices {
+
+	// @formatter:off
+	@Documentation(
+			comment = "{m:myNamespace.getOwnedEntities()}",
+		    value = "Returns the list of Entity directly contained in the given Namespace.",
+		    examples = {
+		    		@Example(
+		    				expression = "{m:myNamespace.getOwnedEntities().name->sep(',')}", 
+		    				result = "The list of Entity directly contained in the given Namespace names separated by coma.")
+		    }
+		)
+	// @formatter:on	
+	public List<Entity> getOwnedEntities(Namespace namespace) {
+		final List<Entity> result = new ArrayList<>();
+
+		for (Type type : namespace.getTypes()) {
+			if (type instanceof Entity) {
+				result.add((Entity) type);
+			}
+		}
+
+		Collections.sort(result, new Comparator<Entity>() {
+
+			@Override
+			public int compare(Entity o1, Entity o2) {
+				return o1.getName().compareTo(o2.getName());
+			}
+		});
+
+		return result;
+	}
+
+	// @formatter:off
+	@Documentation(
+			comment = "{m:myNamespace.hasOwnedEntities()}",
+		    value = "Returns true if the given Namespace directly contains at least one Entity.",
+		    examples = {
+		    		@Example(
+		    				expression = "{m:myNamespace.hasOwnedEntities()}", 
+		    				result = "true or false.")
+		    }
+		)
+	// @formatter:on	
+	public boolean hasOwnedEntities(Namespace namespace) {
+		return !getOwnedEntities(namespace).isEmpty();
+	}
+
+}
