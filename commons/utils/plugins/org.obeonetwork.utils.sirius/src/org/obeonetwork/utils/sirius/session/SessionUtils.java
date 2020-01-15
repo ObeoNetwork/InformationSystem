e068cf024a24a8934780a0ce7daf1a1bbfa9d80d
William Piers <william.piers@obeo.fr>
2020-01-15 11:54:48 +0100

    Fixed DAnalysis getter for non-aird resources
    Change-Id: Ibab8cefa696e0e241905cf590e7a8033a55c8616

--- commons/utils/plugins/org.obeonetwork.utils.sirius/src/org/obeonetwork/utils/sirius/session/SessionUtils.java
+++ commons/utils/plugins/org.obeonetwork.utils.sirius/src/org/obeonetwork/utils/sirius/session/SessionUtils.java
@@ -12,39 +12,35 @@ package org.obeonetwork.utils.sirius.session;
 
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.sirius.business.api.query.AirDResouceQuery;
 import org.eclipse.sirius.business.api.query.URIQuery;
 import org.eclipse.sirius.business.api.session.Session;
-import org.eclipse.sirius.business.api.session.resource.AirdResource;
-import org.eclipse.sirius.ext.base.Option;
 import org.eclipse.sirius.viewpoint.DAnalysis;
 
 /**
  * Utilities around Sirius session
+ * 
  * @author <a href="mailto:stephane.thibaudeau@obeo.fr">Stephane Thibaudeau</a>
- *
  */
 public class SessionUtils {
-	
+
 	/**
-	 * Returns the main analysis for a session
-	 * for a local project this is the root analysis
-	 * for a remote project this is the first remote analysis referenced by the local root analysis
+	 * Returns the main analysis for a session for a local project this is the root analysis for a remote
+	 * project this is the first remote analysis referenced by the local root analysis
+	 * 
 	 * @param session
 	 * @return
 	 */
 	public DAnalysis getMainAnalysis(Session session) {
 		if (session != null) {
-			Resource airdResource = session.getSessionResource();
-			if (airdResource instanceof AirdResource) {
-				Option<DAnalysis> optionAnalysis = new AirDResouceQuery((AirdResource)airdResource).getDAnalysis();
-				if (optionAnalysis.some()) {
-					DAnalysis analysis = optionAnalysis.get();
-					
+			Resource resource = session.getSessionResource();
+			if (resource != null) {
+				if (!resource.getContents().isEmpty() && resource.getContents().get(0) instanceof DAnalysis) {
+					DAnalysis analysis = (DAnalysis)resource.getContents().get(0);
+
 					// Retrieve the first referenced analysis
 					if (!analysis.getReferencedAnalysis().isEmpty()) {
 						DAnalysis firstReferencedAnalysis = analysis.getReferencedAnalysis().get(0);
-					
+
 						// Test if remote resource
 						if (isRemoteResource(firstReferencedAnalysis.eResource())) {
 							return firstReferencedAnalysis;
@@ -61,6 +57,7 @@ public class SessionUtils {
 
 	/**
 	 * Checks if a session is a shared project by checking whether the main analysis is a CDO resource or not
+	 * 
 	 * @param session
 	 * @return
 	 */
@@ -69,7 +66,7 @@ public class SessionUtils {
 		Resource resource = mainAnalysis.eResource();
 		return isRemoteResource(resource);
 	}
-	
+
 	private boolean isRemoteResource(Resource resource) {
 		if (resource != null) {
 			URI uri = resource.getURI();
