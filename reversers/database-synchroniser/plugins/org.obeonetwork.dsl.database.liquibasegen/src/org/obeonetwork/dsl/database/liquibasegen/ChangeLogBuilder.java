2955c7d7fa9afe0643c7123a2bf33dfa3b1126dc
adaussy <adaussy@adaussy-ZenBook-UX533FD-UX533FD>
2020-09-01 15:57:44 +0200

    [SAFRAN-815] Handle AddTable
    First implementation that handle AddTable differences.

--- reversers/database-synchroniser/plugins/org.obeonetwork.dsl.database.liquibasegen/src/org/obeonetwork/dsl/database/liquibasegen/ChangeLogBuilder.java
+++ reversers/database-synchroniser/plugins/org.obeonetwork.dsl.database.liquibasegen/src/org/obeonetwork/dsl/database/liquibasegen/ChangeLogBuilder.java
@@ -10,18 +10,209 @@
  *******************************************************************************/
 package org.obeonetwork.dsl.database.liquibasegen;
 
+import java.text.MessageFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
 
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.emf.compare.Comparison;
+import org.obeonetwork.dsl.database.Column;
+import org.obeonetwork.dsl.database.DatabaseElement;
+import org.obeonetwork.dsl.database.Table;
+import org.obeonetwork.dsl.database.dbevolution.AddTable;
+import org.obeonetwork.dsl.database.dbevolution.DBDiff;
+import org.obeonetwork.dsl.database.gen.common.services.StatusUtils;
+import org.obeonetwork.dsl.database.liquibasegen.service.DefaultTypeMatcher;
+import org.obeonetwork.dsl.database.liquibasegen.service.DefaultTypeMatcher.LiquibaseDefaultType;
+import org.obeonetwork.dsl.database.liquibasegen.service.GenServices;
+import org.obeonetwork.dsl.typeslibrary.Type;
+import org.obeonetwork.dsl.typeslibrary.TypeInstance;
 
+import liquibase.change.ColumnConfig;
+import liquibase.change.ConstraintsConfig;
+import liquibase.change.core.CreateTableChange;
 import liquibase.changelog.ChangeLogChild;
+import liquibase.changelog.ChangeSet;
+import liquibase.changelog.DatabaseChangeLog;
 
 public class ChangeLogBuilder {
+	public static final String FILE_NAME = "run.changelog.xml";
+	private static final String DEFAULT_AUTHOR = "IS Designer";
+
+	private GenServices genService;
+
+	/*
+	 * Used to generate change sets
+	 */
+	private DatabaseChangeLog changelog;
+	private int changeSetCounter;
+	private String timeStamp;
+
+	/**
+	 * List of statutes raised during generation
+	 */
+	private List<IStatus> statuses = new ArrayList<IStatus>();
+
+	private static <E> Stream<E> filterAndCast(Stream<?> stream, Class<E> type) {
+		return stream.filter(e -> type.isInstance(e)).map(e -> type.cast(e));
+	}
 
 	public List<ChangeLogChild> buildContent(Comparison comparisonModel) {
 		List<ChangeLogChild> result = new ArrayList<ChangeLogChild>();
+		genService = new GenServices();
+		timeStamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm-ss--"));
+		try {
+			List<DBDiff> diffs = genService.getOrderedChanges(comparisonModel);
+			result.addAll(genChangeSetsForTables(diffs));
+		} finally {
+			genService.dispose();
+			genService = null;
+		}
+
 		return result;
+
+	}
+
+	private List<ChangeSet> buildAddTableChangeSet(AddTable addTable) {
+		List<ChangeSet> result = new ArrayList<ChangeSet>();
+		String tableName = genService.getFullName(addTable.getTable());
+		String commentPrefix = "[Add table " + tableName + "] ";
+		ChangeSet changeSet = buildCreateTableChangeSet(addTable, commentPrefix);
+		result.add(changeSet);
+		return result;
+	}
+
+	private void remarksSetter(DatabaseElement dbe, Consumer<String> setter) {
+		String comment = dbe.getComments();
+		if (comment != null && !comment.trim().isEmpty()) {
+			setter.accept(comment);
+		}
+	}
+
+	private ChangeSet buildCreateTableChangeSet(AddTable addTable, String commentPrefix) {
+		CreateTableChange ctChange = new CreateTableChange();
+		Table table = addTable.getTable();
+		ctChange.setTableName(genService.safeName(table));
+		remarksSetter(table, ctChange::setRemarks);
+
+		for (Column column : table.getColumns()) {
+			handleColumnInTable(ctChange, table, column);
+		}
+
+		ChangeSet changeSet = buildNextChangeSet();
+		changeSet.addChange(ctChange);
+		changeSet.setComments(commentPrefix + "creation");
+
+		return changeSet;
+	}
+
+	private void handleColumnInTable(CreateTableChange ctChange, Table table, Column column) {
+		ColumnConfig cConfig = new ColumnConfig();
+		cConfig.setName(column.getName());
+
+		Type type = column.getType();
+		setColumnDefaultValue(table, column, cConfig, type);
+
+		// Handle primary key
+		ConstraintsConfig constraintConfig = new ConstraintsConfig();
+		constraintConfig.setNullable(column.isNullable());
+		if (column.isInPrimaryKey()) {
+			constraintConfig.setPrimaryKey(column.isInPrimaryKey());
+		}
+
+		cConfig.setConstraints(constraintConfig);
+
+		cConfig.setAutoIncrement(column.isAutoincrement());
+
+		remarksSetter(column, cConfig::setRemarks);
+
+		ctChange.addColumn(cConfig);
+	}
+
+	private void setColumnDefaultValue(Table table, Column column, ColumnConfig cConfig, Type type) {
+		if (type instanceof TypeInstance) {
+			TypeInstance typeInstance = (TypeInstance) type;
+			String stringType = genService.getType(typeInstance);
+			cConfig.setType(stringType);
+			String defaultValue = column.getDefaultValue();
+			if (defaultValue != null && !defaultValue.isEmpty()) {
+				setColumnDefaultvalue(table, column, cConfig, typeInstance, defaultValue);
+			}
+
+		}
+	}
+
+	private void setColumnDefaultvalue(Table table, Column column, ColumnConfig cConfig, TypeInstance typeInstance,
+			String defaultValue) {
+		LiquibaseDefaultType liquibaseMatchingType = DefaultTypeMatcher
+				.getLiquibaseDefaultType(typeInstance.getNativeType());
+
+		if (liquibaseMatchingType == LiquibaseDefaultType.UNKWOWN) {
+			statuses.add(StatusUtils.createWarningStatus(MessageFormat.format(
+					"Unable to set default value for type {0}#{1}. {2} has an unknown logical type.",
+					genService.getFullName(table), column.getName(), genService.getLabel(typeInstance))));
+		} else if (liquibaseMatchingType == LiquibaseDefaultType.INVALID) {
+			statuses.add(StatusUtils.createWarningStatus(
+					MessageFormat.format("Unable to set default value for type {0}#{1}. {2} missing type information.",
+							genService.getFullName(table), column.getName(), genService.getLabel(typeInstance))));
+		} else {
+			IStatus defaultValueValidation = DefaultTypeMatcher.validateValue(liquibaseMatchingType, defaultValue);
+			String processedValue = DefaultTypeMatcher.preProcessDefaultValue(liquibaseMatchingType, defaultValue);
+			if (defaultValueValidation.isOK()) {
+				switch (liquibaseMatchingType) {
+				case BOOLEAN:
+					cConfig.setDefaultValueBoolean(processedValue);
+					break;
+				case DATE:
+					cConfig.setDefaultValueDate(processedValue);
+					break;
+				case STRING:
+					cConfig.setDefaultValue(processedValue);
+					break;
+				case NUM:
+					cConfig.setDefaultValueNumeric(processedValue);
+					break;
+				default:
+					break;
+				}
+			} else {
+				statuses.add(StatusUtils.createMultiStatus(
+						"Unable to set default value for type {0}#{1}. {2} invalid default value.",
+						Collections.singletonList(defaultValueValidation)));
+			}
+		}
+	}
+
+	public List<IStatus> getStatuses() {
+		return statuses;
+	}
+
+	private ChangeSet buildNextChangeSet() {
+		return new ChangeSet(getNextId(), // Id
+				DEFAULT_AUTHOR, // Author
+				false, // Always run
+				false, // Run on change
+				FILE_NAME, // Changelog filename
+				"", // Context list
+				"", // dbms
+				true, // run in transaction
+				null, changelog);
+	}
+
+	private List<ChangeLogChild> genChangeSetsForTables(List<DBDiff> diffs) {
+		List<ChangeLogChild> result = new ArrayList<ChangeLogChild>();
+		filterAndCast(diffs.stream(), AddTable.class).map(this::buildAddTableChangeSet).forEachOrdered(result::addAll);
+		return result;
+
+	}
+
+	private String getNextId() {
+		return timeStamp + String.valueOf(changeSetCounter++);
 	}
 
 }
---
+++ reversers/database-synchroniser/plugins/org.obeonetwork.dsl.database.liquibasegen/src/org/obeonetwork/dsl/database/liquibasegen/service/DefaultTypeMatcher.java
@@ -0,0 +1,151 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.obeonetwork.dsl.database.liquibasegen.service;
+
+import static org.obeonetwork.dsl.database.gen.common.services.StatusUtils.createErrorStatus;
+
+import java.text.ParseException;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.obeonetwork.dsl.typeslibrary.NativeType;
+
+import liquibase.util.ISODateFormat;
+
+/**
+ * Helper class used to handle column type and default value for the liquibase
+ * generation
+ *
+ */
+public class DefaultTypeMatcher {
+
+	private static final String BINARY_FORMAT = "Binary";
+	private static final String BOOLEAN_FORMAT = "Booleen";
+	private static final String DATE_FORMAT = "Date";
+	private static final String DECIMAL_FORAMT = "Décimal";
+	private static final String INT_FORMAT = "Entier";
+	private static final String LONG_FORMAT = "Entier long";
+	private static final String FLOAT_FORMAT = "Réel";
+	private static final String STRING_FORMAT = "Texte";
+	private static final String TIME_FORMAT = "Time";
+	private static final String TIMESTAMPFORMAT = "Timestamp";
+
+	public enum LiquibaseDefaultType {
+
+		/**
+		 * Returned if the given type has an unknown data type
+		 */
+		UNKWOWN,
+		/**
+		 * Return if the model is not complete enough to guess the matching types
+		 */
+		INVALID,
+		/**
+		 * Returned for data type that can set default value in liquibase
+		 */
+		UNHANDLED, BOOLEAN, STRING, DATE, NUM
+
+	}
+
+	/**
+	 * Gets the matching {@link LiquibaseDefaultType} for the given
+	 * {@link NativeType}
+	 * 
+	 * @param nativeType a {@link NativeType}
+	 * @return a {@link LiquibaseDefaultType}
+	 */
+	public static LiquibaseDefaultType getLiquibaseDefaultType(NativeType nativeType) {
+		if (nativeType != null) {
+			NativeType mdlType = nativeType.getMapsTo();
+			if (mdlType != null) {
+				switch (mdlType.getName()) {
+				case BOOLEAN_FORMAT:
+					return LiquibaseDefaultType.BOOLEAN;
+				case DECIMAL_FORAMT:
+				case INT_FORMAT:
+				case FLOAT_FORMAT:
+				case LONG_FORMAT:
+					return LiquibaseDefaultType.NUM;
+				case TIME_FORMAT:
+				case DATE_FORMAT:
+					return LiquibaseDefaultType.DATE;
+				case STRING_FORMAT:
+					return LiquibaseDefaultType.STRING;
+				case BINARY_FORMAT:
+				case TIMESTAMPFORMAT:
+					return LiquibaseDefaultType.UNHANDLED;
+				default:
+					return LiquibaseDefaultType.UNKWOWN;
+				}
+			}
+		}
+		return LiquibaseDefaultType.INVALID;
+	}
+
+	/**
+	 * Validate the given value for the matching data type
+	 * 
+	 * @param type  a {@link LiquibaseDefaultType}
+	 * @param value the default value
+	 * @return an {@link Status#OK_STATUS} if ok, a status with a message otherwise
+	 */
+	public static IStatus validateValue(LiquibaseDefaultType type, String value) {
+		if (type == null) {
+			return Status.OK_STATUS;
+		}
+		switch (type) {
+		case DATE:
+			return validateDateDefaultValue(value);
+		default:
+			return Status.OK_STATUS;
+		}
+	}
+
+	/**
+	 * Pre process the default value to it can be used by the Liquibase API
+	 * 
+	 * @param type  the Liquibase type
+	 * @param value the input value
+	 * @return a transformed (or not) value
+	 */
+	public static String preProcessDefaultValue(LiquibaseDefaultType type, String value) {
+		if (type == null) {
+			return value;
+		}
+		switch (type) {
+		case STRING:
+			return preProcessString(value);
+		default:
+			return value;
+		}
+	}
+
+	private static String preProcessString(String value) {
+		String trimmedValue = value.trim();
+		if (trimmedValue.length() < 2) {
+			return trimmedValue;
+		}
+		if (trimmedValue.charAt(0) == '\'' && trimmedValue.charAt(trimmedValue.length() - 1) == '\'') {
+			return trimmedValue.substring(1, trimmedValue.length() - 1);
+		}
+		return trimmedValue;
+	}
+
+	private static IStatus validateDateDefaultValue(String value) {
+		try {
+			new ISODateFormat().parse(value);
+			return Status.OK_STATUS;
+		} catch (ParseException e) {
+			return createErrorStatus("Unable to use " + value + " has default date. Expecting ISO Date", e);
+		}
+	}
+
+}
