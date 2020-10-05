6c9008c26f4ee3701d06585a3cdc07f71bf224ac
Arthur Daussy <arthur.daussy@obeo.fr>
2020-10-05 14:31:11 +0200

    [SAFRAN-815] Keep the last generate changelog at the same location
    The changeLog file is keep in two places:
    
    * A folder structure that keep tracks of the date
    * In a common folder for the given input model (help the run of the
    liquibase command)

--- reversers/database-synchroniser/plugins/org.obeonetwork.dsl.database.liquibasegen/src/org/obeonetwork/dsl/database/liquibasegen/LiquibaseGenerator.java
+++ reversers/database-synchroniser/plugins/org.obeonetwork.dsl.database.liquibasegen/src/org/obeonetwork/dsl/database/liquibasegen/LiquibaseGenerator.java
@@ -15,7 +15,9 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
@@ -28,7 +30,6 @@ import org.eclipse.core.runtime.Status;
 import org.eclipse.emf.common.util.Monitor;
 import org.eclipse.emf.compare.Comparison;
 import org.obeonetwork.dsl.database.DataBase;
-import org.obeonetwork.dsl.database.Schema;
 import org.obeonetwork.dsl.database.gen.common.services.StatusUtils;
 import org.obeonetwork.dsl.database.gen.common.services.TypesServices;
 import org.obeonetwork.dsl.typeslibrary.NativeTypesLibrary;
@@ -49,45 +50,43 @@ public class LiquibaseGenerator {
 	public IStatus doGenerate(Monitor monitor, Comparison comparisonModel, Path targetFolder) throws IOException {
 
 		File computeTargetFolder = computeTargetFolder(targetFolder.toFile(), comparisonModel);
-		if (!computeTargetFolder.exists()) {
-			if (!computeTargetFolder.mkdirs()) {
-				throw new IOException("Unable to create folder at " + targetFolder);
-			}
-		}
+		File computeLastTargetFolder = computeLastGenerationTargetFolder(targetFolder.toFile(), comparisonModel);
 
+		// Current generation path (stored in path holding the date)
 		File file = computeTargetFolder.toPath().resolve(ChangeLogBuilder.FILE_NAME).toFile();
-		if (file.createNewFile()) {
-			ChangeLogBuilder changeLogBuilder = new ChangeLogBuilder();
-			List<ChangeLogChild> contents = changeLogBuilder.buildContent(comparisonModel, idPrefixProvider.get());
-			if (!contents.isEmpty()) {
-				XMLChangeLogSerializer xmlSerializer = new XMLChangeLogSerializer();
-				try (OutputStream output = new BufferedOutputStream(new FileOutputStream(file))) {
-					xmlSerializer.write(contents, output);
-					output.flush();
-				}
 
-				// TODO remove this
-				File tmpTarget = computeTargetFolder.toPath().getParent().getParent()
-						.resolve(ChangeLogBuilder.FILE_NAME).toFile();
-				try (OutputStream output = new BufferedOutputStream(new FileOutputStream(tmpTarget))) {
-					xmlSerializer.write(contents, output);
-					output.flush();
-				}
+		File lastFile = computeLastTargetFolder.toPath().resolve(ChangeLogBuilder.FILE_NAME).toFile();
 
+		ChangeLogBuilder changeLogBuilder = new ChangeLogBuilder();
+		List<ChangeLogChild> contents = changeLogBuilder.buildContent(comparisonModel, idPrefixProvider.get());
+		if (!contents.isEmpty()) {
+			XMLChangeLogSerializer xmlSerializer = new XMLChangeLogSerializer();
+			try (OutputStream output = new BufferedOutputStream(new FileOutputStream(file))) {
+				xmlSerializer.write(contents, output);
+				output.flush();
 			}
-			List<IStatus> statuses = changeLogBuilder.getStatuses();
-			if (statuses.isEmpty()) {
-				return Status.OK_STATUS;
-			} else {
-				return StatusUtils.createMultiStatus("See message in the detail section", statuses);
-			}
+
+			Files.copy(file.toPath(), lastFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+
+		}
+		List<IStatus> statuses = changeLogBuilder.getStatuses();
+		if (statuses.isEmpty()) {
+			return Status.OK_STATUS;
 		} else {
-			return StatusUtils.createErrorStatus("Unable to create file at " + file);
+			return StatusUtils.createMultiStatus("See message in the detail section", statuses);
 		}
 
 	}
 
-	private File computeTargetFolder(File folder, Comparison comparison) {
+	/**
+	 * Compute the folder holding the last generated shnage log for the given model
+	 * 
+	 * @param folder     a root folder
+	 * @param comparison the current comparison
+	 * @return a folder
+	 * @throws IOException
+	 */
+	private File computeLastGenerationTargetFolder(File folder, Comparison comparison) throws IOException {
 		String folderName = "";
 		String dbtypeFolderName = "";
 		if (comparison != null) {
@@ -101,18 +100,46 @@ public class LiquibaseGenerator {
 			}
 
 			folderName = database.getName();
-			if (database.getSchemas().size() > 0) {
-				Schema schema = database.getSchemas().get(0);
-				folderName = schema.getName();
-			}
-			folderName += "-";
 		}
-		java.sql.Timestamp timeStampDate = new Timestamp(System.currentTimeMillis());
-		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd/HH-mm-ss");
-		String timestamp = formatter.format(timeStampDate);
-		folderName += timestamp;
 
 		File targetFolder = new File(folder.getAbsolutePath() + "/" + dbtypeFolderName + folderName);
+
+		if (!targetFolder.exists()) {
+			if (!targetFolder.mkdirs()) {
+				throw new IOException("Unable to create folder at " + targetFolder);
+			}
+		}
+		return targetFolder;
+	}
+
+	private File computeTargetFolder(File folder, Comparison comparison) throws IOException {
+		String folderName = "";
+		String dbtypeFolderName = "";
+		if (comparison != null) {
+			DataBase database = (DataBase) comparison.getMatches().get(0).getLeft();
+
+			TypesLibrary physicalTypesLibrary = new TypesServices().getPhysicalTypesLibrary(database);
+			if (physicalTypesLibrary instanceof NativeTypesLibrary) {
+				dbtypeFolderName += ((NativeTypesLibrary) physicalTypesLibrary).getName() + "/";
+			} else if (physicalTypesLibrary instanceof UserDefinedTypesLibrary) {
+				dbtypeFolderName += ((UserDefinedTypesLibrary) physicalTypesLibrary).getName() + "/";
+			}
+
+			folderName = database.getName();
+		}
+		java.sql.Timestamp timeStampDate = new Timestamp(System.currentTimeMillis());
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
+		String timestamp = formatter.format(timeStampDate);
+		folderName += "/" + timestamp;
+
+		File targetFolder = new File(folder.getAbsolutePath() + "/" + dbtypeFolderName + folderName);
+
+		if (!targetFolder.exists()) {
+			if (!targetFolder.mkdirs()) {
+				throw new IOException("Unable to create folder at " + targetFolder);
+			}
+		}
+
 		return targetFolder;
 	}
 
