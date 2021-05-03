aaa191ed0d8c93deeb90d319e74c497066ccd5de
Vincent Richard <vincent.richard@obeo.fr>
2021-05-03 18:32:27 +0200

    [evolCinematic2021] View Container Diagram labels

---
+++ designs/cinematic/plugins/org.obeonetwork.dsl.cinematic.design/src/org/obeonetwork/dsl/cinematic/design/services/CinematicVCDLabelServices.java
@@ -0,0 +1,54 @@
+package org.obeonetwork.dsl.cinematic.design.services;
+
+import org.obeonetwork.dsl.cinematic.view.ViewContainer;
+import org.obeonetwork.dsl.cinematic.view.ViewContainerReference;
+import org.obeonetwork.dsl.cinematic.view.ViewElement;
+import org.obeonetwork.utils.common.StringUtils;
+
+public class CinematicVCDLabelServices {
+
+	public static String getVCDLabel(ViewElement viewElement) {
+		
+		if(!StringUtils.isNullOrWhite(viewElement.getExample())) {
+			return viewElement.getExample();
+		}
+		
+		if(!StringUtils.isNullOrWhite(viewElement.getLabel())) {
+			return viewElement.getLabel();
+		}
+		
+		if(!StringUtils.isNullOrWhite(viewElement.getName())) {
+			return viewElement.getName();
+		}
+		
+		return StringUtils.EMPTY_STRING;
+		
+	}
+	
+	public static String getVCDLabel(ViewContainer viewContainer) {
+		
+		if(!StringUtils.isNullOrWhite(viewContainer.getLabel())) {
+			return viewContainer.getLabel();
+		}
+		
+		if(!StringUtils.isNullOrWhite(viewContainer.getName())) {
+			return viewContainer.getName();
+		}
+		
+		return StringUtils.EMPTY_STRING;
+	}
+	
+	public static String getVCDLabel(ViewContainerReference viewContainerReference) {
+		
+		if(!StringUtils.isNullOrWhite(viewContainerReference.getLabel())) {
+			return viewContainerReference.getLabel();
+		}
+		
+		if(!StringUtils.isNullOrWhite(viewContainerReference.getName())) {
+			return viewContainerReference.getName();
+		}
+		
+		return getVCDLabel(viewContainerReference.getViewContainer());
+	}
+	
+}
