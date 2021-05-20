50fc658cc007a1944d18c92de7719fad31f5c608
Thibault Beziers la Fosse <thibault.beziers-la-fosse@obeo.fr>
2021-05-20 14:35:19 +0200

    [SAFRAN-711] Updated the tabular view for entity metadatas

--- addons/structuredtypes/plugins/fr.gouv.mindef.safran.is.design/src/fr/gouv/mindef/safran/is/design/services/AnnotationServices.java
+++ addons/structuredtypes/plugins/fr.gouv.mindef.safran.is.design/src/fr/gouv/mindef/safran/is/design/services/AnnotationServices.java
@@ -10,13 +10,25 @@
  *******************************************************************************/
 package fr.gouv.mindef.safran.is.design.services;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.net4j.util.StringUtil;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.dialogs.ListDialog;
+import org.obeonetwork.dsl.entity.Entity;
 import org.obeonetwork.dsl.environment.Annotation;
 import org.obeonetwork.dsl.environment.Attribute;
 import org.obeonetwork.dsl.environment.EnvironmentFactory;
 import org.obeonetwork.dsl.environment.MetaData;
 import org.obeonetwork.dsl.environment.MetaDataContainer;
+import org.obeonetwork.dsl.environment.Namespace;
 import org.obeonetwork.dsl.environment.ObeoDSMObject;
+import org.obeonetwork.dsl.environment.Reference;
 
 /**
  * Utilities services concerning Annotations
@@ -30,6 +42,7 @@ public class AnnotationServices {
 	private static final String PHYSICAL_CHECK = "PHYSICAL_CHECK";
 	private static final String PHYSICAL_UNIQUE = "PHYSICAL_UNIQUE";
 	private static final String PHYSICAL_DEFAULT = "PHYSICAL_DEFAULT";
+	private static final String PHYSICAL_TARGET = "PHYSICAL_TARGET";
 
 	/**
 	 * Returns true if an attribute is in a context allowing the user to edit label
@@ -94,6 +107,16 @@ public class AnnotationServices {
 		return getAnnotation(context, PHYSICAL_DEFAULT);
 	}
 
+	/**
+	 * Returns the physical target value of an object
+	 * This physical target is persisted as an annotation
+	 * @param context Object for which we look for the physical target value
+	 * @return the physical target or null if there is none specified for the context object
+	 */
+	public String getPhysicalTarget(ObeoDSMObject context) {	
+		return getAnnotation(context, PHYSICAL_TARGET);
+	}
+	
 	/**
 	 * Returns the annotation with the specified name on the context object 
 	 * @param context Object for which we look for an annotation
@@ -163,6 +186,88 @@ public class AnnotationServices {
 	 */
 	public void setPhysicalDefault(ObeoDSMObject context, String physicalDefault) {
 		setAnnotation(context, PHYSICAL_DEFAULT, physicalDefault);
+	}
+	
+	/**
+	 * Sets the physical target of an object as an annotation
+	 * @param context Object for which we want to set the physical target
+	 * @param physicalTarget New value for the physical target
+	 */
+	public void setPhysicalTarget(ObeoDSMObject context, String physicalTarget) {
+		
+		if (context instanceof Reference) {
+			
+			// put a X in the cell
+			if (physicalTarget != null && !physicalTarget.isEmpty()) {
+				if (physicalTarget.toLowerCase().trim().contains("x")) {
+					physicalTarget = "X";
+					
+					Reference reference = (Reference) context;
+					
+					// Is the opposed reference defined as the physical target ? 
+					Optional<Annotation> physicalTargetAnnotation = reference.getOppositeOf()
+						.getMetadatas() // metadatacontainer
+						.getMetadatas() // list<metadata>
+						.stream()
+						.filter(Annotation.class::isInstance)
+						.map(Annotation.class::cast)
+						.filter(annotation -> PHYSICAL_TARGET.equals(annotation.getTitle()))
+						.findAny();
+					
+					if (physicalTargetAnnotation.isPresent())
+						physicalTargetAnnotation.get().setBody(null);
+					
+				} else {
+					physicalTarget = null;
+					switchReferenceTarget((Reference) context);
+				}							
+			} else {
+				// removed the cell's content: we check the opposite Reference instead				
+				switchReferenceTarget((Reference) context);				
+			}
+		}		
+		setAnnotation(context, PHYSICAL_TARGET, physicalTarget);
+	}	
+	
+	private void switchReferenceTarget(Reference reference) {
+		Optional<Annotation> physicalTargetAnnotation = reference.getOppositeOf()
+			.getMetadatas() // metadatacontainer
+			.getMetadatas() // list<metadata>
+			.stream()
+			.filter(Annotation.class::isInstance)
+			.map(Annotation.class::cast)
+			.filter(annotation -> PHYSICAL_TARGET.equals(annotation.getTitle()))
+			.findAny();
+		
+		if (physicalTargetAnnotation.isPresent())
+			physicalTargetAnnotation.get().setBody("X");
+		else {
+			Reference opposite = reference.getOppositeOf();
+			Annotation annotation = EnvironmentFactory.eINSTANCE.createAnnotation();
+			annotation.setTitle(PHYSICAL_TARGET);
+			annotation.setBody("X");
+			opposite.getMetadatas().getMetadatas().add(annotation);
+		}					
+	}
+	
+	public boolean isValidReference(Reference reference) {
+		String physicalTarget = getPhysicalTarget(reference);
+		String physicalTargetOpposite = getPhysicalTarget(reference.getOppositeOf());
+		
+		if (physicalTarget != null) {
+			if (! physicalTarget.isEmpty()) {
+				if (physicalTargetOpposite != null && ! physicalTargetOpposite.isEmpty()) {
+					return false;
+				}
+				
+				if (! "x".equalsIgnoreCase(physicalTarget)) {
+					return false;
+				}
+			}
+			
+		}
+		
+		return true;
 	}
 	
 	/**
