5dc80273bc5a092f730adee0f666aa09012bc291
Thibault Beziers la Fosse <thibault.beziers-la-fosse@obeo.fr>
2021-06-22 12:02:01 +0200

    [evolCinematic2021] Updated the model creation wizard

--- designs/cinematic/plugins/org.obeonetwork.dsl.cinematic.design/src/org/obeonetwork/dsl/cinematic/design/wizards/NewCinematicModelWizard.java
+++ designs/cinematic/plugins/org.obeonetwork.dsl.cinematic.design/src/org/obeonetwork/dsl/cinematic/design/wizards/NewCinematicModelWizard.java
@@ -27,6 +27,9 @@ import org.obeonetwork.dsl.cinematic.CinematicFactory;
 import org.obeonetwork.dsl.cinematic.CinematicPackage;
 import org.obeonetwork.dsl.cinematic.CinematicRoot;
 import org.obeonetwork.dsl.cinematic.design.Activator;
+import org.obeonetwork.dsl.cinematic.flow.Flow;
+import org.obeonetwork.dsl.cinematic.flow.FlowFactory;
+import org.obeonetwork.dsl.cinematic.flow.FlowPackage;
 import org.obeonetwork.dsl.cinematic.toolkits.Widget;
 import org.obeonetwork.dsl.cinematic.view.ViewContainer;
 import org.obeonetwork.dsl.cinematic.view.ViewFactory;
@@ -34,56 +37,67 @@ import org.obeonetwork.dsl.cinematic.view.ViewPackage;
 import org.obeonetwork.dsl.is.ui.wizards.AbstractISNewModelWizard;
 import org.obeonetwork.dsl.is.ui.wizards.NewModelCreationPage;
 
-
 public class NewCinematicModelWizard extends AbstractISNewModelWizard {
-	
+
 	private static final String DESC_ID_PACKAGE_DIAGRAM = "Package Diagram";
 	private static final String DESC_ID_MOCKUP_DIAGRAM = "View Container Mockup";
+	private static final String DESC_ID_FLOW_DIAGRAM = "Flow Diagram";
 	private static final String DESC_ID_UI_STRUCTURE_TREE = "UI Structure";
 	private static final String CINEMATIC_RESOURCE_FILE_EXTENSION = ".cinematic";
-	
+
 	private NewModelCreationWithMockupPage mockupPage;
-	
+
 	public NewCinematicModelWizard() {
-		super("New Cinematic model", Activator.imageDescriptorFromPlugin(Activator.PLUGIN_ID, "icons/full/wizban/NewCinematic.gif"));
+		super("New Cinematic model",
+				Activator.imageDescriptorFromPlugin(Activator.PLUGIN_ID, "icons/full/wizban/NewCinematic.gif"));
 	}
 
 	@Override
 	protected Collection<EObject> createInitialObjects() {
 		Collection<EObject> initialObjects = new ArrayList<EObject>();
-		
+
 		CinematicRoot cinematicRoot = CinematicFactory.eINSTANCE.createCinematicRoot();
 		cinematicRoot.setCreatedOn(new Date());
 		cinematicRoot.setName(getProjectName());
 		initialObjects.add(cinematicRoot);
-		
-		if (mockupPage.isCreatesMockupRepresentation() && mockupPage.getToolkit() != null && mockupPage.getWidgetContainer() != null) {
+
+		if (mockupPage.isCreatesMockupRepresentation() && mockupPage.getToolkit() != null
+				&& mockupPage.getWidgetContainer() != null) {
 			Widget widget = mockupPage.getWidgetContainer();
 			ViewContainer container = ViewFactory.eINSTANCE.createViewContainer();
 			container.setWidget(widget);
-			
+
 			if (mockupPage.getViewContainerName() != null)
 				container.setName(mockupPage.getViewContainerName());
-			else 
+			else
 				container.setName(String.format("%s_1", widget.getName()));
-				
+
 			cinematicRoot.getViewContainers().add(container);
 			cinematicRoot.getToolkits().add(mockupPage.getToolkit());
-			
+
 			initialObjects.add(container);
 		}
-		
+
+		if (mockupPage.isCreatesFlowRepresentation()) {
+			Flow flow = FlowFactory.eINSTANCE.createFlow();
+			if (mockupPage.getFlowName() != null) {
+				flow.setName(mockupPage.getFlowName());
+			}
+			cinematicRoot.getFlows().add(flow);
+			initialObjects.add(flow);
+		}
+
 		return initialObjects;
 	}
-	
+
 	@Override
 	public void addPages() {
 		modelCreationPage = new NewModelCreationPage("ModelCreationPage", selection, CINEMATIC_RESOURCE_FILE_EXTENSION);
 		modelCreationPage.setTitle("Create a new cinematic model");
 		modelCreationPage.setDescription("Choose the new cinematic model name and path.");
-		
+
 		mockupPage = new NewModelCreationWithMockupPage("ModelCreationMockup");
-		
+
 		addPage(modelCreationPage);
 		addPage(mockupPage);
 	}
@@ -92,23 +106,23 @@ public class NewCinematicModelWizard extends AbstractISNewModelWizard {
 	protected String getWizardTitle() {
 		return "New Cinematic model";
 	}
-	
+
 	@Override
 	protected Collection<URI> getViewpointsURIToBeActivated() {
-		return Arrays.asList(
-				URI.createURI("viewpoint:/org.obeonetwork.dsl.environment.properties/Environment Views"),
-				URI.createURI("viewpoint:/org.obeonetwork.dsl.cinematic.design/Cinematic Views")
-		);
+		return Arrays.asList(URI.createURI("viewpoint:/org.obeonetwork.dsl.environment.properties/Environment Views"),
+				URI.createURI("viewpoint:/org.obeonetwork.dsl.cinematic.design/Cinematic Views"));
 	}
-	
+
 	@Override
 	protected Map<EClassifier, Collection<String>> getRepresentationDescriptionsIDToBeCreated() {
 		Map<EClassifier, Collection<String>> descs = new HashMap<>();
-		descs.put(CinematicPackage.Literals.CINEMATIC_ROOT, Arrays.asList(DESC_ID_UI_STRUCTURE_TREE, DESC_ID_PACKAGE_DIAGRAM));
+		descs.put(CinematicPackage.Literals.CINEMATIC_ROOT,
+				Arrays.asList(DESC_ID_UI_STRUCTURE_TREE, DESC_ID_PACKAGE_DIAGRAM));
 		descs.put(ViewPackage.Literals.VIEW_CONTAINER, Arrays.asList(DESC_ID_MOCKUP_DIAGRAM));
+		descs.put(FlowPackage.Literals.FLOW, Arrays.asList(DESC_ID_FLOW_DIAGRAM));
 		return descs;
 	}
-	
+
 	@Override
 	protected String getRepresentationName(RepresentationDescription representationDescription, EObject object) {
 		String repDescName = representationDescription.getName();
@@ -118,27 +132,28 @@ public class NewCinematicModelWizard extends AbstractISNewModelWizard {
 				return String.format("%1$s %2$s", this.getProjectName(), DESC_ID_PACKAGE_DIAGRAM);
 			} else if (DESC_ID_UI_STRUCTURE_TREE.equals(repDescName)) {
 				return String.format("%1$s %2$s", this.getProjectName(), DESC_ID_UI_STRUCTURE_TREE);
-			}  
-		} else if (object instanceof ViewContainer) {
-			if (DESC_ID_MOCKUP_DIAGRAM.equals(repDescName)) {
-				return String.format("%1$s %2$s", this.getProjectName(), DESC_ID_MOCKUP_DIAGRAM);
 			}
+		} else if (object instanceof ViewContainer && DESC_ID_MOCKUP_DIAGRAM.equals(repDescName)) {
+			return String.format("%1$s %2$s", this.getProjectName(), DESC_ID_MOCKUP_DIAGRAM);
+		} else if (object instanceof Flow && DESC_ID_FLOW_DIAGRAM.equals(repDescName)) {
+			return String.format("%1$s %2$s", this.getProjectName(), DESC_ID_FLOW_DIAGRAM);
 		}
 		return null;
 	}
-	
+
 	@Override
 	protected boolean shouldOpenRepresentation(DRepresentation representation) {
 		if (representation instanceof DDiagram) {
 			String repDescName = ((DDiagram) representation).getDescription().getName();
-			if (mockupPage.isCreatesMockupRepresentation()) {
-				if (DESC_ID_MOCKUP_DIAGRAM.equals(repDescName)) {
-					return true;
-				} else return false;								
-			} else return DESC_ID_PACKAGE_DIAGRAM.equals(repDescName);
+			if (mockupPage.isCreatesMockupRepresentation() && (DESC_ID_MOCKUP_DIAGRAM.equals(repDescName))) {
+				return true;
+			} else if (mockupPage.isCreatesFlowRepresentation() && (DESC_ID_FLOW_DIAGRAM.equals(repDescName))) {
+				return true;
+			} else
+				return DESC_ID_PACKAGE_DIAGRAM.equals(repDescName) && !mockupPage.isCreatesMockupRepresentation()
+						&& !mockupPage.isCreatesFlowRepresentation();
 		}
 		return false;
 	}
-	
-	
+
 }
--- designs/cinematic/plugins/org.obeonetwork.dsl.cinematic.design/src/org/obeonetwork/dsl/cinematic/design/wizards/NewModelCreationWithMockupPage.java
+++ designs/cinematic/plugins/org.obeonetwork.dsl.cinematic.design/src/org/obeonetwork/dsl/cinematic/design/wizards/NewModelCreationWithMockupPage.java
@@ -1,19 +1,20 @@
 package org.obeonetwork.dsl.cinematic.design.wizards;
 
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.ModifyEvent;
-import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
@@ -21,162 +22,229 @@ import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
-import org.obeonetwork.dsl.cinematic.toolkits.Style;
 import org.obeonetwork.dsl.cinematic.toolkits.Toolkit;
 import org.obeonetwork.dsl.cinematic.toolkits.Widget;
 import org.obeonetwork.dsl.cinematic.toolkits.util.ToolkitsProvider;
 import org.obeonetwork.dsl.cinematic.view.ViewContainer;
 import org.obeonetwork.utils.common.StringUtils;
+import org.osgi.framework.Bundle;
 
 /**
- * Defines the main {@link ViewContainer} of a Cinematic model, creates and opens its representation.
- * @author <a href="mailto:thibault.beziers-la-fosse@obeo.fr">Thibault Béziers la Fosse</a>
+ * Defines the main {@link ViewContainer} of a Cinematic model, creates and
+ * opens its representations.
+ * 
+ * @author <a href="mailto:thibault.beziers-la-fosse@obeo.fr">Thibault Béziers
+ *         la Fosse</a>
  */
 public class NewModelCreationWithMockupPage extends WizardPage {
 	private Toolkit toolkit;
 	private Widget widgetContainer;
-	
+
 	private List<Toolkit> toolkits = new ArrayList<>();
 	private List<Widget> widgetContainers;
-	private Text text;
+	private Text containerName;
+	private Text flowName;
 	protected boolean createsMockupRepresentation;
-	
+	protected boolean createsFlowRepresentation;
+
 	public NewModelCreationWithMockupPage(String pageName) {
 		super(pageName);
-		setTitle("Create a new mockup");
-		setDescription("Choose the main container and its name");
-	}
+
+		setTitle("Create a new Cinematic model");
+		setDescription("Choose Mockup and Flow settings");
+		Bundle bundle = Platform.getBundle("org.obeonetwork.dsl.cinematic.design");
+		URL fullPathString = bundle.getEntry("icons/full/wizban/NewMockup.gif");
+		setImageDescriptor(ImageDescriptor.createFromURL(fullPathString));
+	};
 
 	@Override
 	public void createControl(Composite parent) {
-		Composite compositeMockup = new Composite(parent, SWT.NONE);		
+		Composite compositeMockup = new Composite(parent, SWT.NONE);
 		setControl(compositeMockup);
-		
+
 		compositeMockup.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 2, 4));
-		compositeMockup.setLayout(new GridLayout(2, false));		
-	
-		Label toolkitLabel = new Label(compositeMockup, NONE);
-		toolkitLabel.setText("Toolkit:");				
-		
-		Combo toolkitCombo = new Combo(compositeMockup, SWT.READ_ONLY);
-		GridData gd_toolkitCombo = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
-		gd_toolkitCombo.widthHint = 220;
-		toolkitCombo.setLayoutData(gd_toolkitCombo);					
-		
-		new Label(compositeMockup, SWT.NONE);
-		new Label(compositeMockup, SWT.NONE);
-		
-		Label vcLabel = new Label(compositeMockup, NONE);
+		GridLayout gl_compositeMockup = new GridLayout(1, false);
+		gl_compositeMockup.marginHeight = 10;
+		gl_compositeMockup.marginWidth = 0;
+		gl_compositeMockup.verticalSpacing = 0;
+		gl_compositeMockup.horizontalSpacing = 0;
+		compositeMockup.setLayout(gl_compositeMockup);
+
+		Composite composite = new Composite(compositeMockup, SWT.NONE);
+		composite.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, true, 1, 1));
+		composite.setLayout(new GridLayout(2, false));
+
+		Label toolkitLabel = new Label(composite, NONE);
+		GridData gd_toolkitLabel = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+		gd_toolkitLabel.widthHint = 130;
+		toolkitLabel.setLayoutData(gd_toolkitLabel);
+		toolkitLabel.setText("Toolkit:");
+
+		Combo toolkitCombo = new Combo(composite, SWT.READ_ONLY);
+		toolkitCombo.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+
+		Group grpMockupSettings = new Group(compositeMockup, SWT.NONE);
+		grpMockupSettings.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, true, 1, 1));
+		grpMockupSettings.setText("Mockup settings");
+		GridLayout gl_grpMockupSettings = new GridLayout(2, false);
+		grpMockupSettings.setLayout(gl_grpMockupSettings);
+
+		Button initializeWithMockup = new Button(grpMockupSettings, SWT.CHECK);
+		initializeWithMockup.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 2, 1));
+		initializeWithMockup.setText("Create mockup diagram");
+		initializeWithMockup.setSelection(false);
+
+		Label vcLabel = new Label(grpMockupSettings, NONE);
+		GridData gd_vcLabel = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+		gd_vcLabel.widthHint = 130;
+		vcLabel.setLayoutData(gd_vcLabel);
 		vcLabel.setText("Main container:");
-		Combo vcContainerCombo = new Combo(compositeMockup, SWT.READ_ONLY);
-		GridData gd_vcContainerCombo = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
-		gd_vcContainerCombo.widthHint = 220;
-		vcContainerCombo.setLayoutData(gd_vcContainerCombo);					
-		
-		new Label(compositeMockup, SWT.NONE);
-		new Label(compositeMockup, SWT.NONE);
-		
-		Label lblWidgetName = new Label(compositeMockup, SWT.NONE);
+
+		Combo vcContainerCombo = new Combo(grpMockupSettings, SWT.READ_ONLY);
+		vcContainerCombo.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+		vcContainerCombo.setEnabled(false);
+
+		Label lblWidgetName = new Label(grpMockupSettings, SWT.NONE);
+		GridData gd_lblWidgetName = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+		gd_lblWidgetName.widthHint = 130;
+		lblWidgetName.setLayoutData(gd_lblWidgetName);
 		lblWidgetName.setText("Container name:");
-		text = new Text(compositeMockup, SWT.BORDER);
-		GridData gd_text = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
-		gd_text.heightHint = 20;
-		gd_text.widthHint = 233;
-		text.setLayoutData(gd_text);		
-		
-		
+
+		containerName = new Text(grpMockupSettings, SWT.BORDER);
+		containerName.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, false, 1, 1));
+		containerName.addModifyListener(event -> getContainer().updateButtons());
+		containerName.setEnabled(false);
+
+		Group grpFlowSettings = new Group(compositeMockup, SWT.SHADOW_ETCHED_IN);
+		grpFlowSettings.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, true, 1, 1));
+		grpFlowSettings.setLayout(new GridLayout(2, false));
+		grpFlowSettings.setText("Flow settings");
+
+		Button initializeWithFlow = new Button(grpFlowSettings, SWT.CHECK);
+		initializeWithFlow.setText("Create flow diagram");
+		new Label(grpFlowSettings, SWT.NONE);
+
+		Label lblNewLabel = new Label(grpFlowSettings, SWT.NONE);
+		GridData gd_lblNewLabel = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+		gd_lblNewLabel.widthHint = 130;
+		lblNewLabel.setLayoutData(gd_lblNewLabel);
+		lblNewLabel.setText("Flow name:");
+
+		flowName = new Text(grpFlowSettings, SWT.BORDER | SWT.WRAP);
+		flowName.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		flowName.addModifyListener(event -> getContainer().updateButtons());
+		flowName.setEnabled(false);
+
+		initializeWithFlow.addSelectionListener(new SelectionAdapter() {
+
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				createsFlowRepresentation = ((Button) e.getSource()).getSelection();
+				flowName.setEnabled(createsFlowRepresentation);
+
+				if (createsFlowRepresentation) {
+					flowName.setText("Flow");
+				} else {
+					flowName.setText("");
+				}
+			}
+
+		});
+
 		toolkitCombo.addSelectionListener(new SelectionAdapter() {
 
 			@Override
 			public void widgetSelected(SelectionEvent e) {
 				toolkit = toolkits.get(toolkitCombo.getSelectionIndex());
-				widgetContainers = toolkit.getWidgets().stream().filter(Widget::isIsContainer).collect(Collectors.toList());
-				
-				// sorting the containers from the largest to the smallest.
-				widgetContainers.sort((w1, w2) -> {
-					
-					int areaStyle1 = w1.getStyle() == null ? 0 : w1.getStyle().getDefaultHeight() * w1.getStyle().getDefaultWidth();
-					int areaStyle2 = w2.getStyle() == null ? 0 : w2.getStyle().getDefaultHeight() * w2.getStyle().getDefaultWidth();
-					
-					return areaStyle2 - areaStyle1;	
-				}); 
-				
-				vcContainerCombo.setItems((String[]) widgetContainers.stream().map(Widget::getName).collect(Collectors.toList()).toArray(new String[widgetContainers.size()]));
-				vcContainerCombo.select(0);
-				vcContainerCombo.notifyListeners(SWT.Selection, null);				
+				initializeWithMockup.notifyListeners(SWT.Selection, null);
 			}
-			
+
 		});
-		
+
 		vcContainerCombo.addSelectionListener(new SelectionAdapter() {
 
 			@Override
 			public void widgetSelected(SelectionEvent e) {
 				if (vcContainerCombo.getSelectionIndex() != -1) {
 					widgetContainer = widgetContainers.get(vcContainerCombo.getSelectionIndex());
-					text.setText(String.format("%s%s", toolkit.getName(), widgetContainer.getName()));
-				}	
+					containerName.setText(String.format("%s%s", toolkit.getName(), widgetContainer.getName()));
+				}
 			}
 		});
-		
-		text.addModifyListener( event -> getContainer().updateButtons());
-		toolkits = getToolkits(); // Get toolkits provided using the extension point
-		toolkits.sort((t1, t2) -> t1.getName().compareTo(t2.getName()));
-		
-		new Label(compositeMockup, SWT.NONE);
-		new Label(compositeMockup, SWT.NONE);
-		
-		Button initializeWithMockup = new Button(compositeMockup, SWT.CHECK);
-		initializeWithMockup.setText("Create layout for mockup");
-		initializeWithMockup.setSelection(false);
+
 		initializeWithMockup.addSelectionListener(new SelectionAdapter() {
-			
+
 			@Override
 			public void widgetSelected(SelectionEvent event) {
-				
-	            Button btn = (Button) event.getSource();
-	            createsMockupRepresentation = btn.getSelection();
-	            if (createsMockupRepresentation) {
-	        		
-	        		toolkitCombo.setItems(toolkits.stream().map(t -> t.getName()).collect(Collectors.toList()).toArray(new String[toolkits.size()]));		
-	        		toolkitCombo.select(0); 
-	        		toolkitCombo.notifyListeners(SWT.Selection, null);
-	            } else {
-	            	toolkitCombo.setItems();
-	            	vcContainerCombo.setItems();
-	            	text.setText("");
-	            }
-	            
-	            getContainer().updateButtons();
+
+				Button btn = (Button) event.getSource();
+				createsMockupRepresentation = btn.getSelection();
+				containerName.setEnabled(createsMockupRepresentation);
+
+				if (createsMockupRepresentation && toolkit != null) {
+					widgetContainers = toolkit.getWidgets().stream().filter(Widget::isIsContainer)
+							.collect(Collectors.toList());
+					widgetContainers.sort((w1, w2) -> {
+						// sorting the containers from the largest to the smallest.
+						int areaStyle1 = w1.getStyle() == null ? 0
+								: w1.getStyle().getDefaultHeight() * w1.getStyle().getDefaultWidth();
+						int areaStyle2 = w2.getStyle() == null ? 0
+								: w2.getStyle().getDefaultHeight() * w2.getStyle().getDefaultWidth();
+
+						return areaStyle2 - areaStyle1;
+					});
+
+					vcContainerCombo.setEnabled(true);
+					vcContainerCombo.setItems((String[]) widgetContainers.stream().map(Widget::getName)
+							.collect(Collectors.toList()).toArray(new String[widgetContainers.size()]));
+					vcContainerCombo.select(0);
+					vcContainerCombo.notifyListeners(SWT.Selection, null);
+
+				} else {
+					vcContainerCombo.setItems();
+					vcContainerCombo.setEnabled(false);
+					containerName.setText("");
+				}
+
+				getContainer().updateButtons();
 			}
 		});
-		
-		new Label(compositeMockup, SWT.NONE);
-		
+
+		toolkits = getToolkits(); // Get toolkits provided using the extension point
+		toolkits.sort((t1, t2) -> t1.getName().compareTo(t2.getName()));
+		toolkitCombo.setItems(toolkits.stream().map(t -> t.getName()).distinct().collect(Collectors.toList())
+				.toArray(new String[toolkits.size()]));
+
+		Composite composite_1 = new Composite(compositeMockup, SWT.NONE);
+		GridData gd_composite_1 = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+		gd_composite_1.heightHint = 34;
+		composite_1.setLayoutData(gd_composite_1);
+		toolkitCombo.redraw();
 	}
-	
+
 	public List<Toolkit> getToolkits() {
 		Collection<URI> providedToolkitsURI = ToolkitsProvider.getProvidedToolkits();
 		List<Toolkit> toolkitsCollection = new ArrayList<>();
-		
+
 		for (URI uri : providedToolkitsURI) {
 			ResourceSet set = new ResourceSetImpl();
 			Resource resource = set.getResource(uri, true);
 			if (resource != null && resource.getContents() != null) {
 				for (EObject root : resource.getContents()) {
 					if (root instanceof Toolkit) {
-						Toolkit toolkit = (Toolkit)root;
+						Toolkit toolkit = (Toolkit) root;
 						toolkitsCollection.add(toolkit);
 					}
 				}
 			}
 		}
-		
-		return toolkitsCollection;		
+
+		return toolkitsCollection;
 	}
-	
+
 	public Toolkit getToolkit() {
 		return toolkit;
 	}
@@ -186,24 +254,29 @@ public class NewModelCreationWithMockupPage extends WizardPage {
 	}
 
 	public String getViewContainerName() {
-		return text.getText();
+		return containerName.getText();
 	}
 
-	
+	public String getFlowName() {
+		return flowName.getText();
+	}
+
 	public boolean isCreatesMockupRepresentation() {
 		return createsMockupRepresentation;
 	}
 
-	@Override
-	public boolean isPageComplete() {			
-		return !createsMockupRepresentation || 
-				(createsMockupRepresentation 
-					&& toolkit != null 
-					&& widgetContainer != null 
-					&& (!StringUtils.isNullOrWhite(getViewContainerName())));
+	public boolean isCreatesFlowRepresentation() {
+		return createsFlowRepresentation;
 	}
-	
-	
-	
+
+	@Override
+	public boolean isPageComplete() {
+
+		boolean isMockupValid = !createsMockupRepresentation
+				|| (toolkit != null && widgetContainer != null && (!StringUtils.isNullOrWhite(getViewContainerName())));
+
+		boolean isFlowValid = !createsFlowRepresentation || !StringUtils.isNullOrWhite(getFlowName());
+
+		return isMockupValid && isFlowValid;
+	}
 }
- 
