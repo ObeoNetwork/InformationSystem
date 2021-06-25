19c9421fc49d0a1a7d68a69d5ca1e1d70ca7149d
Thibault Beziers la Fosse <thibault.beziers-la-fosse@obeo.fr>
2021-06-25 11:18:51 +0200

    [SAFRAN-936] View Properties for SOA updated to display the new MM elements

--- designs/soa/plugins/org.obeonetwork.dsl.soa.design/src/org/obeonetwork/dsl/soa/design/services/SOAService.java
+++ designs/soa/plugins/org.obeonetwork.dsl.soa.design/src/org/obeonetwork/dsl/soa/design/services/SOAService.java
@@ -47,80 +47,88 @@ import org.obeonetwork.dsl.soa.Wire;
 import org.obeonetwork.dsl.soa.services.HttpStatusService;
 
 public class SOAService {
-	
+
+	// https://stackoverflow.com/questions/163360/regular-expression-to-match-urls-in-java
+	private static final Pattern URL_PARAM_PATTERN = Pattern.compile(
+			"^((https?|ftp|file)://)[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]", Pattern.CASE_INSENSITIVE);
+	// https://stackoverflow.com/questions/8204680/java-regex-email 
+	// compliant with RFC822 and accepts IP address and server names for intranet purposes.
+	private static final Pattern EMAIL_PARAM_PATTERN = Pattern.compile(
+			"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?",
+			Pattern.CASE_INSENSITIVE);
+	private static final Pattern PATH_PARAM_PATTERN = Pattern.compile("\\{[^\\{\\}]*\\}");
+
 	public EObject trace(EObject receiver) {
 		java.lang.System.out.println("receiver.eClass() = \"" + receiver.eClass().getName() + "\"");
 		java.lang.System.out.println("receiver.toString() = \"" + receiver.toString() + "\"");
-		java.lang.System.out.println("receiver.name = \"" + 
-			Optional.ofNullable(receiver.eClass().getEStructuralFeature("name"))
-			.map(f -> (String)receiver.eGet(f)).orElse(null) + "\"");
-		
+		java.lang.System.out
+				.println("receiver.name = \"" + Optional.ofNullable(receiver.eClass().getEStructuralFeature("name"))
+						.map(f -> (String) receiver.eGet(f)).orElse(null) + "\"");
+
 		return receiver;
 	}
-	
+
 	public EObject traceVars(EObject o) {
 		trace(o);
 		Session session = new EObjectQuery(o).getSession();
 		Map<String, ?> vars = session.getInterpreter().getVariables();
 		java.lang.System.out.println(vars.keySet().size() + " variable(s)");
-		for(String var : vars.keySet()) {
+		for (String var : vars.keySet()) {
 			java.lang.System.out.println(var + " = " + vars.get(var));
 		}
 		return o;
 	}
-	
+
 	public Operation setExpositionKind(Operation operation, ExpositionKind expositionKind) {
-		
+
 		operation.setExposition(expositionKind);
-		if(expositionKind == ExpositionKind.NONE) {
+		if (expositionKind == ExpositionKind.NONE) {
 			operation.setURI(null);
 			operation.setVerb(Verb.GET);
 			operation.setPaged(true);
 		}
-		
+
 		return operation;
 	}
-	
+
 	public Parameter setStatusCode(Parameter parameter, String statusCode) {
-		if(!Objects.equals(parameter.getStatusCode(), statusCode)) {
-			if(isNullOrWhite(parameter.getStatusMessage()) || 
-				parameter.getStatusMessage().trim().equals(HttpStatusService.getHttpMessage(parameter.getStatusCode()))) {
+		if (!Objects.equals(parameter.getStatusCode(), statusCode)) {
+			if (isNullOrWhite(parameter.getStatusMessage()) || parameter.getStatusMessage().trim()
+					.equals(HttpStatusService.getHttpMessage(parameter.getStatusCode()))) {
 				parameter.setStatusMessage(HttpStatusService.getHttpMessage(statusCode));
 			}
 			parameter.setStatusCode(statusCode);
 		}
 		return parameter;
 	}
-	
-	public List<Component> allNonReferencedExternalComponents (System context) {
+
+	public List<Component> allNonReferencedExternalComponents(System context) {
 		List<Component> allNonReferencedExternalComponents = allExternalComponents(context);
 		// Remove all referenced external components
-		allNonReferencedExternalComponents
-				.removeAll(allReferencedExternalComponents(context));
+		allNonReferencedExternalComponents.removeAll(allReferencedExternalComponents(context));
 		return allNonReferencedExternalComponents;
 	}
-	
+
 	public List<Component> allExternalComponents(System context) {
 		List<Component> allExternalComponents = allComponents(context);
 		// Remove The component contained in the System.
 		allExternalComponents.removeAll(context.getOwnedComponents());
 		return allExternalComponents;
 	}
-	
+
 	public List<Component> allReferencedExternalComponents(System context) {
 		List<Component> allReferencedExternelComponents = allReferencedComponents(context);
 		// Remove The component contained in the System.
 		allReferencedExternelComponents.removeAll(context.getOwnedComponents());
 		return allReferencedExternelComponents;
 	}
-	
-	public List<Component> allReferencedComponents(System  context) {
+
+	public List<Component> allReferencedComponents(System context) {
 		List<Component> allReferencedComponents = new ArrayList<Component>();
 		List<Wire> ownedWires = context.getOwnedWires();
 		// Add Components parent of source and dest of westWire
 		for (Wire wire : ownedWires) {
-			Component componentSource = (Component) wire.getSource()
-					.eContainer();
+			Component componentSource = (Component) wire.getSource().eContainer();
 			allReferencedComponents.add(componentSource);
 			Component componentDest = (Component) wire.getDest().eContainer();
 			allReferencedComponents.add(componentDest);
@@ -130,7 +138,7 @@ public class SOAService {
 		componentsSet.addAll(allReferencedComponents);
 		return new ArrayList<Component>(componentsSet);
 	}
-	
+
 	public List<Component> allComponents(System context) {
 		EcoreService ecoreService = new EcoreService();
 		// Retrieve allRoots
@@ -146,19 +154,20 @@ public class SOAService {
 		}
 		return components;
 	}
-	
+
 	/**
 	 * Return all selectable externable components and their ancestors
+	 * 
 	 * @param semanticDiagram
 	 * @return
 	 */
 	public List<EObject> allSelectableExternalComponentsAndAncestors(DSemanticDiagram semanticDiagram) {
 		Set<EObject> result = new HashSet<EObject>();
-		
+
 		EObject rootObject = semanticDiagram.getTarget();
 		if (rootObject instanceof System) {
-			System system = (System)rootObject;
-			
+			System system = (System) rootObject;
+
 			// retrieve all selectable external components
 			List<Component> components = allSelectableExternalComponents(system, semanticDiagram);
 			for (Component component : components) {
@@ -166,12 +175,13 @@ public class SOAService {
 				result.addAll(getAncestorsAndSelf(component));
 			}
 		}
-		
+
 		return new ArrayList<EObject>(result);
 	}
-	
+
 	/**
 	 * Returns the object and all its ancestors in a list
+	 * 
 	 * @param object
 	 * @return
 	 */
@@ -179,17 +189,16 @@ public class SOAService {
 		List<EObject> result = new ArrayList<EObject>();
 		// Add current object
 		result.add(object);
-		
+
 		EObject container = object.eContainer();
 		if (container != null) {
 			result.addAll(getAncestorsAndSelf(container));
 		}
-		
+
 		return result;
 	}
-	
-	public List<Component> allSelectableExternalComponents(System context,
-			DSemanticDiagram semanticDiagram) {
+
+	public List<Component> allSelectableExternalComponents(System context, DSemanticDiagram semanticDiagram) {
 		List<Component> allNonReferencedExternalComponents = allNonReferencedExternalComponents(context);
 		List<Component> allComponentToRemove = new ArrayList<Component>();
 		// For all semanticDiagram retrieve all contents elements of type
@@ -203,64 +212,92 @@ public class SOAService {
 		allNonReferencedExternalComponents.removeAll(allComponentToRemove);
 		return allNonReferencedExternalComponents;
 	}
-	
-	public boolean isRequiredService (Service context){
-		return context.getKind().equals (getRequiredLiteral(context));
+
+	public boolean isRequiredService(Service context) {
+		return context.getKind().equals(getRequiredLiteral(context));
 	}
-	
-	public InterfaceKind getProvidedLiteral(EObject object){
+
+	public InterfaceKind getProvidedLiteral(EObject object) {
 		return InterfaceKind.PROVIDED_LITERAL;
 	}
-	
-	public InterfaceKind getRequiredLiteral(EObject object){
+
+	public InterfaceKind getRequiredLiteral(EObject object) {
 		return InterfaceKind.REQUIRED_LITERAL;
 	}
-	
+
 	private static class StatusLabelProvider extends LabelProvider {
 
 		@Override
 		public String getText(Object element) {
-			String statusCode = (String) element; 
+			String statusCode = (String) element;
 			return statusCode + " : " + HttpStatusService.getHttpMessage(statusCode); //$NON-NLS-1$
 		}
-		
+
 	}
-	
+
 	public void openStatusCodeSelectionDialog(Parameter parameter) {
-		
+
 		Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
-		
+
 		ListDialog dialog = new ListDialog(shell);
 		dialog.setTitle(Messages.SOAService_StatusSelectionDiaglogTitle);
 		dialog.setMessage(Messages.SOAService_StatusSelectionDiaglogMessage + parameter.getName());
 		dialog.setContentProvider(ArrayContentProvider.getInstance());
 		dialog.setLabelProvider(new StatusLabelProvider());
-		if(parameter.eContainingFeature() == SoaPackage.eINSTANCE.getOperation_Output()) {
+		if (parameter.eContainingFeature() == SoaPackage.eINSTANCE.getOperation_Output()) {
 			dialog.setInput(HttpStatusService.getStandardSuccessCodes());
-		} else if(parameter.eContainingFeature() == SoaPackage.eINSTANCE.getOperation_Fault()) {
+		} else if (parameter.eContainingFeature() == SoaPackage.eINSTANCE.getOperation_Fault()) {
 			dialog.setInput(HttpStatusService.getStandardErrorCodes());
 		}
 		if (dialog.open() == Dialog.OK && dialog.getResult().length == 1) {
-        	String selectedStatusCode = (String) dialog.getResult()[0];
-    		parameter.setStatusCode(selectedStatusCode);
-    		parameter.setStatusMessage(HttpStatusService.getHttpMessage(selectedStatusCode));
+			String selectedStatusCode = (String) dialog.getResult()[0];
+			parameter.setStatusCode(selectedStatusCode);
+			parameter.setStatusMessage(HttpStatusService.getHttpMessage(selectedStatusCode));
 		}
-		
+
 	}
-	
-	private static final Pattern PATH_PARAM_PATTERN = Pattern.compile("\\{[^\\{\\}]*\\}");
-	
+
 	public List<String> getParamIdsFromURI(Operation operation) {
 		List<String> paramIds = new ArrayList<>();
-		if(operation.getURI() != null) {
+		if (operation.getURI() != null) {
 			Matcher matcher = PATH_PARAM_PATTERN.matcher(operation.getURI());
-			while(matcher.find()) {
+			while (matcher.find()) {
 				String match = matcher.group();
 				paramIds.add(match.substring(1, match.length() - 1));
 			}
 		}
-		
+
 		return paramIds;
 	}
-	
+
+	/**
+	 * Checks if a URL is valid.
+	 * 
+	 * @param URL a {@link String}
+	 * @return <code>true</code> if the URL is valid
+	 */
+	public static boolean isValidURL(String URL) {
+		if (URL != null) {
+			Matcher matcher = URL_PARAM_PATTERN.matcher(URL);
+			return matcher.matches();
+		} else {
+			return false;
+		}
+
+	}
+
+	/**
+	 * Checks if an email is valid.
+	 * 
+	 * @param email a {@link String}
+	 * @return <code>true</code> if the email is valid
+	 */
+	public static boolean isValidEmail(String email) {
+		if (email != null) {
+			Matcher matcher = EMAIL_PARAM_PATTERN.matcher(email);
+			return matcher.matches();
+		} else {
+			return false;
+		}
+	}
 }
