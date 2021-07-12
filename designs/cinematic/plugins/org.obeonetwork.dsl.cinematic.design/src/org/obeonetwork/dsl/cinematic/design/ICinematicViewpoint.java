b39fc782fff99a46d266c6641ce689bfe9c7698f
Vincent Richard <vincent.richard@obeo.fr>
2021-07-12 11:02:46 +0200

    [evolCinematic2021] M2Doc convenience sevices to insert Cinematic diagrams

--- designs/cinematic/plugins/org.obeonetwork.dsl.cinematic.design/src/org/obeonetwork/dsl/cinematic/design/ICinematicViewpoint.java
+++ designs/cinematic/plugins/org.obeonetwork.dsl.cinematic.design/src/org/obeonetwork/dsl/cinematic/design/ICinematicViewpoint.java
@@ -22,6 +22,8 @@ public interface ICinematicViewpoint {
 	public final static String ID = "Cinematic Views";
 	public final static String VIEW_CONTAINER_DIAGRAM_ID = "View Container Mockup";
 	public final static String FLOW_DIAGRAM_ID = "Flow Diagram";
+	public final static String LAYOUT_DIAGRAM_ID = "Layout Diagram";
+	public final static String PACKAGE_DIAGRAM_ID = "Package Diagram";
 	public final static int VIEW_CONTAINER_DIAGRAM_DEFAULT_BORDER_SIZE = 4;
 	
 }
