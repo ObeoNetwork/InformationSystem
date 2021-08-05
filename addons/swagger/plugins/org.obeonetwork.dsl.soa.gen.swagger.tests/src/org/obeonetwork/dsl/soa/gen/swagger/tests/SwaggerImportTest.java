c0599bb40a3981c9f4182bca0c1e18555fc8e483
Thibault Beziers la Fosse <thibault.beziers-la-fosse@obeo.fr>
2021-08-05 10:48:22 +0200

    [SAFRAN-902] Fixed Import tests

--- addons/swagger/plugins/org.obeonetwork.dsl.soa.gen.swagger.tests/src/org/obeonetwork/dsl/soa/gen/swagger/tests/SwaggerImportTest.java
+++ addons/swagger/plugins/org.obeonetwork.dsl.soa.gen.swagger.tests/src/org/obeonetwork/dsl/soa/gen/swagger/tests/SwaggerImportTest.java
@@ -20,14 +20,20 @@ import static org.obeonetwork.dsl.soa.gen.swagger.tests.TestHelper.findFirstFile
 import static org.obeonetwork.dsl.soa.gen.swagger.tests.TestHelper.loadEnvironment;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Map;
 
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.junit.Test;
 import org.obeonetwork.dsl.environment.Environment;
 import org.obeonetwork.dsl.soa.System;
 import org.obeonetwork.dsl.soa.gen.swagger.SwaggerImporter;
 import org.obeonetwork.dsl.soa.gen.swagger.utils.SwaggerExportUtil.MapperType;
+import org.obeonetwork.dsl.soa.util.SoaResourceFactoryImpl;
 
 public class SwaggerImportTest {
 	
@@ -57,7 +63,17 @@ public class SwaggerImportTest {
 		destinationSystem.setName(expectedSystem.getName());
 		
 		assertTrue("Swagger import returned with error status code", status != IStatus.ERROR);
-		
+		Resource.Factory.Registry reg = Resource.Factory.Registry.INSTANCE;
+        Map<String, Object> m = reg.getExtensionToFactoryMap();
+        m.put("soa", new SoaResourceFactoryImpl());
+        Resource resource = destinationResourceSet.createResource(URI.createURI("model.soa"));
+        resource.getContents().add(destinationSystem);
+        try {
+			resource.save(Collections.EMPTY_MAP);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		assertECoreEquals("Imported model is different for file " + inputFile.getAbsolutePath(), expectedSystem, destinationSystem); 
 	}
 
