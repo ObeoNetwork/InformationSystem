48b6df97d0c1b290cce61d334fab22970f78eb96
Thibault Beziers la Fosse <thibault.beziers-la-fosse@obeo.fr>
2021-09-14 16:57:02 +0200

    [colors] updated sequence diagram

--- commons/utils/plugins/org.obeonetwork.utils.sirius/src/org/obeonetwork/utils/sirius/services/EObjectUtils.java
+++ commons/utils/plugins/org.obeonetwork.utils.sirius/src/org/obeonetwork/utils/sirius/services/EObjectUtils.java
@@ -17,6 +17,7 @@ import java.util.List;
 
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EDataType;
 import org.eclipse.emf.ecore.EFactory;
 import org.eclipse.emf.ecore.EObject;
@@ -24,6 +25,7 @@ import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.EcorePackage;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 
 public class EObjectUtils {
 
@@ -124,5 +126,16 @@ public class EObjectUtils {
 		}
 		return values;
 	}
-
+	
+	/**
+	 * Returns the name of the root package of a model element
+	 * @param eObject an {@link EObject}
+	 * @return the name as a {@link String}
+	 */
+	public static String getMetamodelRootPackageName(EObject eObject) {
+		EObject rootContainer = EcoreUtil.getRootContainer(eObject, true);
+		EClass clazz = rootContainer.eClass();
+		return clazz.getEPackage().getName();
+	}
 }
+ 
