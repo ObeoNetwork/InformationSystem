f6642cec4a0db9fe4d188d4ffc0535177e9214d2
Thibault Beziers la Fosse <thibault.beziers-la-fosse@obeo.fr>
2021-09-23 15:22:44 +0200

    [SAFRAN-965] Checking if a StructuredType is referenced by a REST API to display the Property extensions

--- models/soa/plugins/org.obeonetwork.dsl.soa/src/org/obeonetwork/dsl/soa/services/PropertiesExtensionsService.java
+++ models/soa/plugins/org.obeonetwork.dsl.soa/src/org/obeonetwork/dsl/soa/services/PropertiesExtensionsService.java
@@ -19,9 +19,18 @@ import java.util.stream.Collectors;
 
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.sirius.business.api.query.EObjectQuery;
+import org.obeonetwork.dsl.environment.Attribute;
+import org.obeonetwork.dsl.environment.EnvironmentPackage;
 import org.obeonetwork.dsl.environment.MetaDataContainer;
 import org.obeonetwork.dsl.environment.ObeoDSMObject;
+import org.obeonetwork.dsl.environment.Reference;
+import org.obeonetwork.dsl.environment.Type;
+import org.obeonetwork.dsl.soa.ExpositionKind;
+import org.obeonetwork.dsl.soa.Operation;
+import org.obeonetwork.dsl.soa.Parameter;
 import org.obeonetwork.dsl.soa.PropertiesExtension;
+import org.obeonetwork.dsl.soa.SoaPackage;
 import org.obeonetwork.dsl.soa.SoaPackage.Literals;
 
 /**
@@ -32,15 +41,16 @@ public class PropertiesExtensionsService {
 	public static final String PROPERTIES_EXTENSION_PREFIX = "x-";
 	
 	private static final Map<EClass, Collection<String>> POSSIBLE_CONTEXTS = new HashMap<>();
+	
 	static {
 		POSSIBLE_CONTEXTS.put(Literals.COMPONENT, Arrays.asList("OpenAPI", "Paths"));
 		POSSIBLE_CONTEXTS.put(Literals.CONTACT, Arrays.asList("Contact"));
 		POSSIBLE_CONTEXTS.put(Literals.INFORMATION, Arrays.asList("Info"));
 		POSSIBLE_CONTEXTS.put(Literals.LICENSE, Arrays.asList("License"));
 		POSSIBLE_CONTEXTS.put(Literals.SERVICE, Arrays.asList("Tag"));
-//		POSSIBLE_CONTEXTS.put(EnvironmentPackage.Literals.TYPE, Arrays.asList("Schema"));
-//		POSSIBLE_CONTEXTS.put(EnvironmentPackage.Literals.ATTRIBUTE, Arrays.asList("Schema"));
-//		POSSIBLE_CONTEXTS.put(EnvironmentPackage.Literals.REFERENCE, Arrays.asList("Schema"));
+		POSSIBLE_CONTEXTS.put(EnvironmentPackage.Literals.TYPE, Arrays.asList("Schema"));
+		POSSIBLE_CONTEXTS.put(EnvironmentPackage.Literals.ATTRIBUTE, Arrays.asList("Schema"));
+		POSSIBLE_CONTEXTS.put(EnvironmentPackage.Literals.REFERENCE, Arrays.asList("Schema"));
 		POSSIBLE_CONTEXTS.put(Literals.OPERATION, Arrays.asList("Operation", "PathItem", "ApiResponses"));
 		POSSIBLE_CONTEXTS.put(Literals.PARAMETER, Arrays.asList("Parameter", "RequestBody", "ApiResponse", "Schema"));
 		POSSIBLE_CONTEXTS.put(Literals.MEDIA_TYPE, Arrays.asList("MediaType"));
@@ -73,10 +83,61 @@ public class PropertiesExtensionsService {
 	 * @return true if element can have properties extensions
 	 */
 	public static boolean isPropertiesExtensionPossible(EObject object) {
-		if (object != null) {
-			return POSSIBLE_CONTEXTS.keySet().stream().anyMatch(eClass -> eClass.isInstance(object));
+		if (object != null && POSSIBLE_CONTEXTS.keySet().stream().anyMatch(eClass -> eClass.isInstance(object))) {
+			if (EnvironmentPackage.Literals.TYPE.isInstance(object)) {
+				return isReferencedByRESTService((Type) object);
+			} else if (EnvironmentPackage.Literals.ATTRIBUTE.isInstance(object)) {
+				return isReferencedByRESTService((Attribute) object);
+			} else if (EnvironmentPackage.Literals.REFERENCE.isInstance(object)) {
+				return isReferencedByRESTService((Reference) object);				
+			} else {
+				return true;
+			}
 		}
 		return false;
+	}
+	
+	/**
+	 * Checks if a {@link Type} is referenced by a Soa {@link Operation}
+	 * @param type a {@link Type}
+	 * @return <code>true</code> or <code>false</code>
+	 */
+	private static boolean isReferencedByRESTService(Type type) {
+		return new EObjectQuery(type).getInverseReferences(SoaPackage.eINSTANCE.getParameter_Type())
+			.stream()
+			.filter(Parameter.class::isInstance)
+			.map(Parameter.class::cast)
+			.anyMatch(p -> p.eContainer() != null && p.eContainer() instanceof Operation && ExpositionKind.REST.equals(((Operation) p.eContainer()).getExposition()));
+	}
+	
+	/**
+	 * Checks if a {@link Reference} starts from, or targets, a {@link Type} that is referenced by a Soa {@link Operation}
+	 * @param reference a {@link Reference}
+	 * @return <code>true</code> or <code>false</code>
+	 */
+	private static boolean isReferencedByRESTService(Reference reference) {
+		boolean isTargetRestService = false;
+		boolean isSourceRestService = false;
+		if (reference.getContainingType() != null) { 
+			isSourceRestService = isReferencedByRESTService(reference.getContainingType());					
+		}
+		if (reference.getReferencedType() != null) {
+			isTargetRestService = isReferencedByRESTService(reference.getReferencedType());
+		}
+		return isTargetRestService || isSourceRestService;
+	}
+	
+	/**
+	 * Checks if the {@link Attribute} is contained in a {@link Type} that is referenced by a Soa {@link Operation}
+	 * @param attribute an {@link Attribute}
+	 * @return <code>true</code> or <code>false</code>
+	 */
+	private static boolean isReferencedByRESTService(Attribute attribute) {
+		if (attribute.getContainingType() != null) {
+			return isReferencedByRESTService(attribute.getContainingType());
+		} else {
+			return false;
+		}
 	}
 	
 	/**
