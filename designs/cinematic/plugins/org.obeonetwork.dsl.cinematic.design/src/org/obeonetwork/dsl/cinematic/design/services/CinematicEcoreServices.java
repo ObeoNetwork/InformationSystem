c80764d7e721478c164fe332bc11ee4401a433f9
Vincent Richard <vincent.richard@obeo.fr>
2022-01-28 12:14:14 +0100

    [SAFRAN-1000] Cinematic - Fix navigation tools from Flow and Package diagram to UI Structure

--- designs/cinematic/plugins/org.obeonetwork.dsl.cinematic.design/src/org/obeonetwork/dsl/cinematic/design/services/CinematicEcoreServices.java
+++ designs/cinematic/plugins/org.obeonetwork.dsl.cinematic.design/src/org/obeonetwork/dsl/cinematic/design/services/CinematicEcoreServices.java
@@ -19,7 +19,6 @@ import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.sirius.business.api.session.Session;
 import org.eclipse.sirius.business.api.session.SessionManager;
 import org.eclipse.sirius.viewpoint.DSemanticDecorator;
-import org.obeonetwork.dsl.cinematic.AbstractPackage;
 import org.obeonetwork.dsl.cinematic.CinematicRoot;
 import org.obeonetwork.dsl.cinematic.NamedElement;
 import org.obeonetwork.dsl.cinematic.view.ViewEvent;
@@ -68,23 +67,6 @@ public class CinematicEcoreServices {
 		}
 		
 		return clone;
-	}
-	
-	/**
-	 * Return the abstractPackage container.
-	 * @param context the AbstractPackage
-	 * @return the container
-	 */
-	public AbstractPackage getAbstractPackageContainer(AbstractPackage context){
-		EObject container = context.eContainer();
-		AbstractPackage abstractPackageToReturn = null;
-		while (container.eContainer() != null && !(context.eContainer() instanceof AbstractPackage)){
-			container = container.eContainer();
-		}
-		if (container instanceof AbstractPackage){
-			abstractPackageToReturn = (AbstractPackage)container;
-		}
-		return abstractPackageToReturn;
 	}
 	
 	/**
