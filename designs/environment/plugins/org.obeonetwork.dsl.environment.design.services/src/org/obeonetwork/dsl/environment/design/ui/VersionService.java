e108874a78a7684c11003080292908ebf200802b
Fabien Coulon <fabien.coulon@obeo.fr>
2023-04-19 10:36:42 +0200

    [SAFRAN-1085] REQUIREMENT - Fix version increment button in properties view
    This commit fixes '+1' button in properties view for the 'version' attributes of
    Requirment and Task elements.
    The confirmation dialog now displays correctly before changing the value.

---
+++ designs/environment/plugins/org.obeonetwork.dsl.environment.design.services/src/org/obeonetwork/dsl/environment/design/ui/VersionService.java
@@ -0,0 +1,44 @@
+package org.obeonetwork.dsl.environment.design.ui;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.PlatformUI;
+
+public class VersionService {
+
+	/**
+	 * This service opens a dialog asking the user if he really want to increment the feature 'version' of the given {@link context}.
+	 * If the answer is affirmative the incremented 'version' is returned, otherwise the original 'version' is returned.
+	 * 
+	 * If the given {@link context} is not a {@link Task} or a {@link Requirement}, 0 is returned and no dialog is opened.
+	 * 
+	 */
+	public int incrementVersion(EObject context) {
+
+		if(!hasVersion(context)) {
+			return 0;
+		}
+		
+		Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+		boolean increment = MessageDialog.openQuestion(shell, "Increment version number",
+				"Are you really sure you want to increment the version number ?\n"
+						+ "Version number should be incremented only on major modifications.");
+		if (increment == true) {
+			int newVersion = getVersion(context) + 1;
+			return newVersion;
+		}
+
+		return getVersion(context);
+	}
+	
+	protected boolean hasVersion(EObject object) {
+		return "Task".equals(object.eClass().getName()) || "Requirement".equals(object.eClass().getName());
+	}
+	
+	protected int getVersion(EObject object) {
+		EStructuralFeature versionFeature = object.eClass().getEStructuralFeature("version");
+		return (int) object.eGet(versionFeature);
+	}
+}
