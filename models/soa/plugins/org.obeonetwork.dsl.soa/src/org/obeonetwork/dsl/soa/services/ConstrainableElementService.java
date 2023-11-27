8f593925cb9ef8ccc8adcc81562ba244a575df95
Vincent Richard <vincent.richard@obeo.fr>
2023-11-27 17:45:39 +0100

    [releng] Add missing copyrights

--- models/soa/plugins/org.obeonetwork.dsl.soa/src/org/obeonetwork/dsl/soa/services/ConstrainableElementService.java
+++ models/soa/plugins/org.obeonetwork.dsl.soa/src/org/obeonetwork/dsl/soa/services/ConstrainableElementService.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2008, 2023 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
 package org.obeonetwork.dsl.soa.services;
 
 import org.obeonetwork.dsl.environment.Attribute;
