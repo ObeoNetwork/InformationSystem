084220c1b9568d22a9a92d6559cf22f35b535259
fksimo <fksimo@obeo.fr>
2024-02-16 15:48:04 +0100

    [SAFRAN-1049] Fixing data sets and adding new tests
    - Fixing (obsolete namespaces in xml change log) data sets
    
    - Adding tests for option "create schema if not exists" on Liquibase
    generation changelog

--- reversers/database-synchroniser/plugins/org.obeonetwork.dsl.database.liquibasegen.test/src/org/obeonetwork/dsl/database/liquibasegen/update/MySQLLiquibaseUpdateTest.java
+++ reversers/database-synchroniser/plugins/org.obeonetwork.dsl.database.liquibasegen.test/src/org/obeonetwork/dsl/database/liquibasegen/update/MySQLLiquibaseUpdateTest.java
@@ -10,6 +10,7 @@
  *******************************************************************************/
 package org.obeonetwork.dsl.database.liquibasegen.update;
 
+import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
@@ -25,7 +26,13 @@ public class MySQLLiquibaseUpdateTest extends AbstractLiquibaseUpdateTest {
 	
 	/**
 	 * Requires a MySQL server to be running. You can check the Docker configuration in org.obeonetwork.dsl.database.test
+	 * A database named public must be (existing or) created too.
 	 * <code>docker run --name mysql5 -p 3306:3306 -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=test mysql:5.7</code>
+	 * Within the container create database with commands:
+	 * <code>mysql --host=localhost --user=root --password=password</code>
+	 * <code>create database public;</code>
+	 * 
+	 * 
 	 * @param fileName the {@link Parameters}
 	 */
 	public MySQLLiquibaseUpdateTest(String fileName) {
@@ -35,4 +42,10 @@ public class MySQLLiquibaseUpdateTest extends AbstractLiquibaseUpdateTest {
 		password = "password";
 	}
 
+	@Before
+	public void removeSchema() throws Exception {
+		//Work around to delete a schema in MySQL (since a schema is a database)
+		//before running  tests.
+		applyChangeLog("update/update-clean-utils/run.schema.clean.changelog.xml") ;
+	}
 }
