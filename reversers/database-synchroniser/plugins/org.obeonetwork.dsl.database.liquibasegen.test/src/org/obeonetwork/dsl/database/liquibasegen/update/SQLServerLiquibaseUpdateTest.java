7f495d6736e0105da126aa50e68428358cf87db5
fksimo <fksimo@obeo.fr>
2024-02-16 15:48:04 +0100

    [SAFRAN-1049] Fixing data sets and adding new tests
    - Fixing (obsolete namespaces in xml change log) data sets
    
    - Adding tests for option "create schema if not exists" on Liquibase
    generation changelog

--- reversers/database-synchroniser/plugins/org.obeonetwork.dsl.database.liquibasegen.test/src/org/obeonetwork/dsl/database/liquibasegen/update/SQLServerLiquibaseUpdateTest.java
+++ reversers/database-synchroniser/plugins/org.obeonetwork.dsl.database.liquibasegen.test/src/org/obeonetwork/dsl/database/liquibasegen/update/SQLServerLiquibaseUpdateTest.java
@@ -13,6 +13,7 @@ package org.obeonetwork.dsl.database.liquibasegen.update;
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
@@ -28,23 +29,34 @@ public class SQLServerLiquibaseUpdateTest extends AbstractLiquibaseUpdateTest{
 	/**
 	 * 
 	 * Requires a MS SQL server to be running. You can check the Docker configuration in org.obeonetwork.dsl.database.test.
-	 * A database labelled test must be created too.
-	 * <code>docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=password" -p 1433:1433 -d mcr.microsoft.com/mssql/server:2017-latest</code>
+	 * A database named test must be created too.
+	 * <code>docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=P4sS-w0rd" --host localhost -p 1433:1433 -d mcr.microsoft.com/mssql/server:2022-latest</code>
+	 * Within the container create database with commands:
+	 * <code>/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P 'P4sS-w0rd'</code>
+	 * <code>create database test; go</code>
 	 * @param fileName
 	 */
 	public SQLServerLiquibaseUpdateTest(String fileName) {
 		super(fileName);
-		url = "jdbc:sqlserver://localhost:1433;databaseName=test";
+		url = "jdbc:sqlserver://localhost:1433;databaseName=test;encrypt=true;trustServerCertificate=true";
 		username = "sa";
-		password = "P4sSw0rd";		
+		password = "P4sS-w0rd";
 	}
 
 	@Parameters
 	public static List<String> parameters() {
-		return Arrays.asList(	"update/update-mssql/01-create-table/run.changelog.xml",
+		return Arrays.asList(	"update/update-mssql/00-add-schema/run.changelog.xml",
+								"update/update-mssql/01-create-table/run.changelog.xml",
 								"update/update-mssql/02-primary-key/run.changelog.xml",
 								"update/update-mssql/03-foreign-key/run.changelog.xml",
 								"update/update-mssql/04-sequence/run.changelog.xml",
 								"update/update-mssql/05-index/run.changelog.xml");
+	}
+	
+	@Before
+	public void removeSchema() throws Exception {
+		//Delete a schema used in test in MSSQL database if existing before running  tests.
+		//Since liquibase.dropAll() doesn't drop all schemas.
+		applyChangeLog("update/update-clean-utils/run.mssql.schema.clean.changelog.xml") ;
 	}
 }
