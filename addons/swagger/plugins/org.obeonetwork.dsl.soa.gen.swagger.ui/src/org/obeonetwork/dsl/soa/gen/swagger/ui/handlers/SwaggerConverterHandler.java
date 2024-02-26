f62b1111d66112795fa234eaf4d4dbf38be68f6b
fksimo <fksimo@obeo.fr>
2024-02-26 13:22:24 +0100

    [SAFRAN-1016]  Migrating library from OpenAPI 3.0 to OpenAPI 3.1
    - OpenAPI exports and Preview are performed in OpenAPI 3.1.0 version
    - OpenAPI imports are performed in OpenAPI 3.0 and 3.1.0 versions, but
    new 3.1.0 version's fields are not managed
    - Conversion function from OpenAPI 3.0 to OpenAPI 3.1.0, plus its
    documentation
    - Swagger Import UI updated

--- addons/swagger/plugins/org.obeonetwork.dsl.soa.gen.swagger.ui/src/org/obeonetwork/dsl/soa/gen/swagger/ui/handlers/SwaggerConverterHandler.java
+++ addons/swagger/plugins/org.obeonetwork.dsl.soa.gen.swagger.ui/src/org/obeonetwork/dsl/soa/gen/swagger/ui/handlers/SwaggerConverterHandler.java
@@ -11,14 +11,11 @@
 package org.obeonetwork.dsl.soa.gen.swagger.ui.handlers;
 
 import static org.obeonetwork.dsl.soa.gen.swagger.Activator.logError;
-import static org.obeonetwork.dsl.soa.gen.swagger.Activator.logWarning;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.OutputStream;
-import java.util.List;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
@@ -29,11 +26,7 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.swt.widgets.Display;
-import org.obeonetwork.dsl.soa.gen.swagger.ui.wizards.Messages;
 import org.obeonetwork.dsl.soa.gen.swagger.utils.SwaggerFileConverter;
-import org.obeonetwork.dsl.soa.gen.swagger.utils.UnsupportedSwagerFileException;
 import org.obeonetwork.utils.common.ui.handlers.EventHelper;
 
 import com.google.common.io.Files;
@@ -46,66 +39,34 @@ public class SwaggerConverterHandler extends AbstractHandler implements IHandler
 		File inputFile = iFile.getLocation().toFile();
 		String path = inputFile.getAbsolutePath();
 		String extension = Files.getFileExtension(path);
-		if (path == null || extension == null) {
+		if(path==null || extension==null) {
 			return null;
 		}
-		String nameWithoutExtension = path.substring(0, path.length() - extension.length() - 1);
-		File outputFile = new File(nameWithoutExtension + "-3.1.0." + extension);
+		String nameWithoutExtension = path.substring(0,path.length()-extension.length()-1);
+		File outputFile = new File(nameWithoutExtension+"-3.1.0."+extension);
 		OutputStream outputStream = null;
-		boolean anyError = false;
 		try {
 			outputStream = new FileOutputStream(outputFile);
 		} catch (FileNotFoundException e) {
-			anyError = true;
-			logError(String.format("Cannot write file: %s", outputFile.getAbsolutePath()), e);
+			logError(String.format("Cannot write file: %s",path), e);
 		} catch (SecurityException e) {
-			anyError = true;
-			logError(String.format("Cannot write file: %s", outputFile.getAbsolutePath()), e);
+			logError(String.format("Cannot write file: %s",path), e);
 		}
-		List<String> parsingWarnings = null;
-		if (outputStream != null) {
-			try {
-				parsingWarnings = SwaggerFileConverter.convert(inputFile, outputStream);
-			} catch (IOException | SecurityException | IllegalArgumentException | UnsupportedSwagerFileException e) {
-				anyError = true;
-				logError(String.format("Cannot convert to OpenAPI 3.1.0 version the file : %s.",
-						inputFile.getAbsolutePath()), e);
-			}
+		if(outputStream!=null) {
+			SwaggerFileConverter.convert(inputFile, outputStream);
 		}
-
-		if (outputFile.exists()) {
+		
+		if(outputFile.exists()) {
 			try {
 				IContainer container = iFile.getParent();
-				if (container != null) {
+				if(container!=null) {
 					container.refreshLocal(IResource.DEPTH_ONE, new NullProgressMonitor());
 				}
 			} catch (CoreException e) {
-				anyError = true;
-				logError(String.format("Error during refreshing for container of file: %s",
-						outputFile.getAbsolutePath()), e);
+				logError(String.format("Error during refreshing for container of file: %s",outputFile.getAbsolutePath()), e);
 			}
 		}
-		boolean anyWarning = parsingWarnings != null && !parsingWarnings.isEmpty();
-		if (anyWarning) {
-			logWarning(String.format("Conversion of file[%s] to OpenAPI 3.1.0: parsing messages:\n%s",
-					inputFile.getAbsolutePath(), parsingWarnings.toString()));
-		}
-		if (anyError) {
-			if (outputFile != null && outputFile.exists()) {
-				try {
-					outputFile.delete();
-				} catch (SecurityException e) {
-				}
-			}
-			MessageDialog.openError(Display.getCurrent().getActiveShell(), Messages.SwaggerConvertWizard_Title,
-					Messages.SwaggerConvertWizard_Failure_message);
-		} else if (anyWarning) {
-			MessageDialog.openWarning(Display.getCurrent().getActiveShell(), Messages.SwaggerConvertWizard_Title,
-					Messages.SwaggerConvertWizard_Warning_message);
-		} else {
-			MessageDialog.openInformation(Display.getCurrent().getActiveShell(), Messages.SwaggerConvertWizard_Title,
-					Messages.SwaggerConvertWizard_Success_message);
-		}
+		
 		return null;
 	}
 }
