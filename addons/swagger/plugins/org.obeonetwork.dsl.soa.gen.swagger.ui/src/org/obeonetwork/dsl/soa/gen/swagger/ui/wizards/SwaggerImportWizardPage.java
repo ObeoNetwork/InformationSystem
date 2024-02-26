42d342001d3c9115dadab73722e6d1283dbf2eb6
fksimo <fksimo@obeo.fr>
2024-02-26 13:22:24 +0100

    [SAFRAN-1016]  Migrating library from OpenAPI 3.0 to OpenAPI 3.1
    - OpenAPI exports and Preview are performed in OpenAPI 3.1.0 version
    - OpenAPI imports are performed in OpenAPI 3.0 and 3.1.0 versions, but
    new 3.1.0 version's fields are not managed
    - Conversion function from OpenAPI 3.0 to OpenAPI 3.1.0, plus its
    documentation
    - Swagger Import UI updated

--- addons/swagger/plugins/org.obeonetwork.dsl.soa.gen.swagger.ui/src/org/obeonetwork/dsl/soa/gen/swagger/ui/wizards/SwaggerImportWizardPage.java
+++ addons/swagger/plugins/org.obeonetwork.dsl.soa.gen.swagger.ui/src/org/obeonetwork/dsl/soa/gen/swagger/ui/wizards/SwaggerImportWizardPage.java
@@ -10,10 +10,13 @@
  *******************************************************************************/
 package org.obeonetwork.dsl.soa.gen.swagger.ui.wizards;
 
+import static org.obeonetwork.dsl.soa.gen.swagger.Activator.logError;
+
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 import java.io.File;
+import java.io.IOException;
 import java.util.Objects;
 
 import org.eclipse.core.databinding.DataBindingContext;
@@ -31,26 +34,31 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
+import org.obeonetwork.dsl.soa.gen.swagger.SwaggerFileQuery;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 public class SwaggerImportWizardPage extends WizardPage {
-	
+
 	private SwaggerImportWizard wizard;
-	
+
 	private DataBindingContext bindingContext;
-	
+
 	SwaggerImportWizardParameters model;
-	
+
 	private Text txtSwaggerFilePath;
 	private Text txtPaginationExtension;
+	private Label inputVersionLabel;
 
 	private PropertyChangeListener swaggerFilePathModelListener;
-	
+
 	static class SwaggerImportWizardParameters {
 		private String swaggerFilePath = "";
 		private String paginationExtension = "";
-		
+		private String validInputVersion = null;
+
 		private PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
-		
+
 		public void addPropertyChangeListener(String propertyName, PropertyChangeListener listener) {
 			propertyChangeSupport.addPropertyChangeListener(propertyName, listener);
 		}
@@ -64,21 +72,31 @@ public class SwaggerImportWizardPage extends WizardPage {
 		}
 
 		public void setSwaggerFilePath(String swaggerFilePath) {
-			if(!Objects.equals(this.swaggerFilePath, swaggerFilePath)) 
-				propertyChangeSupport.firePropertyChange("swaggerFilePath", this.swaggerFilePath, this.swaggerFilePath = swaggerFilePath); //$NON-NLS-1$
+			if (!Objects.equals(this.swaggerFilePath, swaggerFilePath))
+				propertyChangeSupport.firePropertyChange("swaggerFilePath", this.swaggerFilePath, //$NON-NLS-1$
+						this.swaggerFilePath = swaggerFilePath);
 		}
-		
+
 		public String getPaginationExtension() {
 			return paginationExtension;
 		}
 
 		public void setPaginationExtension(String paginationExtension) {
-			if(!Objects.equals(this.paginationExtension, paginationExtension)) 
-				propertyChangeSupport.firePropertyChange("paginationExtension", this.paginationExtension, this.paginationExtension = paginationExtension); //$NON-NLS-1$
+			if (!Objects.equals(this.paginationExtension, paginationExtension))
+				propertyChangeSupport.firePropertyChange("paginationExtension", this.paginationExtension, //$NON-NLS-1$
+						this.paginationExtension = paginationExtension);
 		}
-		
+
+		public String getValidInputVersion() {
+			return validInputVersion;
+		}
+
+		public void setValidInputVersion(String version) {
+			validInputVersion = version;
+		}
+
 	}
-	
+
 	public SwaggerImportWizardPage(SwaggerImportWizard wizard) {
 		super("swaggerImportWizardPage");
 		setTitle("Swagger import");
@@ -93,71 +111,128 @@ public class SwaggerImportWizardPage extends WizardPage {
 
 		setControl(container);
 		container.setLayout(new GridLayout(3, false));
-		
+
 		Label lblSwaggerFilePath = new Label(container, SWT.NONE);
 		lblSwaggerFilePath.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
 		lblSwaggerFilePath.setText("Swagger file");
-		
+
 		txtSwaggerFilePath = new Text(container, SWT.BORDER);
 		txtSwaggerFilePath.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
-		
+
 		Button btnSelectSwaggerFilePath = new Button(container, SWT.NONE);
 		btnSelectSwaggerFilePath.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
 				FileDialog dialog = new FileDialog(SwaggerImportWizardPage.this.getShell(), SWT.OPEN);
-				dialog.setFilterExtensions(new String [] { "*.yaml;*.yml;*.json", "*.yaml;*.yml", "*.json" }); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+				dialog.setFilterExtensions(new String[] { "*.yaml;*.yml;*.json", "*.yaml;*.yml", "*.json" }); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 				dialog.setFilterPath(model.getSwaggerFilePath());
 				String swaggerFilePath = dialog.open();
-				if(swaggerFilePath != null) {
+				if (swaggerFilePath != null) {
 					model.setSwaggerFilePath(swaggerFilePath);
 				}
 			}
 		});
 		btnSelectSwaggerFilePath.setText("...");
-		
+
 		model.setSwaggerFilePath(wizard.getDefaultInputDirPath());
-		
+
 		Label lblPaginationExtension = new Label(container, SWT.NONE);
 		lblPaginationExtension.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
 		lblPaginationExtension.setText("Pagination extension");
-		
+
 		txtPaginationExtension = new Text(container, SWT.BORDER);
 		txtPaginationExtension.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1));
-		
+
 		bindingContext = initDataBindings();
-		
+
 		swaggerFilePathModelListener = new PropertyChangeListener() {
-			
+
 			@Override
 			public void propertyChange(PropertyChangeEvent evt) {
-				setPageComplete(isComplete());
-				if(!fileExists()) {
+				setPageComplete(false);
+				// Check file exits
+				if (!fileExists()) {
 					setErrorMessage("Selected file does not exist.");
-				} else {
-					setErrorMessage(null);
+					resetInputVersionLabel();
+					return;
 				}
+				// Validate file content.
+				SwaggerFileQuery fileQuery = null;
+				try {
+					File inputFile = new File(model.getSwaggerFilePath());
+					fileQuery = new SwaggerFileQuery(inputFile);
+				} catch (JsonProcessingException e) {
+					setErrorMessage("Selected file content is not valid. See error log.");
+					logError(String.format("Invalid file content : %s.", model.getSwaggerFilePath()), e);
+				} catch (IOException e) {
+					setErrorMessage("Selected file content cannot be processed. See error log.");
+					logError(String.format("Cannot validate file content : %s.", model.getSwaggerFilePath()), e);
+				}
+				if (fileQuery == null) {
+					resetInputVersionLabel();
+					setPageComplete(false);
+					return;
+				}
+				setErrorMessage(null);
+				String inputVersion = fileQuery.getVersion();
+				boolean supportedVersion = SwaggerFileQuery.isVersionSupported(inputVersion);
+				if (supportedVersion) {
+					model.setValidInputVersion(inputVersion);
+				} else {
+					model.setValidInputVersion(null);
+				}
+				if (inputVersionLabel != null) {
+					if (!supportedVersion) {
+						inputVersionLabel.setText("Not supported");
+					} else {
+						inputVersionLabel.setText(inputVersion);
+					}
+				}
+				setPageComplete(isComplete());
 			}
 
 		};
 		model.addPropertyChangeListener("swaggerFilePath", swaggerFilePathModelListener); //$NON-NLS-1$
-		
+
+		Label labelInputVersion = new Label(container, SWT.NONE);
+		labelInputVersion.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		labelInputVersion.setText("Input version:");
+
+		inputVersionLabel = new Label(container, SWT.WRAP);
+		inputVersionLabel.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, false, 2, 1));
+
+		Label supportedVersions = new Label(container, SWT.BOTTOM | SWT.BOLD);
+		supportedVersions.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		supportedVersions.setText("Supported versions:");
+		supportedVersions.setForeground(container.getDisplay().getSystemColor(SWT.COLOR_WIDGET_DARK_SHADOW));
+
+		Label supportedVersionsText = new Label(container, SWT.NONE);
+		supportedVersionsText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, true, 1, 1));
+		supportedVersionsText.setText(SwaggerFileQuery.getSupportedVersions().toString());
+		supportedVersionsText.setForeground(
+				container.getDisplay().getSystemColor(SWT.COLOR_WIDGET_DARK_SHADOW | SWT.COLOR_DARK_GREEN));
+
 	}
-	
+
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	protected DataBindingContext initDataBindings() {
 		DataBindingContext bindingContext = new DataBindingContext();
-		
+
 		// swaggerFilePath
-		IObservableValue observeTextSwaggerFilePathObserveWidget = WidgetProperties.text(SWT.Modify).observe(txtSwaggerFilePath);
+		IObservableValue observeTextSwaggerFilePathObserveWidget = WidgetProperties.text(SWT.Modify)
+				.observe(txtSwaggerFilePath);
 		IObservableValue textTxtSwaggerFilePathObserveValue = BeanProperties.value("swaggerFilePath").observe(model);
-		bindingContext.bindValue(observeTextSwaggerFilePathObserveWidget, textTxtSwaggerFilePathObserveValue, null, null);
-		
+		bindingContext.bindValue(observeTextSwaggerFilePathObserveWidget, textTxtSwaggerFilePathObserveValue, null,
+				null);
+
 		// paginationExtension
-		IObservableValue observeTextTxtPaginationExtensionObserveWidget = WidgetProperties.text(SWT.Modify).observe(txtPaginationExtension);
-		IObservableValue textTxtPaginationExtensionObserveValue = BeanProperties.value("paginationExtension").observe(model);
-		bindingContext.bindValue(observeTextTxtPaginationExtensionObserveWidget, textTxtPaginationExtensionObserveValue, null, null);
-		
+		IObservableValue observeTextTxtPaginationExtensionObserveWidget = WidgetProperties.text(SWT.Modify)
+				.observe(txtPaginationExtension);
+		IObservableValue textTxtPaginationExtensionObserveValue = BeanProperties.value("paginationExtension")
+				.observe(model);
+		bindingContext.bindValue(observeTextTxtPaginationExtensionObserveWidget, textTxtPaginationExtensionObserveValue,
+				null, null);
+
 		return bindingContext;
 	}
 
@@ -165,19 +240,28 @@ public class SwaggerImportWizardPage extends WizardPage {
 		File file = new File(model.getSwaggerFilePath());
 		return file.exists() && file.isFile();
 	}
-	
+
 	public boolean isComplete() {
-		return fileExists();
+		return fileExists() && model.getValidInputVersion() != null;
+	}
+
+	private void resetInputVersionLabel() {
+		if (inputVersionLabel != null) {
+			inputVersionLabel.setText("");
+		}
+		if (model != null) {
+			model.setValidInputVersion(null);
+		}
 	}
 
 	@Override
 	public void dispose() {
 		model.removePropertyChangeListener(swaggerFilePathModelListener);
 		bindingContext.dispose();
-		
+
 		super.dispose();
 	}
-	
+
 	public String getSwaggerFilePath() {
 		return model.getSwaggerFilePath();
 	}
--- addons/swagger/plugins/org.obeonetwork.dsl.soa.gen.swagger/src/org/obeonetwork/dsl/soa/gen/swagger/SwaggerImporter.java
+++ addons/swagger/plugins/org.obeonetwork.dsl.soa.gen.swagger/src/org/obeonetwork/dsl/soa/gen/swagger/SwaggerImporter.java
@@ -29,7 +29,9 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.swagger.v3.core.util.Json;
+import io.swagger.v3.core.util.Json31;
 import io.swagger.v3.core.util.Yaml;
+import io.swagger.v3.core.util.Yaml31;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.OAuthFlow;
@@ -37,14 +39,11 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.security.SecurityScheme.Type;
 
 public class SwaggerImporter {
-	
-	private static final String OPEN_API_SUPPORTED_VERSION_PATTERN = "OpenAPI 3.[0-9]+(.[0-9]+)?";
-	private static final String OPEN_API_SUPPORTED_VERSIONS = "OpenAPI 3.x";
-	
+
 	private System soaSystem;
 	private Environment environment;
 	private SwaggerFileQuery fileQuery;
-	
+
 	public SwaggerImporter(System system, Environment environment) {
 		this.soaSystem = system;
 		this.environment = environment;
@@ -52,7 +51,7 @@ public class SwaggerImporter {
 
 	public int importFromFile(String inputFilePath, String paginationExtension) {
 		int status = IStatus.OK;
-		
+
 		File inputFile = new File(inputFilePath);
 
 		String swaggerVersion = null;
@@ -66,20 +65,33 @@ public class SwaggerImporter {
 			logError("I/O exception.", e);
 			status = IStatus.ERROR;
 		}
-		
-		if(status != IStatus.ERROR && !swaggerVersion.matches(OPEN_API_SUPPORTED_VERSION_PATTERN)) {
-			logError(String.format("Unsupported format : %s. Supported version are %s.", swaggerVersion, OPEN_API_SUPPORTED_VERSIONS));
+
+		if (status != IStatus.ERROR && !SwaggerFileQuery.isVersionSupported(swaggerVersion)) {
+			logError(String.format("Unsupported format : %s. Supported versions are %s.", swaggerVersion,
+					SwaggerFileQuery.getSupportedVersions()));
 			status = IStatus.ERROR;
 		}
-		
+
 		OpenAPI swagger = null;
-		if(status != IStatus.ERROR) {
-			
+		if (status != IStatus.ERROR) {
+
 			ObjectMapper objectMapper = null;
-			if(inputFilePath.endsWith(".yaml") || inputFilePath.endsWith(".yml")) {
-				objectMapper = Yaml.mapper();
-			} else if(inputFilePath.endsWith(".json")) {
-				objectMapper = Json.mapper();
+
+			if (inputFilePath.endsWith(".yaml") || inputFilePath.endsWith(".yml")) {
+				if (SwaggerFileQuery.isOpenAPI31Version(swaggerVersion)) {
+					// "Yaml" mapper doesn't support all new OpenAPI 3.1.0 attributes.
+					objectMapper = Yaml31.mapper();
+				} else {
+					objectMapper = Yaml.mapper();
+				}
+
+			} else if (inputFilePath.endsWith(".json")) {
+				if (SwaggerFileQuery.isOpenAPI31Version(swaggerVersion)) {
+					// "Json" mapper doesn't support all new OpenAPI 3.1.0 attributes.
+					objectMapper = Json31.mapper();
+				} else {
+					objectMapper = Json.mapper();
+				}
 			}
 
 			try {
@@ -97,60 +109,64 @@ public class SwaggerImporter {
 				status = IStatus.ERROR;
 			}
 		}
-		
-		if(status != IStatus.ERROR) {
-			
-			SoaComponentBuilder soaComponentBuilder = new SoaComponentBuilder(swagger, environment, paginationExtension);
+
+		if (status != IStatus.ERROR) {
+
+			SoaComponentBuilder soaComponentBuilder = new SoaComponentBuilder(swagger, environment,
+					paginationExtension);
 			status = soaComponentBuilder.build();
-			
-			if(status != IStatus.ERROR) {
+
+			if (status != IStatus.ERROR) {
 				Component soaComponent = soaComponentBuilder.getComponent();
 				Namespace soaComponentNamespace = soaComponentBuilder.getNamespace();
-				
-				if(soaComponent != null && SystemGenUtil.getComponentByName(soaSystem, soaComponent.getName()) != null) {
-					logError(String.format("Component with name %s already exist.", soaComponent.getName()));
+
+				if (soaComponent != null
+						&& SystemGenUtil.getComponentByName(soaSystem, soaComponent.getName()) != null) {
+					logError(String.format("Component with name %s already exists.", soaComponent.getName()));
 					status = IStatus.ERROR;
 				}
-				
-				if(soaComponentNamespace != null && NamespaceGenUtil.getNamespaceByName(soaSystem, soaComponentNamespace.getName()) != null) {
-					logError(String.format("Namespace with name %s already exist.", soaComponentNamespace.getName()));
+
+				if (soaComponentNamespace != null
+						&& NamespaceGenUtil.getNamespaceByName(soaSystem, soaComponentNamespace.getName()) != null) {
+					logError(String.format("Namespace with name %s already exists.", soaComponentNamespace.getName()));
 					status = IStatus.ERROR;
 				}
-				
-				if(status != IStatus.ERROR) {
-					if(soaComponent != null) {
+
+				if (status != IStatus.ERROR) {
+					if (soaComponent != null) {
 						soaSystem.getOwnedComponents().add(soaComponent);
 					}
-					if(soaComponentNamespace != null) {
+					if (soaComponentNamespace != null) {
 						soaSystem.getOwnedNamespaces().add(soaComponentNamespace);
 					}
 				}
 			}
 		}
-		
-		return status;		
+
+		return status;
 	}
-	
+
 	/**
-	 * Checks the {@link SecurityScheme} of the {@link Components}. 
-	 * If a scheme has the OpenIDConnect {@link Type}, it gathers the {@link OAuthFlow} it may contain.
+	 * Checks the {@link SecurityScheme} of the {@link Components}. If a scheme has
+	 * the OpenIDConnect {@link Type}, it gathers the {@link OAuthFlow} it may
+	 * contain.
 	 * 
-	 * This is a workaround the fact that io.swagger.v3 doesn't read OpenId Connect security schemes.
+	 * This is a workaround the fact that io.swagger.v3 doesn't read OpenId Connect
+	 * security schemes.
 	 * 
 	 * @param api the {@link OpenAPI}
 	 */
 	private void addFlowsToOpenIdConnectSchemes(OpenAPI api) {
-		if(api.getComponents().getSecuritySchemes() != null) {
-			api.getComponents().getSecuritySchemes().entrySet().stream()
-			.filter(entry -> entry.getValue().getType().equals(SecurityScheme.Type.OPENIDCONNECT))
-			.forEach(entry -> {
-				try {
-					entry.getValue().setFlows(fileQuery.getOAuthFlows(entry.getKey()));
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			});
+		if (api.getComponents() != null && api.getComponents().getSecuritySchemes() != null) {
+			api.getComponents().getSecuritySchemes().entrySet().stream().filter(entry -> entry.getValue() != null
+					&& SecurityScheme.Type.OPENIDCONNECT.equals(entry.getValue().getType())).forEach(entry -> {
+						try {
+							entry.getValue().setFlows(fileQuery.getOAuthFlows(entry.getKey()));
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+					});
 		}
 	}
-	
+
 }
