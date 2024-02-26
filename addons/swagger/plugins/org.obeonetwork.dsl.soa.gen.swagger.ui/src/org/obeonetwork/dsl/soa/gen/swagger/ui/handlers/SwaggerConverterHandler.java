7baa0bc56b4e51138d376d67c3d09504ad856b93
fksimo <fksimo@obeo.fr>
2024-02-26 13:22:24 +0100

    [SAFRAN-1016]  Migrating library from OpenAPI 3.0 to OpenAPI 3.1
    - OpenAPI exports and Preview are performed in OpenAPI 3.1.0 version
    - OpenAPI imports are performed in OpenAPI 3.0 and 3.1.0 versions, but
    new 3.1.0 version's fields are not managed
    - Conversion function from OpenAPI 3.0 to OpenAPI 3.1.0, plus its
    documentation
    - Swagger Import UI updated

---
+++ addons/swagger/plugins/org.obeonetwork.dsl.soa.gen.swagger.ui/src/org/obeonetwork/dsl/soa/gen/swagger/ui/handlers/SwaggerConverterHandler.java
@@ -0,0 +1,72 @@
+/*******************************************************************************
+ * Copyright (c) 2008, 2023 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.obeonetwork.dsl.soa.gen.swagger.ui.handlers;
+
+import static org.obeonetwork.dsl.soa.gen.swagger.Activator.logError;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.obeonetwork.dsl.soa.gen.swagger.utils.SwaggerFileConverter;
+import org.obeonetwork.utils.common.ui.handlers.EventHelper;
+
+import com.google.common.io.Files;
+
+public class SwaggerConverterHandler extends AbstractHandler implements IHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IFile iFile = EventHelper.uwrapSingleSelection(event, IFile.class);
+		File inputFile = iFile.getLocation().toFile();
+		String path = inputFile.getAbsolutePath();
+		String extension = Files.getFileExtension(path);
+		if(path==null || extension==null) {
+			return null;
+		}
+		String nameWithoutExtension = path.substring(0,path.length()-extension.length()-1);
+		File outputFile = new File(nameWithoutExtension+"-3.1.0."+extension);
+		OutputStream outputStream = null;
+		try {
+			outputStream = new FileOutputStream(outputFile);
+		} catch (FileNotFoundException e) {
+			logError(String.format("Cannot write file: %s",path), e);
+		} catch (SecurityException e) {
+			logError(String.format("Cannot write file: %s",path), e);
+		}
+		if(outputStream!=null) {
+			SwaggerFileConverter.convert(inputFile, outputStream);
+		}
+		
+		if(outputFile.exists()) {
+			try {
+				IContainer container = iFile.getParent();
+				if(container!=null) {
+					container.refreshLocal(IResource.DEPTH_ONE, new NullProgressMonitor());
+				}
+			} catch (CoreException e) {
+				logError(String.format("Error during refreshing for container of file: %s",outputFile.getAbsolutePath()), e);
+			}
+		}
+		
+		return null;
+	}
+}
