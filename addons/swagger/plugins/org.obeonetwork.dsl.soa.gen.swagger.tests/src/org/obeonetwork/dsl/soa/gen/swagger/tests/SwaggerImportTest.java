97905bd03855e2b17728362539effa1da105ab82
fksimo <fksimo@obeo.fr>
2024-02-26 13:26:52 +0100

    [SAFRAN-1016]  Test migrating library from OpenAPI 3.0 to OpenAPI 3.1
    - Update import/export OpenAPI 3.0 tests
    - Adding import/export OpenAPI 3.1.0 tests
    - New fields of OpenAPI 3.1.0 version are not taken into account in SOA
    model import/export.

--- addons/swagger/plugins/org.obeonetwork.dsl.soa.gen.swagger.tests/src/org/obeonetwork/dsl/soa/gen/swagger/tests/SwaggerImportTest.java
+++ addons/swagger/plugins/org.obeonetwork.dsl.soa.gen.swagger.tests/src/org/obeonetwork/dsl/soa/gen/swagger/tests/SwaggerImportTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2024 Obeo.
+ * Copyright (c) 2008, 2023 Obeo.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -123,13 +123,4 @@ public class SwaggerImportTest {
 		testImportFromFile("petstore-310", MapperType.YAML);
 	}
 
-	@Test
-	public void testJsonImportSwagger1() {
-		testImportFromFile("swagger1", MapperType.JSON);
-	}
-
-	@Test
-	public void testJsonImportSwagger2() {
-		testImportFromFile("swagger2", MapperType.JSON);
-	}
 }
