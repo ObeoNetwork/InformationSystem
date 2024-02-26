b6253d7aeeabc6d9590c926b597d6f3df8d95dc6
fksimo <fksimo@obeo.fr>
2024-02-26 13:22:24 +0100

    [SAFRAN-1016]  Migrating library from OpenAPI 3.0 to OpenAPI 3.1
    - OpenAPI exports and Preview are performed in OpenAPI 3.1.0 version
    - OpenAPI imports are performed in OpenAPI 3.0 and 3.1.0 versions, but
    new 3.1.0 version's fields are not managed
    - Conversion function from OpenAPI 3.0 to OpenAPI 3.1.0, plus its
    documentation
    - Swagger Import UI updated

--- addons/swagger/plugins/org.obeonetwork.dsl.soa.gen.swagger/src/org/obeonetwork/dsl/soa/gen/swagger/OpenApiParserHelper.java
+++ addons/swagger/plugins/org.obeonetwork.dsl.soa.gen.swagger/src/org/obeonetwork/dsl/soa/gen/swagger/OpenApiParserHelper.java
@@ -11,21 +11,25 @@
 package org.obeonetwork.dsl.soa.gen.swagger;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
+import io.swagger.v3.oas.models.media.ArraySchema;
+import io.swagger.v3.oas.models.media.JsonSchema;
 import io.swagger.v3.oas.models.media.Schema;
 
 @SuppressWarnings("rawtypes")
 public class OpenApiParserHelper {
 
 	public static final String COMPONENT_SCHEMA_$REF = "#/components/schemas/";
-    
+
 	public static final String OPEN_API_TYPE_OBJECT = "object";
 	public static final String OPEN_API_TYPE_STRING = "string";
 	public static final String OPEN_API_TYPE_NUMBER = "number";
 	public static final String OPEN_API_TYPE_INTEGER = "integer";
 	public static final String OPEN_API_TYPE_BOOLEAN = "boolean";
-	
+
 	public static final String OPEN_API_FORMAT_BINARY = "binary";
 	public static final String OPEN_API_FORMAT_DATE = "date";
 	public static final String OPEN_API_FORMAT_DOUBLE = "double";
@@ -35,13 +39,13 @@ public class OpenApiParserHelper {
 	public static final String OPEN_API_FORMAT_DATETIME = "date-time";
 	public static final String OPEN_API_FORMAT_BYTE = "byte";
 	public static final String OPEN_API_FORMAT_PASSWORD = "password";
-    
+
 	public static final String OPEN_API_IN_BODY = "body";
 	public static final String OPEN_API_IN_COOKIE = "cookie";
 	public static final String OPEN_API_IN_HEADER = "header";
 	public static final String OPEN_API_IN_PATH = "path";
 	public static final String OPEN_API_IN_QUERY = "query";
-    
+
 	public static final String SOA_PRIMITIVE_TYPE_NAME_BINARY = "Binary";
 	public static final String SOA_PRIMITIVE_TYPE_NAME_BOOLEAN = "Boolean";
 	public static final String SOA_PRIMITIVE_TYPE_NAME_DATE = "Date";
@@ -52,80 +56,149 @@ public class OpenApiParserHelper {
 	public static final String SOA_PRIMITIVE_TYPE_NAME_STRING = "String";
 	public static final String SOA_PRIMITIVE_TYPE_NAME_TIME = "Time";
 	public static final String SOA_PRIMITIVE_TYPE_NAME_TIMESTAMP = "Timestamp";
-	
-    private static final Map<String, Schema> dataTypePrototypeSchemas = new HashMap<>();
-    static {
-		dataTypePrototypeSchemas.put(SOA_PRIMITIVE_TYPE_NAME_BINARY,    createSchema(OPEN_API_TYPE_STRING,  OPEN_API_FORMAT_BINARY));
-		dataTypePrototypeSchemas.put(SOA_PRIMITIVE_TYPE_NAME_DATE,      createSchema(OPEN_API_TYPE_STRING,  OPEN_API_FORMAT_DATE));
-		dataTypePrototypeSchemas.put(SOA_PRIMITIVE_TYPE_NAME_STRING,    createSchema(OPEN_API_TYPE_STRING,  null));
-		dataTypePrototypeSchemas.put(SOA_PRIMITIVE_TYPE_NAME_TIME,      createSchema(OPEN_API_TYPE_STRING,  OPEN_API_FORMAT_DATETIME));
-		dataTypePrototypeSchemas.put(SOA_PRIMITIVE_TYPE_NAME_TIMESTAMP, createSchema(OPEN_API_TYPE_STRING,  OPEN_API_FORMAT_DATETIME));
-    	dataTypePrototypeSchemas.put(SOA_PRIMITIVE_TYPE_NAME_BOOLEAN,   createSchema(OPEN_API_TYPE_BOOLEAN, null));
-    	dataTypePrototypeSchemas.put(SOA_PRIMITIVE_TYPE_NAME_INTEGER,   createSchema(OPEN_API_TYPE_INTEGER, OPEN_API_FORMAT_INT32));
-    	dataTypePrototypeSchemas.put(SOA_PRIMITIVE_TYPE_NAME_LONG,      createSchema(OPEN_API_TYPE_INTEGER, OPEN_API_FORMAT_INT64));
-    	dataTypePrototypeSchemas.put(SOA_PRIMITIVE_TYPE_NAME_FLOAT,     createSchema(OPEN_API_TYPE_NUMBER,  OPEN_API_FORMAT_FLOAT));
-		dataTypePrototypeSchemas.put(SOA_PRIMITIVE_TYPE_NAME_DOUBLE,    createSchema(OPEN_API_TYPE_NUMBER,  OPEN_API_FORMAT_DOUBLE));
-    }
-    public static Schema createPrimitiveTypeSchema(String primitiveTypeName) {
+
+	private static final Map<String, Schema> dataTypePrototypeSchemas = new HashMap<>();
+	static {
+		dataTypePrototypeSchemas.put(SOA_PRIMITIVE_TYPE_NAME_BINARY,
+				createSchema(OPEN_API_TYPE_STRING, OPEN_API_FORMAT_BINARY));
+		dataTypePrototypeSchemas.put(SOA_PRIMITIVE_TYPE_NAME_DATE,
+				createSchema(OPEN_API_TYPE_STRING, OPEN_API_FORMAT_DATE));
+		dataTypePrototypeSchemas.put(SOA_PRIMITIVE_TYPE_NAME_STRING, createSchema(OPEN_API_TYPE_STRING, null));
+		dataTypePrototypeSchemas.put(SOA_PRIMITIVE_TYPE_NAME_TIME,
+				createSchema(OPEN_API_TYPE_STRING, OPEN_API_FORMAT_DATETIME));
+		dataTypePrototypeSchemas.put(SOA_PRIMITIVE_TYPE_NAME_TIMESTAMP,
+				createSchema(OPEN_API_TYPE_STRING, OPEN_API_FORMAT_DATETIME));
+		dataTypePrototypeSchemas.put(SOA_PRIMITIVE_TYPE_NAME_BOOLEAN, createSchema(OPEN_API_TYPE_BOOLEAN, null));
+		dataTypePrototypeSchemas.put(SOA_PRIMITIVE_TYPE_NAME_INTEGER,
+				createSchema(OPEN_API_TYPE_INTEGER, OPEN_API_FORMAT_INT32));
+		dataTypePrototypeSchemas.put(SOA_PRIMITIVE_TYPE_NAME_LONG,
+				createSchema(OPEN_API_TYPE_INTEGER, OPEN_API_FORMAT_INT64));
+		dataTypePrototypeSchemas.put(SOA_PRIMITIVE_TYPE_NAME_FLOAT,
+				createSchema(OPEN_API_TYPE_NUMBER, OPEN_API_FORMAT_FLOAT));
+		dataTypePrototypeSchemas.put(SOA_PRIMITIVE_TYPE_NAME_DOUBLE,
+				createSchema(OPEN_API_TYPE_NUMBER, OPEN_API_FORMAT_DOUBLE));
+	}
+
+	public static Schema createPrimitiveTypeSchema(String primitiveTypeName) {
 		Schema prototypeSchema = dataTypePrototypeSchemas.get(primitiveTypeName);
-		if(prototypeSchema != null) {
-			return createSchema(prototypeSchema.getType(), prototypeSchema.getFormat());
+		if (prototypeSchema != null) {
+			return createSchema(getSingleSchemaType(prototypeSchema), prototypeSchema.getFormat());
 		}
-		
+
 		return null;
 	}
 
 	public static String getPrimitiveTypeName(Schema schema) {
 		String primitiveTypeName = null;
-		
-		if(OPEN_API_TYPE_STRING.equals(schema.getType())) {
-			if(OPEN_API_FORMAT_BINARY.equals(schema.getFormat())) {
+		String schemaType = getSingleSchemaType(schema);
+		if (OPEN_API_TYPE_STRING.equals(schemaType)) {
+			if (OPEN_API_FORMAT_BINARY.equals(schema.getFormat())) {
 				primitiveTypeName = SOA_PRIMITIVE_TYPE_NAME_BINARY;
-			} else if(OPEN_API_FORMAT_DATE.equals(schema.getFormat())) {
+			} else if (OPEN_API_FORMAT_DATE.equals(schema.getFormat())) {
 				primitiveTypeName = SOA_PRIMITIVE_TYPE_NAME_DATE;
-			} else if(OPEN_API_FORMAT_DATETIME.equals(schema.getFormat())) {
+			} else if (OPEN_API_FORMAT_DATETIME.equals(schema.getFormat())) {
 				primitiveTypeName = SOA_PRIMITIVE_TYPE_NAME_TIMESTAMP;
 			} else {
 				primitiveTypeName = SOA_PRIMITIVE_TYPE_NAME_STRING;
 			}
-		} else if(OPEN_API_TYPE_BOOLEAN.equals(schema.getType())) {
+		} else if (OPEN_API_TYPE_BOOLEAN.equals(schemaType)) {
 			primitiveTypeName = SOA_PRIMITIVE_TYPE_NAME_BOOLEAN;
-		} else if(OPEN_API_TYPE_INTEGER.equals(schema.getType())) {
-			if(OPEN_API_FORMAT_INT32.equals(schema.getFormat())) {
+		} else if (OPEN_API_TYPE_INTEGER.equals(schemaType)) {
+			if (OPEN_API_FORMAT_INT32.equals(schema.getFormat())) {
 				primitiveTypeName = SOA_PRIMITIVE_TYPE_NAME_INTEGER;
-			} else if(OPEN_API_FORMAT_INT64.equals(schema.getFormat())) {
+			} else if (OPEN_API_FORMAT_INT64.equals(schema.getFormat())) {
 				primitiveTypeName = SOA_PRIMITIVE_TYPE_NAME_LONG;
 			} else {
 				primitiveTypeName = SOA_PRIMITIVE_TYPE_NAME_LONG;
 			}
-		} else if(OPEN_API_TYPE_NUMBER.equals(schema.getType())) {
-			if(OPEN_API_FORMAT_FLOAT.equals(schema.getFormat())) {
+		} else if (OPEN_API_TYPE_NUMBER.equals(schemaType)) {
+			if (OPEN_API_FORMAT_FLOAT.equals(schema.getFormat())) {
 				primitiveTypeName = SOA_PRIMITIVE_TYPE_NAME_FLOAT;
-			} else if(OPEN_API_FORMAT_DOUBLE.equals(schema.getFormat())) {
+			} else if (OPEN_API_FORMAT_DOUBLE.equals(schema.getFormat())) {
 				primitiveTypeName = SOA_PRIMITIVE_TYPE_NAME_DOUBLE;
 			} else {
 				primitiveTypeName = SOA_PRIMITIVE_TYPE_NAME_DOUBLE;
 			}
-		} 
-		
+		}
+
 		return primitiveTypeName;
-    }
-    
+	}
+
 	private static <T> Schema<T> createSchema(String type, String format) {
-    	Schema<T> schema = new Schema<>();
-    	schema.setType(type);
-    	schema.setFormat(format);
-    	return schema;
-    }
-	
+		Schema<T> schema = new Schema<>();
+		schema.setType(type);
+		// Account for OpenAPI 3.1.0
+		Set<String> types = new HashSet<>();
+		types.add(type);
+		schema.setTypes(types);
+
+		schema.setFormat(format);
+		return schema;
+	}
+
 	public static boolean isEnum(Schema schema) {
-		return OPEN_API_TYPE_STRING.equals(schema.getType()) 
-				&& schema.getEnum() != null 
+		return OPEN_API_TYPE_STRING.equals(getSingleSchemaType(schema)) && schema.getEnum() != null
 				&& !schema.getEnum().isEmpty();
 	}
 
-	public static boolean isObject(Schema schema) {
-		return OPEN_API_TYPE_OBJECT.equals(schema.getType());
+	/**
+	 * <p>
+	 * Gets the single type of a Schema whether it's from an OpenAPI 3.1 file or an
+	 * Open 3.0 file.
+	 * </p>
+	 * <p>
+	 * <b>Multiple values are not currently well-managed for type of Schema.</b>
+	 * </p>
+	 * <p>
+	 * By default, if several values are found including
+	 * {@value #OPEN_API_TYPE_STRING}, the latter is returned.
+	 * </p>
+	 * <p>
+	 * By default, if several values are found including
+	 * {@value #OPEN_API_TYPE_OBJECT}, the latter is returned.
+	 * </p>
+	 * 
+	 * @param schema
+	 * @return
+	 */
+	public static String getSingleSchemaType(Schema schema) {
+		if (schema.getType() != null) {
+			// OpenAPI 3.0
+			return schema.getType();
+		}
+		// OpenAPI 3.1
+		Set types = schema.getTypes();
+		if (types == null) {
+			return null;
+		}
+		if (types.size() == 1) {
+			return (String) types.toArray()[0];
+		} else if (types.size() > 1) {
+			if (types.contains(OPEN_API_TYPE_STRING)) {
+				return OPEN_API_TYPE_STRING;
+			}
+			if (types.contains(OPEN_API_TYPE_OBJECT)) {
+				return OPEN_API_TYPE_OBJECT;
+			}
+		}
+		return null;
 	}
-	
+
+	public static boolean isObject(Schema schema) {
+		return OPEN_API_TYPE_OBJECT.equals(getSingleSchemaType(schema));
+	}
+
+	/**
+	 * Workaround to determine whether schema is an ArraySchema. Due to bug:
+	 * https://github.com/OpenAPITools/openapi-generator/issues/14200 OpenAPI 3.1.0
+	 * arrays are unmarshalled as JsonSchema instead of ArraySchema as in OpenAPI
+	 * 3.0.0.
+	 * 
+	 * @param schema
+	 * @return
+	 */
+	public static boolean isArraySchema(Schema schema) {
+		return (schema instanceof ArraySchema)
+				|| ("array".equals(getSingleSchemaType(schema)) && schema instanceof JsonSchema);
+	}
 }
--- addons/swagger/plugins/org.obeonetwork.dsl.soa.gen.swagger/src/org/obeonetwork/dsl/soa/gen/swagger/utils/SwaggerExportUtil.java
+++ addons/swagger/plugins/org.obeonetwork.dsl.soa.gen.swagger/src/org/obeonetwork/dsl/soa/gen/swagger/utils/SwaggerExportUtil.java
@@ -19,48 +19,62 @@ import org.eclipse.core.runtime.IStatus;
 import org.obeonetwork.dsl.soa.Component;
 import org.obeonetwork.dsl.soa.gen.swagger.SwaggerBuilder;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import io.swagger.v3.core.util.Json;
-import io.swagger.v3.core.util.Yaml;
+import io.swagger.v3.core.util.Json31;
+import io.swagger.v3.core.util.OpenAPI30To31;
+import io.swagger.v3.core.util.Yaml31;
 import io.swagger.v3.oas.models.OpenAPI;
 
 public class SwaggerExportUtil {
-	
-    public enum MapperType {
-        YAML, JSON;
-    }
 
-    public static SwaggerExportResult export(Component component, MapperType outputType, OutputStream outputStream) {
-    	
+	public enum MapperType {
+		YAML, JSON;
+	}
+
+	public static SwaggerExportResult export(Component component, MapperType outputType, OutputStream outputStream) {
 
 		SwaggerBuilder swaggerBuilder = new SwaggerBuilder(component);
-		
+
 		int status = swaggerBuilder.build();
-		
-		OpenAPI swagger  = null;
-		
-		if(status != IStatus.ERROR) {
-	        swagger  = swaggerBuilder.getOpenAPI();
-	        
-	        ObjectMapper mapper = null;	        
-	        switch (outputType) {
-	        case JSON:
-	            mapper = Json.mapper();
-	            break;
-	        case YAML:
-	            mapper = Yaml.mapper();
-	            break;
-	        }
-	        mapper.setSerializationInclusion(Include.NON_NULL);
-	        mapper.setSerializationInclusion(Include.NON_EMPTY);
-	        
-	        mapper.enable(MapperFeature.USE_ANNOTATIONS);
-	        try {
+
+		OpenAPI swagger = null;
+
+		if (status != IStatus.ERROR) {
+			swagger = swaggerBuilder.getOpenAPI();
+
+			OpenAPI30To31 oapi30To31 = new OpenAPI30To31();
+			oapi30To31.process(swagger);
+			// Don't write dialect.
+			swagger.setJsonSchemaDialect(null);
+			ObjectMapper mapper = null;
+			switch (outputType) {
+			case JSON:
+				mapper = Json31.mapper();
+				break;
+			case YAML:
+				mapper = Yaml31.mapper();
+				break;
+			}
+			mapper.setSerializationInclusion(Include.NON_NULL);
+			mapper.setSerializationInclusion(Include.NON_EMPTY);
+			// Since Jackson 2.9.X., using option Include.NON_EMPTY in
+			// setSerializationInclusion has as effect to ignore maps with an (unique) entry
+			// whose value is null/empty (empty list for instance.)
+
+			// See also: https://github.com/FasterXML/jackson-databind/issues/2136
+			// So we force an entry to be taken into account with
+			// a non-empty entry's key and any values accepted for the entry's value.
+			mapper.setDefaultPropertyInclusion(
+					JsonInclude.Value.construct(JsonInclude.Include.NON_EMPTY, JsonInclude.Include.ALWAYS));
+
+			mapper.enable(MapperFeature.USE_ANNOTATIONS);
+			try {
 				mapper.writerWithDefaultPrettyPrinter().writeValue(outputStream, swagger);
 			} catch (JsonGenerationException e) {
 				logError("Json generation exception.", e);
@@ -73,15 +87,15 @@ public class SwaggerExportUtil {
 				status = IStatus.ERROR;
 			}
 		}
-		
+
 		return new SwaggerExportResult(swagger, status);
 	}
-    
-    public static class SwaggerExportResult {
-    	private OpenAPI swagger;
-    	private int status;
 
-    	public SwaggerExportResult(OpenAPI swagger, int status) {
+	public static class SwaggerExportResult {
+		private OpenAPI swagger;
+		private int status;
+
+		public SwaggerExportResult(OpenAPI swagger, int status) {
 			super();
 			this.swagger = swagger;
 			this.status = status;
@@ -90,9 +104,9 @@ public class SwaggerExportUtil {
 		public OpenAPI getSwagger() {
 			return swagger;
 		}
-		
+
 		public int getStatus() {
 			return status;
 		}
-    }	
+	}
 }
