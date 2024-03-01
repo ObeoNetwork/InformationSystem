edd3ec8a90a16de69edd6fcffdba9b01a7cb4f86
fksimo <fksimo@obeo.fr>
2024-03-01 09:43:01 +0100

    [SAFRAN-899]  Adding a converter from Swagger to OpenAPI
    - Swagger converter added
    - Swagger 1 and 2 supported in SOA Import
    - Update documentation
    - Tests

---
+++ addons/swagger/plugins/org.obeonetwork.dsl.soa.gen.swagger/src/org/obeonetwork/dsl/soa/gen/swagger/utils/UnsupportedSwagerFileException.java
@@ -0,0 +1,12 @@
+package org.obeonetwork.dsl.soa.gen.swagger.utils;
+
+public class UnsupportedSwagerFileException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public UnsupportedSwagerFileException(String message) {
+		super(message);
+	}
+}
