68b7e735f9990d21811ca9505540e95d17baaa42
fksimo <fksimo@obeo.fr>
2024-03-08 15:16:05 +0100

    [SAFRAN-710] WIP Adding presentation containers below Graal System node
    - Adding in model explorer logical nodes as container (by kind) of Graal
    System constituent elements
    - Regenerating from genmodel
    - Updating documentation

--- models/graal/plugins/org.obeonetwork.graal.edit/src/org/obeonetwork/graal/provider/SystemItemProvider.java
+++ models/graal/plugins/org.obeonetwork.graal.edit/src/org/obeonetwork/graal/provider/SystemItemProvider.java
@@ -10,36 +10,40 @@
  *******************************************************************************/
 package org.obeonetwork.graal.provider;
 
-
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
+import org.eclipse.emf.common.command.Command;
+import org.eclipse.emf.common.command.CommandWrapper;
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.notify.Notification;
-
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EStructuralFeature;
-
+import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
+import org.eclipse.emf.edit.provider.IDisposable;
+import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
 import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
 import org.eclipse.emf.edit.provider.ViewerNotification;
-
 import org.obeonetwork.dsl.environment.EnvironmentFactory;
 import org.obeonetwork.dsl.environment.EnvironmentPackage;
-
 import org.obeonetwork.graal.GraalFactory;
 import org.obeonetwork.graal.GraalPackage;
+import org.obeonetwork.graal.System;
+import org.obeonetwork.graal.provider.util.SystemContentItemProviderUtil;
 
 /**
  * This is the item provider adapter for a {@link org.obeonetwork.graal.System} object.
- * <!-- begin-user-doc -->
- * <!-- end-user-doc -->
+ * <!-- begin-user-doc --> <!-- end-user-doc -->
  * @generated
  */
 public class SystemItemProvider extends NamedElementItemProvider {
 	/**
-	 * This constructs an instance from a factory and a notifier.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * This constructs an instance from a factory and a notifier. <!--
+	 * begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	public SystemItemProvider(AdapterFactory adapterFactory) {
@@ -47,9 +51,9 @@ public class SystemItemProvider extends NamedElementItemProvider {
 	}
 
 	/**
-	 * This returns the property descriptors for the adapted class.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * This returns the property descriptors for the adapted class. <!--
+	 * begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
@@ -57,18 +61,18 @@ public class SystemItemProvider extends NamedElementItemProvider {
 		if (itemPropertyDescriptors == null) {
 			super.getPropertyDescriptors(object);
 
-			addNamespacesPropertyDescriptor(object);
-			addTypesPropertyDescriptor(object);
-			addOwnedTasksPropertyDescriptor(object);
-			addOwnedGroupsPropertyDescriptor(object);
+//			addNamespacesPropertyDescriptor(object);
+//			addTypesPropertyDescriptor(object);
+//			addOwnedTasksPropertyDescriptor(object);
+//			addOwnedGroupsPropertyDescriptor(object);
 		}
 		return itemPropertyDescriptors;
 	}
 
 	/**
-	 * This adds a property descriptor for the Namespaces feature.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * This adds a property descriptor for the Namespaces feature. <!--
+	 * begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	protected void addNamespacesPropertyDescriptor(Object object) {
@@ -89,8 +93,8 @@ public class SystemItemProvider extends NamedElementItemProvider {
 
 	/**
 	 * This adds a property descriptor for the Types feature.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc
+	 * --> <!-- end-user-doc -->
 	 * @generated
 	 */
 	protected void addTypesPropertyDescriptor(Object object) {
@@ -110,9 +114,9 @@ public class SystemItemProvider extends NamedElementItemProvider {
 	}
 
 	/**
-	 * This adds a property descriptor for the Owned Tasks feature.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * This adds a property descriptor for the Owned Tasks feature. <!--
+	 * begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	protected void addOwnedTasksPropertyDescriptor(Object object) {
@@ -132,9 +136,9 @@ public class SystemItemProvider extends NamedElementItemProvider {
 	}
 
 	/**
-	 * This adds a property descriptor for the Owned Groups feature.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * This adds a property descriptor for the Owned Groups feature. <!--
+	 * begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	protected void addOwnedGroupsPropertyDescriptor(Object object) {
@@ -154,31 +158,31 @@ public class SystemItemProvider extends NamedElementItemProvider {
 	}
 
 	/**
-	 * This specifies how to implement {@link #getChildren} and is used to deduce an appropriate feature for an
-	 * {@link org.eclipse.emf.edit.command.AddCommand}, {@link org.eclipse.emf.edit.command.RemoveCommand} or
+	 * This specifies how to implement {@link #getChildren} and is used to deduce an
+	 * appropriate feature for an {@link org.eclipse.emf.edit.command.AddCommand},
+	 * {@link org.eclipse.emf.edit.command.RemoveCommand} or
 	 * {@link org.eclipse.emf.edit.command.MoveCommand} in {@link #createCommand}.
-	 * <!-- begin-user-doc -->
-	 * Specify the order of the System sub nodes.
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> Specify the order of the System sub nodes. <!--
+	 * end-user-doc -->
+	 * 
 	 * @generated NOT
 	 */
 	@Override
 	public Collection<? extends EStructuralFeature> getChildrenFeatures(Object object) {
 		if (childrenFeatures == null) {
 			super.getChildrenFeatures(object);
-			childrenFeatures.add(GraalPackage.Literals.SYSTEM__SUB_SYSTEMS);
-			childrenFeatures.add(GraalPackage.Literals.TASKS_CONTAINER__TASKS);
-			childrenFeatures.add(GraalPackage.Literals.SYSTEM__USE_CASES);
-			childrenFeatures.add(GraalPackage.Literals.SYSTEM__USER_STORIES);
-			childrenFeatures.add(GraalPackage.Literals.SYSTEM__ACTORS);
-			childrenFeatures.add(EnvironmentPackage.Literals.NAMESPACES_CONTAINER__OWNED_NAMESPACES);
+//			childrenFeatures.add(GraalPackage.Literals.SYSTEM__SUB_SYSTEMS);
+//			childrenFeatures.add(GraalPackage.Literals.TASKS_CONTAINER__TASKS);
+//			childrenFeatures.add(GraalPackage.Literals.SYSTEM__USE_CASES);
+//			childrenFeatures.add(GraalPackage.Literals.SYSTEM__USER_STORIES);
+//			childrenFeatures.add(GraalPackage.Literals.SYSTEM__ACTORS);
+//			childrenFeatures.add(EnvironmentPackage.Literals.NAMESPACES_CONTAINER__OWNED_NAMESPACES);
 		}
 		return childrenFeatures;
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
 	 * @generated
 	 */
 	@Override
@@ -191,8 +195,7 @@ public class SystemItemProvider extends NamedElementItemProvider {
 
 	/**
 	 * This returns System.gif.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
 	 * @generated
 	 */
 	@Override
@@ -201,8 +204,7 @@ public class SystemItemProvider extends NamedElementItemProvider {
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
 	 * @generated
 	 */
 	@Override
@@ -223,13 +225,11 @@ public class SystemItemProvider extends NamedElementItemProvider {
 			getString("_UI_System_type") :
 			getString("_UI_System_type") + " " + label;
 	}
-	
 
 	/**
 	 * This handles model notifications by calling {@link #updateChildren} to update any cached
 	 * children and by creating a viewer notification, which it passes to {@link #fireNotifyChanged}.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
 	 * @generated
 	 */
 	@Override
@@ -260,40 +260,107 @@ public class SystemItemProvider extends NamedElementItemProvider {
 	protected void collectNewChildDescriptors(Collection<Object> newChildDescriptors, Object object) {
 		super.collectNewChildDescriptors(newChildDescriptors, object);
 
-		newChildDescriptors.add
-			(createChildParameter
-				(GraalPackage.Literals.TASKS_CONTAINER__TASKS,
-				 GraalFactory.eINSTANCE.createTask()));
-
-		newChildDescriptors.add
-			(createChildParameter
-				(GraalPackage.Literals.TASKS_CONTAINER__TASKS,
-				 GraalFactory.eINSTANCE.createTasksGroup()));
-
-		newChildDescriptors.add
-			(createChildParameter
-				(EnvironmentPackage.Literals.NAMESPACES_CONTAINER__OWNED_NAMESPACES,
-				 EnvironmentFactory.eINSTANCE.createNamespace()));
-
-		newChildDescriptors.add
-			(createChildParameter
-				(GraalPackage.Literals.SYSTEM__SUB_SYSTEMS,
-				 GraalFactory.eINSTANCE.createSystem()));
-
-		newChildDescriptors.add
-			(createChildParameter
-				(GraalPackage.Literals.SYSTEM__ACTORS,
-				 GraalFactory.eINSTANCE.createActor()));
-
-		newChildDescriptors.add
-			(createChildParameter
-				(GraalPackage.Literals.SYSTEM__USE_CASES,
-				 GraalFactory.eINSTANCE.createUseCase()));
-
-		newChildDescriptors.add
-			(createChildParameter
-				(GraalPackage.Literals.SYSTEM__USER_STORIES,
-				 GraalFactory.eINSTANCE.createUserStory()));
+//		newChildDescriptors.add
+//			(createChildParameter
+//				(GraalPackage.Literals.TASKS_CONTAINER__TASKS,
+//				 GraalFactory.eINSTANCE.createTask()));
+//
+//		newChildDescriptors.add
+//			(createChildParameter
+//				(GraalPackage.Literals.TASKS_CONTAINER__TASKS,
+//				 GraalFactory.eINSTANCE.createTasksGroup()));
+//
+//		newChildDescriptors.add
+//			(createChildParameter
+//				(EnvironmentPackage.Literals.NAMESPACES_CONTAINER__OWNED_NAMESPACES,
+//				 EnvironmentFactory.eINSTANCE.createNamespace()));
+//
+//		newChildDescriptors.add
+//			(createChildParameter
+//				(GraalPackage.Literals.SYSTEM__SUB_SYSTEMS,
+//				 GraalFactory.eINSTANCE.createSystem()));
+//
+//		newChildDescriptors.add
+//			(createChildParameter
+//				(GraalPackage.Literals.SYSTEM__ACTORS,
+//				 GraalFactory.eINSTANCE.createActor()));
+//
+//		newChildDescriptors.add
+//			(createChildParameter
+//				(GraalPackage.Literals.SYSTEM__USE_CASES,
+//				 GraalFactory.eINSTANCE.createUseCase()));
+//
+//		newChildDescriptors.add
+//			(createChildParameter
+//				(GraalPackage.Literals.SYSTEM__USER_STORIES,
+//				 GraalFactory.eINSTANCE.createUserStory()));
 	}
 
+	protected List children = null;
+
+	@Override
+	public Collection<?> getChildren(Object object) {
+		if (children == null) {
+			System system = (System) object;
+			children = new ArrayList();
+			SystemContentItemProviderUtil.addSystemCustomContentItemProviders(children, system, adapterFactory);
+		}
+		return children;
+	}
+
+	public Object getSystemCustomContentItemProvider(EStructuralFeature reference) {
+		return SystemContentItemProviderUtil.getSystemCustomContentItemProvider(reference, children);
+	}
+
+	@Override
+	public Object getParent(Object object) {
+		Object system = super.getParent(object);
+		if (system instanceof System) {// Ensure object is not a root System.
+			SystemItemProvider systemItemProvider = (SystemItemProvider) adapterFactory.adapt(system,
+					IEditingDomainItemProvider.class);
+			return systemItemProvider != null
+					? systemItemProvider.getSystemCustomContentItemProvider(GraalPackage.Literals.SYSTEM__SUB_SYSTEMS)
+					: null;
+		}
+		return system;
+
+	}
+
+	
+	protected Command createRemoveCommand(EditingDomain domain, EObject owner, EStructuralFeature feature,
+			Collection collection) {
+		return createWrappedCommand(super.createRemoveCommand(domain, owner, feature, collection), owner, feature);
+	}
+
+
+	protected Command createAddCommand(EditingDomain domain, EObject owner, EStructuralFeature feature,
+			Collection collection, int index) {
+		return createWrappedCommand(super.createAddCommand(domain, owner, feature, collection, index), owner, feature);
+	}
+
+	protected Command createWrappedCommand(Command command, final EObject owner, final EStructuralFeature feature) {
+		if (SystemContentItemProviderUtil.SYSTEM_CUSTOM_CONTENT_ORDERED_REFERENCES.contains(feature)) {
+			return new CommandWrapper(command) {
+				public Collection getAffectedObjects() {
+					Collection affected = super.getAffectedObjects();
+					if (affected.contains(owner)) {
+						affected = Collections.singleton(getSystemCustomContentItemProvider(feature));
+					}
+					return affected;
+				}
+			};
+		}
+		return command;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public void dispose() {
+		super.dispose();
+		if (children != null) {
+			for (Object child : children) {
+				((IDisposable) child).dispose();
+			}
+		}
+	}
 }
