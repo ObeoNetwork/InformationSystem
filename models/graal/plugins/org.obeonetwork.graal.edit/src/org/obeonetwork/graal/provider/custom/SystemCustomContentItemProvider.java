434b35a09eff05eecdf0a38b4e7e04e0624f963f
fksimo <fksimo@obeo.fr>
2024-03-13 14:21:12 +0100

    [SAFRAN-710] Impact of logical blocks on the graal emf editor
    - Make sure the graal emf editor works properly with new logical blocks
    added below the node Graal System

--- models/graal/plugins/org.obeonetwork.graal.edit/src/org/obeonetwork/graal/provider/custom/SystemCustomContentItemProvider.java
+++ models/graal/plugins/org.obeonetwork.graal.edit/src/org/obeonetwork/graal/provider/custom/SystemCustomContentItemProvider.java
@@ -3,7 +3,6 @@ package org.obeonetwork.graal.provider.custom;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Objects;
-import java.util.function.Supplier;
 
 import org.eclipse.emf.common.command.Command;
 import org.eclipse.emf.common.command.UnexecutableCommand;
@@ -11,15 +10,14 @@ import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.edit.command.AddCommand;
-import org.eclipse.emf.edit.command.CommandParameter;
 import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.obeonetwork.graal.System;
-import org.obeonetwork.graal.provider.util.SystemContentItemProviderUtil;
 
 /**
  * ItemProvider for the logical blocs: Actors, UseCases, SubSystems,
  * UserStories, Tasks and Namespaces below a {@link System}.
+ * 
  * 
  * @author Obeo
  *
@@ -27,7 +25,6 @@ import org.obeonetwork.graal.provider.util.SystemContentItemProviderUtil;
 public class SystemCustomContentItemProvider extends TransientSystemItemProvider {
 
 	private EStructuralFeature reference;
-	private Supplier<EObject> elementFactory;
 	private String labelText;
 
 	private SystemCustomContentItemProvider(AdapterFactory adapterFactory, System system) {
@@ -35,13 +32,11 @@ public class SystemCustomContentItemProvider extends TransientSystemItemProvider
 	}
 
 	public SystemCustomContentItemProvider(AdapterFactory adapterFactory, System system, EStructuralFeature reference,
-			Supplier<EObject> elementFactory, String labelText) {
+			String labelText) {
 		this(adapterFactory, system);
 		Objects.requireNonNull(reference);
-		Objects.requireNonNull(elementFactory);
 		Objects.requireNonNull(system);
 		this.reference = reference;
-		this.elementFactory = elementFactory;
 		this.labelText = labelText;
 	}
 
@@ -60,15 +55,6 @@ public class SystemCustomContentItemProvider extends TransientSystemItemProvider
 	}
 
 	@Override
-	protected String getFeatureText(Object feature) {
-		if (feature instanceof EStructuralFeature
-				&& SystemContentItemProviderUtil.SYSTEM_CUSTOM_CONTENT_ORDERED_REFERENCES.contains(feature)) {
-			return SystemContentItemProviderUtil.getFeatureText((EStructuralFeature) feature);
-		}
-		return super.getFeatureText(feature);
-	}
-
-	@Override
 	public Object getImage(final Object element) {
 		return AbstractUIPlugin.imageDescriptorFromPlugin("org.obeonetwork.graal.edit", //$NON-NLS-1$
 				"icons/full/obj16/" + reference.getEType().getName() + ".gif"); // $NON-NLS-N$
@@ -79,9 +65,7 @@ public class SystemCustomContentItemProvider extends TransientSystemItemProvider
 	@Override
 	protected void collectNewChildDescriptors(Collection<Object> newChildDescriptors, Object object) {
 		super.collectNewChildDescriptors(newChildDescriptors, object);
-		CommandParameter cmdParameter = createChildParameter(reference, elementFactory.get());
-		cmdParameter.setOwner(target);
-		newChildDescriptors.add(cmdParameter);
+		// No creation allowed at logical nodes level.
 	}
 
 	@Override
@@ -89,24 +73,22 @@ public class SystemCustomContentItemProvider extends TransientSystemItemProvider
 		ArrayList<Object> newChildDescriptors = new ArrayList<Object>();
 		collectNewChildDescriptors(newChildDescriptors, object);
 		return newChildDescriptors;
-		// return super.getNewChildDescriptors(target, editingDomain, sibling);
 	}
 
 	@Override
 	protected Command createDragAndDropCommand(EditingDomain domain, Object owner, float location, int operations,
 			int operation, Collection collection) {
-		if (new AddCommand(domain, (EObject) owner, reference, collection).canExecute()) {
-			return super.createDragAndDropCommand(domain, owner, location, operations, operation, collection);
+		EObject finalOwner = (EObject) target;// actualOwner((EObject)owner, reference);
+		if (finalOwner != null) {
+			if (new AddCommand(domain, (EObject) finalOwner, reference, collection).canExecute()) {
+				return super.createDragAndDropCommand(domain, finalOwner, location, operations, operation, collection);
+			}
 		}
 		return UnexecutableCommand.INSTANCE;
 	}
 
 	public EStructuralFeature getReference() {
 		return reference;
-	}
-
-	public Supplier<EObject> getElementFactory() {
-		return elementFactory;
 	}
 
 	public String getLabelText() {
