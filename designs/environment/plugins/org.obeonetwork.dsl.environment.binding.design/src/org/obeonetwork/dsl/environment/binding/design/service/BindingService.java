d87a9d12e6d85ac4e00193a6b8a77486a8477167
fksimo <fksimo@obeo.fr>
2024-03-22 11:59:54 +0100

    [SAFRAN-1136] Updating binding diagram and binding details editor
    - The binding diagram now allows ViewContainer as an left element
    - The binding details editor is now more generic (children and their
    labels in the editor are contributed via an extension added)
    - Adding contributions to the binding details editor for StructuredType
    and ViewContainer
    - Modifying labels of binding details fields in the binding editor
    - Supplement to fix: duplicate of the Binding details editor in model
    explorer
    - Fixing RepresentationDescription recognized by BindindDialectServices

--- designs/environment/plugins/org.obeonetwork.dsl.environment.binding.design/src/org/obeonetwork/dsl/environment/binding/design/service/BindingService.java
+++ designs/environment/plugins/org.obeonetwork.dsl.environment.binding.design/src/org/obeonetwork/dsl/environment/binding/design/service/BindingService.java
@@ -16,6 +16,7 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.emf.common.util.TreeIterator;
@@ -32,8 +33,13 @@ import org.eclipse.sirius.diagram.DNode;
 import org.eclipse.sirius.diagram.DSemanticDiagram;
 import org.eclipse.sirius.ui.business.api.session.UserSession;
 import org.eclipse.sirius.viewpoint.DRepresentation;
+import org.eclipse.sirius.viewpoint.DRepresentationDescriptor;
+import org.eclipse.sirius.viewpoint.ViewpointPackage;
 import org.eclipse.sirius.viewpoint.description.RepresentationDescription;
 import org.eclipse.sirius.viewpoint.description.Viewpoint;
+import org.obeonetwork.dsl.cinematic.AbstractPackage;
+import org.obeonetwork.dsl.cinematic.view.AbstractViewElement;
+import org.obeonetwork.dsl.cinematic.view.ViewContainer;
 import org.obeonetwork.dsl.environment.BindingInfo;
 import org.obeonetwork.dsl.environment.BindingRegistry;
 import org.obeonetwork.dsl.environment.BoundableElement;
@@ -41,10 +47,10 @@ import org.obeonetwork.dsl.environment.EnvironmentFactory;
 import org.obeonetwork.dsl.environment.EnvironmentPackage;
 import org.obeonetwork.dsl.environment.Namespace;
 import org.obeonetwork.dsl.environment.NamespacesContainer;
+import org.obeonetwork.dsl.environment.ObeoDSMObject;
 import org.obeonetwork.dsl.environment.StructuredType;
 import org.obeonetwork.dsl.environment.Type;
 import org.obeonetwork.dsl.environment.TypesDefinition;
-import org.obeonetwork.dsl.environment.binding.dialect.BindingDialectServices;
 import org.obeonetwork.dsl.environment.bindingdialect.DBindingEditor;
 
 public class BindingService {
@@ -71,29 +77,6 @@ public class BindingService {
 			return session.getSemanticResources();
 		}
 		return Collections.emptyList();
-	}
-
-	public Collection<StructuredType> getAllBindableElements(DSemanticDiagram diagram, StructuredType structuredType) {
-		// Collect all structured types
-		Collection<StructuredType> bindableElements = new ArrayList<StructuredType>();
-
-		// First get all structured types
-		bindableElements.addAll(getAllStructuredTypes(structuredType));
-
-		// Remove the target element
-		bindableElements.remove(structuredType);
-
-		// Remove elements already bound with the target
-		for (BindingInfo bindingInfo : getAllBindingInfosOnDiagram(diagram)) {
-			if (bindingInfo.getLeft() == structuredType) {
-				bindableElements.remove(bindingInfo.getRight());
-			}
-			if (bindingInfo.getRight() == structuredType) {
-				bindableElements.remove(bindingInfo.getLeft());
-			}
-		}
-
-		return bindableElements;
 	}
 
 	private Collection<BindingInfo> getAllBindingInfosOnDiagram(DSemanticDiagram diagram) {
@@ -137,7 +120,7 @@ public class BindingService {
 				&& eClass.getEPackage().getNsURI().startsWith("http://www.obeonetwork.org/dsl/overview/");
 	}
 
-	public Collection<BindingInfo> getRelatedBindingInfos(Namespace namespace) {
+	private Collection<BindingInfo> getRelatedBindingInfos(Namespace namespace) {
 		// Use of LinkedHashSet to keep order and avoid potential lock/unlock
 		// loops problems
 		Set<BindingInfo> results = new LinkedHashSet<BindingInfo>();
@@ -160,29 +143,27 @@ public class BindingService {
 		return results;
 	}
 
+	/**
+	 * 
+	 * @param structuredType
+	 * @return a collection of {@link BindingInfo} applicable for Entity/DTO
+	 *         bindings and not for {@link AbstractViewElement}s.
+	 */
 	private Collection<BindingInfo> getRelatedBindingInfos(StructuredType structuredType) {
 		EObjectQuery query = new EObjectQuery(structuredType);
-		Collection<EObject> targets = new HashSet<EObject>();
-		targets.addAll(query.getInverseReferences(EnvironmentPackage.Literals.BINDING_INFO__LEFT));
-		targets.addAll(query.getInverseReferences(EnvironmentPackage.Literals.BINDING_INFO__RIGHT));
-		Collection<BindingInfo> bindingInfos = new ArrayList<BindingInfo>();
-		for (EObject target : targets) {
-			if (target instanceof BindingInfo) {
-				bindingInfos.add((BindingInfo) target);
-			}
-		}
-		return bindingInfos;
-	}
+		Collection<BindingInfo> bindingInfos = new HashSet<BindingInfo>();
 
-	public BindingRegistry getBindingRegistry(Namespace namespace) {
-		if (namespace.getBindingRegistries().isEmpty()) {
-			BindingRegistry bindingRegistry = EnvironmentFactory.eINSTANCE.createBindingRegistry();
-			namespace.getBindingRegistries().add(bindingRegistry);
-			namespace.eResource().getContents().add(bindingRegistry);
-			return bindingRegistry;
-		} else {
-			return namespace.getBindingRegistries().get(0);
-		}
+		bindingInfos.addAll(query.getInverseReferences(EnvironmentPackage.Literals.BINDING_INFO__LEFT).stream()
+				.filter(BindingInfo.class::isInstance).map(BindingInfo.class::cast)
+				// Ensure bindingInfo doesn't target an AbstractViewElement
+				.filter(bi -> !(bi.getRight() instanceof AbstractViewElement)).collect(Collectors.toList()));
+
+		bindingInfos.addAll(query.getInverseReferences(EnvironmentPackage.Literals.BINDING_INFO__RIGHT).stream()
+				.filter(BindingInfo.class::isInstance).map(BindingInfo.class::cast)
+				// Ensure bindingInfo doesn't target an AbstractViewElement.
+				.filter(bi -> !(bi.getLeft() instanceof AbstractViewElement)).collect(Collectors.toList()));
+
+		return bindingInfos;
 	}
 
 	public BindingInfo reconnectBindingTarget(BindingInfo bindingInfo, BoundableElement oldTarget,
@@ -202,8 +183,10 @@ public class BindingService {
 	}
 
 	private String getBoundElementName(EObject object) {
-		if ((object != null) && ((object instanceof StructuredType))) {
+		if ((object instanceof StructuredType)) {
 			return ((StructuredType) object).getName();
+		} else if (object instanceof ViewContainer) {
+			return ((ViewContainer) object).getName();
 		}
 		return null;
 	}
@@ -226,8 +209,8 @@ public class BindingService {
 		if (editor == null) {
 			RepresentationDescription representationDescription = getBindingEditorRepresentation(session);
 			if (representationDescription != null) {
-				DRepresentation representation = BindingDialectServices.get().createRepresentation(
-						computeBindingEditorName(bindingInfo), bindingInfo, representationDescription,
+				DRepresentation representation = DialectManager.INSTANCE.createRepresentation(
+						computeBindingEditorName(bindingInfo), bindingInfo, representationDescription, session,
 						new NullProgressMonitor());
 				if (representation != null && representation instanceof DBindingEditor) {
 					editor = (DBindingEditor) representation;
@@ -251,5 +234,134 @@ public class BindingService {
 			}
 		}
 		return null;
+	}
+
+	/**
+	 * 
+	 * @param context
+	 * @return whether a Binding diagram can be created from context.
+	 */
+	public boolean isValidContextForBindingDiagram(ObeoDSMObject context) {
+		if (!(context instanceof Namespace || context instanceof ViewContainer)) {
+			return false;
+		}
+		if (context instanceof ViewContainer) {
+			if (!(context.eContainer() instanceof AbstractPackage)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * 
+	 * @param rootBindingInfos should either be environment's{@link Namespace}, or
+	 *                         view's {@link ViewContainer}.
+	 * 
+	 * @return all {@link BindingInfo} related to rootBindingInfos.
+	 */
+	public Collection<BindingInfo> getRelatedBindingInfos(ObeoDSMObject rootBindingInfos) {
+		if (rootBindingInfos instanceof Namespace) {
+			return getRelatedBindingInfos((Namespace) rootBindingInfos);
+		}
+		if (rootBindingInfos instanceof ViewContainer) {
+			return getRelatedBindingInfos((ViewContainer) rootBindingInfos);
+		}
+		return Collections.emptySet();
+	}
+
+	private Collection<BindingInfo> getRelatedBindingInfos(ViewContainer viewContainer) {
+		// Use of LinkedHashSet to keep order and avoid potential lock/unlock loops
+		// problems
+		Set<BindingInfo> results = new LinkedHashSet<BindingInfo>();
+		ViewContainer topParent = getViewContainerForBindingInfos((ViewContainer) viewContainer);
+		if (topParent != null) {
+			for (BindingRegistry bindingRegistry : ((ViewContainer) topParent).getBindingRegistries()) {
+				results.addAll(bindingRegistry.getBindingInfos());
+			}
+		}
+		return results;
+	}
+
+	/**
+	 * The returned ViewContainer is such that it's
+	 * <p>
+	 * Either an ancestor of container that has no {@link ViewContainer} as a parent
+	 * but an {@link AbstractPackage} instead.
+	 * </p>
+	 * <p>
+	 * Or container itself if it has {@link AbstractPackage} as a parent.
+	 * </p>
+	 *
+	 * @param container
+	 * @return a ViewContainer that should contain the {@link BindingRegistry}
+	 *         containing {@link BindingInfo}s.
+	 */
+	private static ViewContainer getViewContainerForBindingInfos(ViewContainer container) {
+		EObject topParent = container;
+		while (topParent != null && !(topParent.eContainer() instanceof AbstractPackage)) {
+			topParent = topParent.eContainer();
+		}
+		if (topParent instanceof ViewContainer) {
+			return (ViewContainer) topParent;
+		}
+		return null;
+	}
+
+	public Collection<StructuredType> getAllBindableElements(DSemanticDiagram diagram,
+			BoundableElement boundableElement) {
+		// Collect all structured types
+		Collection<StructuredType> bindableElements = new ArrayList<StructuredType>();
+
+		// First get all structured types
+		bindableElements.addAll(getAllStructuredTypes(boundableElement));
+
+		// Remove the target element
+		bindableElements.remove(boundableElement);
+
+		// Remove elements already bound with the target
+		for (BindingInfo bindingInfo : getAllBindingInfosOnDiagram(diagram)) {
+			if (bindingInfo.getLeft() == boundableElement) {
+				bindableElements.remove(bindingInfo.getRight());
+			}
+			if (bindingInfo.getRight() == boundableElement) {
+				bindableElements.remove(bindingInfo.getLeft());
+			}
+		}
+
+		return bindableElements;
+	}
+
+	/**
+	 * 
+	 * @param context should either be environment's{@link Namespace}, or view's
+	 *                {@link ViewContainer}.
+	 * @return
+	 */
+	public BindingRegistry getBindingRegistry(ObeoDSMObject context) {
+		if (context instanceof Namespace) {
+			if (context.getBindingRegistries().isEmpty()) {
+				return createBindingRegistry(context);
+			} else {
+				return context.getBindingRegistries().get(0);
+			}
+		}
+		if (context instanceof ViewContainer) {
+			ViewContainer containerForRegistry = getViewContainerForBindingInfos((ViewContainer) context);
+			if (containerForRegistry != null) {
+				if (containerForRegistry.getBindingRegistries().isEmpty()) {
+					return createBindingRegistry(containerForRegistry);
+				} else {
+					return containerForRegistry.getBindingRegistries().get(0);
+				}
+			}
+		}
+		return null;
+	}
+
+	private BindingRegistry createBindingRegistry(ObeoDSMObject container) {
+		BindingRegistry bindingRegistry = EnvironmentFactory.eINSTANCE.createBindingRegistry();
+		container.getBindingRegistries().add(bindingRegistry);
+		return bindingRegistry;
 	}
 }
