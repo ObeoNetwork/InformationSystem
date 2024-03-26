896fd0455982a58b837fa6c61a8a1966d03ef385
fksimo <fksimo@obeo.fr>
2024-03-26 15:13:03 +0100

    [SAFRAN-1136] Adding a binding action on ViewContainer Mockup
    - Action for editing bindings on ViewElement and ViewContainers
    - Moving registry under CinematicRoot for bindings on ViewElement and
    ViewContainer

--- designs/cinematic/plugins/org.obeonetwork.dsl.cinematic.design/src/org/obeonetwork/dsl/cinematic/design/services/CinematicDropServices.java
+++ designs/cinematic/plugins/org.obeonetwork.dsl.cinematic.design/src/org/obeonetwork/dsl/cinematic/design/services/CinematicDropServices.java
@@ -15,9 +15,12 @@ import java.util.Collection;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.obeonetwork.dsl.cinematic.view.AbstractViewElement;
+import org.obeonetwork.dsl.cinematic.view.ViewContainer;
 
 public class CinematicDropServices {
-	
+
 	public static boolean isDropAllowedForCinematicElementOn(EObject element, EObject container) {
 		EList<EReference> containmentReferences = container.eClass().getEAllContainments();
 		for (EReference eReference : containmentReferences) {
@@ -27,12 +30,20 @@ public class CinematicDropServices {
 				}
 			}
 		}
-		
+
 		return true;
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	public static EObject dropOnCinematicContainer(EObject element, EObject container) {
+		if (element instanceof AbstractViewElement && container instanceof ViewContainer) {
+			if (!CinematicBindingServices.canDropAbstractViewElementIntoViewContainer((AbstractViewElement) element,
+					(ViewContainer) container)) {
+				MessageDialog.openWarning(null, "Drop into a ViewContainer",
+						"Cannot drop since parent ViewContainer(s) bindings will be lost.");
+				return element;
+			}
+		}
 		EList<EReference> containmentReferences = container.eClass().getEAllContainments();
 		for (EReference eReference : containmentReferences) {
 			if (eReference.getEType() != null) {
@@ -40,7 +51,7 @@ public class CinematicDropServices {
 					// We're going to move the element into this containment feature
 					Object oldValues = container.eGet(eReference, true);
 					if (oldValues instanceof Collection) {
-						((Collection<EObject>)oldValues).add(element);
+						((Collection<EObject>) oldValues).add(element);
 					}
 //					container.eSet(eReference, oldValues);
 					return element;
