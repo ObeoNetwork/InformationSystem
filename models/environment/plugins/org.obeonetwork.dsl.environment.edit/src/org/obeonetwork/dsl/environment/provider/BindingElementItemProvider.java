ce4577cd1382fb1256541e81053d7afa636fd5fa
Vincent Richard <vincent.richard@obeo.fr>
2024-04-03 15:00:49 +0200

    [releng] Bump copyright end year to 2024

--- models/environment/plugins/org.obeonetwork.dsl.environment.edit/src/org/obeonetwork/dsl/environment/provider/BindingElementItemProvider.java
+++ models/environment/plugins/org.obeonetwork.dsl.environment.edit/src/org/obeonetwork/dsl/environment/provider/BindingElementItemProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2023 Obeo.
+ * Copyright (c) 2008, 2024 Obeo.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -38,7 +38,7 @@ public class BindingElementItemProvider extends ObeoDSMObjectItemProvider {
 	 * <!-- end-user-doc -->
 	 * @generated
 	 */
-	public static final String copyright = "Copyright (c) 2008, 2023 Obeo.\nAll rights reserved. This program and the accompanying materials\nare made available under the terms of the Eclipse Public License v2.0\nwhich accompanies this distribution, and is available at\nhttps://www.eclipse.org/legal/epl-2.0/\n\nContributors:\n    Obeo - initial API and implementation";
+	public static final String copyright = "Copyright (c) 2008, 2024 Obeo.\nAll rights reserved. This program and the accompanying materials\nare made available under the terms of the Eclipse Public License v2.0\nwhich accompanies this distribution, and is available at\nhttps://www.eclipse.org/legal/epl-2.0/\n\nContributors:\n    Obeo - initial API and implementation";
 
 	private ComposedAdapterFactory composedAdapterFactory;
 
