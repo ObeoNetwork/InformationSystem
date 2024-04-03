84cb012c33db008339ba77c129eff43d538e0afc
Vincent Richard <vincent.richard@obeo.fr>
2024-04-03 15:00:49 +0200

    [releng] Bump copyright end year to 2024

--- models/environment/plugins/org.obeonetwork.dsl.environment/src/org/obeonetwork/dsl/environment/impl/StructuredTypeImpl.java
+++ models/environment/plugins/org.obeonetwork.dsl.environment/src/org/obeonetwork/dsl/environment/impl/StructuredTypeImpl.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2023 Obeo.
+ * Copyright (c) 2008, 2024 Obeo.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -54,7 +54,7 @@ public abstract class StructuredTypeImpl extends TypeImpl implements StructuredT
 	 * <!-- end-user-doc -->
 	 * @generated
 	 */
-	public static final String copyright = "Copyright (c) 2008, 2023 Obeo.\nAll rights reserved. This program and the accompanying materials\nare made available under the terms of the Eclipse Public License v2.0\nwhich accompanies this distribution, and is available at\nhttps://www.eclipse.org/legal/epl-2.0/\n\nContributors:\n    Obeo - initial API and implementation";
+	public static final String copyright = "Copyright (c) 2008, 2024 Obeo.\nAll rights reserved. This program and the accompanying materials\nare made available under the terms of the Eclipse Public License v2.0\nwhich accompanies this distribution, and is available at\nhttps://www.eclipse.org/legal/epl-2.0/\n\nContributors:\n    Obeo - initial API and implementation";
 
 	/**
 	 * <!-- begin-user-doc -->
