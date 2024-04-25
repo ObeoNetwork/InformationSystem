644fa2b0097d24cefb4fb3a4803aca424296a1cc
Vincent Richard <vincent.richard@obeo.fr>
2024-04-25 18:07:39 +0200

    [SAFRAN-1144] Tab ordering - Test

---
+++ designs/environment/plugins/org.obeonetwork.dsl.environment.design.tests/src/org/obeonetwork/dsl/environment/design/tests/ISPropertiesTabsRegistryInstallTest.java
@@ -0,0 +1,14 @@
+package org.obeonetwork.dsl.environment.design.tests;
+
+import org.junit.Test;
+import org.obeonetwork.dsl.environment.design.views.ISPropertiesTabsRegistry;
+
+public class ISPropertiesTabsRegistryInstallTest {
+	
+	@Test
+	public void testInstallISPropertiesTabsRegistry() {
+		// Test ISPropertiesTabsRegistry.install() introspection is working.
+		ISPropertiesTabsRegistry.install();
+	}
+
+}
