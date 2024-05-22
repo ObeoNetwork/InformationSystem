18260428354a8919c6c83ef29a58ec288de46e66
Vincent Richard <vincent.richard@obeo.fr>
2024-05-22 11:20:50 +0200

    [SAFRAN-822] Use ISObjectSelectionWizard to benefit the search field for select linked requirement dialog

--- commons/tools/plugins/org.obeonetwork.tools.requirement/src/org/obeonetwork/tools/requirement/ui/view/action/LinkRequirementAction.java
+++ commons/tools/plugins/org.obeonetwork.tools.requirement/src/org/obeonetwork/tools/requirement/ui/view/action/LinkRequirementAction.java
@@ -13,13 +13,26 @@
  */
 package org.obeonetwork.tools.requirement.ui.view.action;
 
+import static java.util.stream.Collectors.toList;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.PlatformUI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.transaction.RecordingCommand;
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
+import org.eclipse.jface.window.Window;
+import org.eclipse.sirius.business.api.query.EObjectQuery;
+import org.obeonetwork.dsl.environment.design.wizards.ISObjectSelectionWizard;
+import org.obeonetwork.dsl.environment.design.wizards.ISObjectTreeItemWrapper;
+import org.obeonetwork.dsl.requirement.Repository;
+import org.obeonetwork.dsl.requirement.Requirement;
 import org.obeonetwork.tools.linker.ui.view.EObjectLinksView;
 import org.obeonetwork.tools.linker.ui.view.EObjectLinksViewAction;
 import org.obeonetwork.tools.requirement.RequirementLinkerPlugin;
-import org.obeonetwork.tools.requirement.ui.dialog.LinkRequirementsDialog;
+import org.obeonetwork.tools.requirement.core.util.RequirementService;
 
 /**
  * @author Obeo
@@ -38,6 +51,23 @@ public class LinkRequirementAction extends EObjectLinksViewAction {
 		this.setEnabled(false);
 	}
 
+	private static List<EObject> getRequirementTreeNodeChildren(Object parent) {
+		List<EObject> children = new ArrayList<>();
+		
+		if(parent instanceof Resource) {
+			Resource resource = (Resource) parent;
+			resource.getContents().stream()
+			.filter(Repository.class::isInstance).map(Repository.class::cast)
+			.forEach(repo -> children.add(repo));
+		} else if(parent instanceof EObject)  {
+			EObject eObject = (EObject) parent;
+			children.addAll(eObject.eContents());
+		}
+		
+		return children;
+		
+	}
+	
 	/**
 	 * {@inheritDoc}
 	 * 
@@ -45,12 +75,58 @@ public class LinkRequirementAction extends EObjectLinksViewAction {
 	 */
 	@Override
 	public void run() {
-		if (linksView.getInput() != null) {
-			Shell shell = PlatformUI.getWorkbench().getDisplay().getActiveShell();
-			LinkRequirementsDialog linkRequirementsDialog = new LinkRequirementsDialog(shell, linksView.getInput());
-			linkRequirementsDialog.open();
-			linksView.refresh();
+		if (linksView.getInput() == null) {
+			return;
 		}
+		
+		EObject context = linksView.getInput();
+		ISObjectTreeItemWrapper treeRoot = new ISObjectTreeItemWrapper(
+				LinkRequirementAction::getRequirementTreeNodeChildren,
+				(wrappedEObject) -> wrappedEObject instanceof Requirement
+			);
+		
+		for(Resource repositoryResource : RequirementService.findRequirementsRepositories(context)) {
+			new ISObjectTreeItemWrapper(treeRoot, repositoryResource);
+		}
+		
+        final ISObjectSelectionWizard wizard = new ISObjectSelectionWizard(
+        		"Link Requirements", 
+        		"Select the requirements to be linked to this element.", 
+        		null, 
+        		treeRoot,
+        		true);
+        
+        wizard.setLevelToExpand(2);
+        
+        Collection<Requirement> preSelectedRequirements = RequirementService.getLinkedRequirementsSet(context);
+		wizard.setPreSelectedObjects(preSelectedRequirements);
+        
+		if(wizard.open() == Window.OK) {
+			TransactionalEditingDomain ted = new EObjectQuery(context).getSession().getTransactionalEditingDomain();
+			ted.getCommandStack().execute(new RecordingCommand(ted, "Link Requirements") {
+				
+				@Override
+				protected void doExecute() {
+					Collection<Requirement> selectedRequirements = wizard.getSelectedObjects()
+							.stream().map(Requirement.class::cast).collect(toList());
+					// Unlink deselected requirements
+					for(Requirement preSelectedRequirement : preSelectedRequirements) {
+						if(!selectedRequirements.contains(preSelectedRequirement)) {
+							preSelectedRequirement.getReferencedObject().remove(context);
+						}
+					}
+					// Link newly selected requirements
+					for(Requirement selectedRequirement : selectedRequirements) {
+						if(!preSelectedRequirements.contains(selectedRequirement)) {
+							selectedRequirement.getReferencedObject().add(context);
+						}
+					}
+				}
+			});
+			
+		}
+		
+		linksView.refresh();
 	}
 
 	/**
