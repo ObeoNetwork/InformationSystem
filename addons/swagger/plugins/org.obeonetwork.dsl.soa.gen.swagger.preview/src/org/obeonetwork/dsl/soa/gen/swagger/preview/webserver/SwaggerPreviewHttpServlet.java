b9f17e8c5740927ef88d3c536878a084fc94a47f
fksimo <fksimo@obeo.fr>
2024-06-07 18:39:49 +0200

    [SAFRAN-1159] Fix Swagger Preview
    Swagger Preview now displays content generated from the SOA component on
    which it was launched

--- addons/swagger/plugins/org.obeonetwork.dsl.soa.gen.swagger.preview/src/org/obeonetwork/dsl/soa/gen/swagger/preview/webserver/SwaggerPreviewHttpServlet.java
+++ addons/swagger/plugins/org.obeonetwork.dsl.soa.gen.swagger.preview/src/org/obeonetwork/dsl/soa/gen/swagger/preview/webserver/SwaggerPreviewHttpServlet.java
@@ -10,10 +10,14 @@
  *******************************************************************************/
 package org.obeonetwork.dsl.soa.gen.swagger.preview.webserver;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.URLDecoder;
 import java.nio.file.Files;
+import java.nio.file.Path;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -23,6 +27,8 @@ import javax.servlet.http.HttpServletResponse;
 public class SwaggerPreviewHttpServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
+	
+	private static String swaggerFileNameParameter = "";
 
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException {
         try {
@@ -47,6 +53,9 @@ public class SwaggerPreviewHttpServlet extends HttpServlet {
 			}
 			
         	String resourceName = URLDecoder.decode(req.getRequestURI().substring(1), "UTF-8");
+        	if(req.getParameter("url")!=null) {
+        		swaggerFileNameParameter = req.getParameter("url");
+        	}
         	File resourceFile = SwaggerPreviewJettyServer.instance().getSwaggerUiDistFile(resourceName);
         	if(resourceFile == null) {
         		resourceFile = SwaggerPreviewJettyServer.instance().getSwaggerFile(resourceName);
@@ -56,7 +65,15 @@ public class SwaggerPreviewHttpServlet extends HttpServlet {
     			}
         	}
 			if(resourceFile != null) {
-				Files.copy(resourceFile.toPath(), resp.getOutputStream());
+				Path pathToCopy;
+				if (resourceFile.getAbsolutePath().matches(".*swagger-initializer.*\\.js")) {
+					pathToCopy = changeDefaultURLInSwaggerInitializerJavascript(resourceFile.getAbsolutePath(),
+							swaggerFileNameParameter);
+				} else {
+					pathToCopy = resourceFile.toPath();
+				}
+				
+				Files.copy(pathToCopy, resp.getOutputStream());
 			} else {
 				resp.getWriter().write("404 not found");
 			}
@@ -65,4 +82,24 @@ public class SwaggerPreviewHttpServlet extends HttpServlet {
 			e.printStackTrace();
 		}
     }
+	
+	private static final String DEFAULT_SWAGGER_URL_IN_INITIALIZER = "https://petstore.swagger.io/v2/swagger.json";
+
+	private static Path changeDefaultURLInSwaggerInitializerJavascript(String absolutePath, String swaggerFileName)
+			throws IOException {
+		File newTempFile = File.createTempFile("swagger-initializer", ".js");
+		newTempFile.deleteOnExit();
+		BufferedReader file = new BufferedReader(new FileReader(absolutePath));
+		PrintWriter writer = new PrintWriter(newTempFile, "UTF-8");
+		String line;
+		while ((line = file.readLine()) != null) {
+			if (line.contains("url: \"" + DEFAULT_SWAGGER_URL_IN_INITIALIZER + "\",")) {
+				line = line.replace(DEFAULT_SWAGGER_URL_IN_INITIALIZER, swaggerFileName);
+			}
+			writer.println(line);
+		}
+		file.close();
+		writer.close();
+		return newTempFile.toPath();
+	}
 }
