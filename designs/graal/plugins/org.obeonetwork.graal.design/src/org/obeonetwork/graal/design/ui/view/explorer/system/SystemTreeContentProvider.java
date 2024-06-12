132f3fcfb29220fe7d8fdaad08dd1f7f3a6ed862
fksimo <fksimo@obeo.fr>
2024-06-12 11:10:48 +0200

    [SAFRAN-710] Fix representations not appearing in model explorer view
    - The children of the nodes (Task, UseCase, etc.) under the logical
    nodes (Tasks, SubSystems, etc.) of a Graal System did not include
    representations. Now they do.

--- designs/graal/plugins/org.obeonetwork.graal.design/src/org/obeonetwork/graal/design/ui/view/explorer/system/SystemTreeContentProvider.java
+++ designs/graal/plugins/org.obeonetwork.graal.design/src/org/obeonetwork/graal/design/ui/view/explorer/system/SystemTreeContentProvider.java
@@ -1,10 +1,24 @@
 package org.obeonetwork.graal.design.ui.view.explorer.system;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.edit.provider.ComposedAdapterFactory;
 import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
 import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.sirius.business.api.dialect.DialectManager;
+import org.eclipse.sirius.business.api.query.RepresentationDescriptionQuery;
+import org.eclipse.sirius.business.api.session.Session;
+import org.eclipse.sirius.business.api.session.SessionManager;
+import org.eclipse.sirius.common.tools.api.util.EqualityHelper;
+import org.eclipse.sirius.viewpoint.DRepresentationDescriptor;
+import org.eclipse.sirius.viewpoint.description.RepresentationDescription;
+import org.eclipse.sirius.viewpoint.description.Viewpoint;
 import org.obeonetwork.graal.System;
 import org.obeonetwork.graal.provider.custom.SystemCustomContentItemProvider;
 
@@ -13,6 +27,8 @@ import org.obeonetwork.graal.provider.custom.SystemCustomContentItemProvider;
  *
  */
 public class SystemTreeContentProvider implements ITreeContentProvider {
+	final AdapterFactory composedAdapterFactory = new ComposedAdapterFactory(
+			ComposedAdapterFactory.Descriptor.Registry.INSTANCE);
 	@Override
 	public Object[] getElements(Object inputElement) {
 		return new Object[0];
@@ -24,14 +40,52 @@ public class SystemTreeContentProvider implements ITreeContentProvider {
 			SystemCustomContentItemProvider systemCustomContentItemProvider = (SystemCustomContentItemProvider) parentElement;
 			return systemCustomContentItemProvider.getChildren(systemCustomContentItemProvider.getTarget()).toArray();
 		} else if (parentElement instanceof EObject && !(parentElement instanceof System)) {// Below SystemCustomContentItemProvider
-			final AdapterFactory composedAdapterFactory = new ComposedAdapterFactory(
-					ComposedAdapterFactory.Descriptor.Registry.INSTANCE);
+			List<Object> result = new ArrayList<>();
+			result.addAll(getRepresentationDescriptorsAssociatedToEObject((EObject) parentElement));
 			Object itemProvider = composedAdapterFactory.adapt((EObject) parentElement,
 					IEditingDomainItemProvider.class);
-			return ((IEditingDomainItemProvider) itemProvider).getChildren(parentElement).toArray();
+			if(itemProvider!=null) {
+				result.addAll(((IEditingDomainItemProvider) itemProvider).getChildren(parentElement));
+			}
+			return result.toArray();
 		}
 		return new Object[0];
 	}
+	
+	/**
+	 * Adapted from
+	 * org.eclipse.sirius.ui.tools.internal.views.common.SessionWrapperContentProvider
+	 * 
+	 * @param eObject
+	 * @return
+	 */
+	private Collection<DRepresentationDescriptor> getRepresentationDescriptorsAssociatedToEObject(
+			final EObject eObject) {
+		final Session session = SessionManager.INSTANCE.getSession(eObject);
+		if (session != null && session.isOpen()) {
+			return DialectManager.INSTANCE.getRepresentationDescriptors(eObject, session).stream()
+					.filter(repDescriptor -> {
+						if (repDescriptor.eResource() != null) {
+							RepresentationDescription description = repDescriptor.getDescription();
+							if (description != null) {
+								Viewpoint reprViewpoint = new RepresentationDescriptionQuery(description)
+										.getParentViewpoint();
+								if (reprViewpoint != null && !reprViewpoint.eIsProxy()
+										&& reprViewpoint.eResource() != null) {
+									for (final Viewpoint viewpoint : session.getSelectedViewpoints(false)) {
+										if (EqualityHelper.areEquals(viewpoint, reprViewpoint)) {
+											return true;
+										}
+									}
+								}
+							}
+						}
+						return false;
+					}).sorted(Comparator.comparing(DRepresentationDescriptor::getName)).toList();
+		}
+		return Collections.emptyList();
+	}
+	
 
 	@Override
 	public Object getParent(Object element) {
