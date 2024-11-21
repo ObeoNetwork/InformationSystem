562998e67112bbeb1557cc462b755f015c8ad218
Vincent Richard <vincent.richard@obeo.fr>
2024-11-21 15:57:58 +0100

    [releng] Add missing copyrights

--- designs/environment/plugins/org.obeonetwork.dsl.environment.design/src/org/obeonetwork/dsl/environment/design/extension/StructuredTypeChildrenContribution.java
+++ designs/environment/plugins/org.obeonetwork.dsl.environment.design/src/org/obeonetwork/dsl/environment/design/extension/StructuredTypeChildrenContribution.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2008, 2024 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
 package org.obeonetwork.dsl.environment.design.extension;
 
 import java.util.ArrayList;
