a24c388c9b0b7986f1edd0485713865e4a63894b
Nicolas Peransin <nicolas.peransin@obeo.fr>
2026-02-19 15:59:10 +0100

    [SAFRAN-767] Replace EEF 1.x view by EEF 2+ view

--- commons/tools/plugins/org.obeonetwork.tools.requirement/src/org/obeonetwork/tools/requirement/ui/view/LinkedRequirementsView.java
+++ commons/tools/plugins/org.obeonetwork.tools.requirement/src/org/obeonetwork/tools/requirement/ui/view/LinkedRequirementsView.java
@@ -8,23 +8,13 @@
  * Contributors:
  *     Obeo - initial API and implementation
  *******************************************************************************/
-/**
- * 
- */
+
 package org.obeonetwork.tools.requirement.ui.view;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.eef.runtime.EEFRuntimePlugin;
-import org.eclipse.emf.eef.runtime.context.impl.DomainPropertiesEditionContext;
-import org.eclipse.emf.eef.runtime.impl.operation.WizardEditingOperation;
-import org.eclipse.emf.transaction.TransactionalEditingDomain;
-import org.eclipse.emf.transaction.util.TransactionUtil;
-import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.CellLabelProvider;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerFilter;
@@ -39,7 +29,6 @@ import org.obeonetwork.tools.requirement.ui.view.action.LinkRequirementAction;
 import org.obeonetwork.tools.requirement.ui.view.action.UnlinkRequirementAction;
 import org.obeonetwork.tools.requirement.ui.view.util.LinkedRequirementsContentProvider;
 import org.obeonetwork.tools.requirement.ui.view.util.LinkedRequirementsLabelProvider;
-import org.obeonetwork.tools.requirement.wizard.operation.RequirementEditingOperation;
 
 /**
  * @author Obeo
@@ -53,11 +42,11 @@ public class LinkedRequirementsView extends EObjectLinksView {
 	protected void update(EObject selectedEObject) {
 		// Requirement and categories should not be used to attach requirements
 		// to them
+		EObject filtered = selectedEObject;
 		if (selectedEObject != null && selectedEObject.eClass().getEPackage() == RequirementPackage.eINSTANCE) {
-			super.update(null);
-		} else {
-			super.update(selectedEObject);
+			filtered = null;
 		}
+		super.update(filtered);
 	}
 
 	/**
@@ -99,19 +88,17 @@ public class LinkedRequirementsView extends EObjectLinksView {
 	 */
 	@Override
 	protected List<ViewerFilter> createFilters() {
-		List<ViewerFilter> result = new ArrayList<ViewerFilter>();
-		result.add(new ViewerFilter() {
+		return List.of(new ViewerFilter() {
 
 			@Override
 			public boolean select(Viewer viewer, Object parentElement, Object element) {
 				return element instanceof RequirementLink;
 			}
 		});
-		return result;
 	}
 
 	/**
-	 * {@inheritDoc}
+	 * Create actions for selection.
 	 * 
 	 * @see org.obeonetwork.tools.linker.ui.view.EObjectLinksView#fillLocalActionBar()
 	 */
@@ -122,42 +109,17 @@ public class LinkedRequirementsView extends EObjectLinksView {
 		getViewSite().getActionBars().getToolBarManager().add(new LinkRequirementAction(this));
 		getViewSite().getActionBars().getToolBarManager().add(new UnlinkRequirementAction(this));
 	}
-
+	
 	/**
-	 * {@inheritDoc}
+	 * Create action to edit selected requirement.
 	 * 
 	 * @see org.obeonetwork.tools.linker.ui.view.EObjectLinksView#createDoubleClickAction()
 	 */
 	@Override
-	protected Action createDoubleClickAction() {
-		return new Action() {
-
-			/**
-			 * {@inheritDoc}
-			 * 
-			 * @see org.eclipse.jface.action.Action#run()
-			 */
-			@Override
-			public void run() {
-				if (getSelectedEntries().size() > 0) {
-					RequirementLink eObjectLink = (RequirementLink) getSelectedEntries().get(0);
-
-					TransactionalEditingDomain editingDomain = TransactionUtil.getEditingDomain(getInput());
-					DomainPropertiesEditionContext propertiesEditionContext = new DomainPropertiesEditionContext(null,
-							null, editingDomain, adapterFactory, eObjectLink.getRequirement());
-					WizardEditingOperation operation = new RequirementEditingOperation(propertiesEditionContext);
-					try {
-						operation.execute(new NullProgressMonitor(), null);
-						refresh();
-					} catch (ExecutionException e) {
-						EEFRuntimePlugin.getDefault().logError("An error occured during wizard editing.", e); //$NON-NLS-1$
-					}
-				}
-			}
-
-		};
+	protected IAction createDoubleClickAction() {
+		return new EditRequirementAction(this);
 	}
-
+	
 	@Override
 	protected String getShowChildrenActionTitle() {
 		return RequirementLinkerPlugin.getInstance().getString("LinkedRequirementsView_ShowChildrenAction_title");
